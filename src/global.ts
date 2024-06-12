d802467f5375f047b9bb5ef9309a4e7e165b0800
Darrell <darrell.anderson@gmail.com>
2024-06-12 09:29:46 -0700

    tmp

--- src/global.ts
+++ src/global.ts
@@ -4,12 +4,14 @@ import { SystemRegistry } from "./lib/system-lib/registry/system-registry";
 export class TI4Class {
   systemRegistry: SystemRegistry = new SystemRegistry().load(SYSTEM_DATA);
 }
+const TI4 = new TI4Class();
+Object.freeze(TI4);
 
 // Place "TI4" in the global namespace.
 declare global {
   var TI4: TI4Class;
 }
-globalThis.TI4 = new TI4Class();
+globalThis.TI4 = TI4;
 
 // Run any delayed initialization, things that need globalThis.TI4 to be set.
 // TODO XXX
