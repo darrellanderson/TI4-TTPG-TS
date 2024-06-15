fae7b64fee50ffa9751fa766a7ed65396e1a8eb8
Darrell <darrell.anderson@gmail.com>
2024-06-15 16:43:03 -0700

    test coverage

--- src/lib/system-lib/registry/planet-attachment-registry.test.ts
+++ src/lib/system-lib/registry/planet-attachment-registry.test.ts
@@ -1,6 +1,11 @@
-import { MockGameObject } from "ttpg-mock";
+import { GameObject, Player, Vector } from "@tabletop-playground/api";
+import { MockGameObject, MockPlayer } from "ttpg-mock";
+
+import { Planet } from "../planet/planet";
+import { PlanetAttachment } from "../planet-attachment/planet-attachment";
 import { PlanetAttachmentRegistry } from "./planet-attachment-registry";
-import { GameObject } from "@tabletop-playground/api";
+import { System } from "../system/system";
+import { resetGlobalThisTI4 } from "../../../global/global";
 
 it("constructor", () => {
   new PlanetAttachmentRegistry();
@@ -28,4 +33,122 @@ it("object create/desroy", () => {
   expect(registry.getByPlanetAttachmentObjId("my-id")).toBeUndefined();
 
   registry.destroy();
+});
+
+it("grab/release", () => {
+  resetGlobalThisTI4(); // for TI4.systemRegistry
+  new MockGameObject({
+    templateMetadata: "tile.system:base/1",
+  });
+  const system: System | undefined = TI4.systemRegistry.getByPosition(
+    new Vector(0, 0, 0)
+  );
+  expect(system).toBeDefined();
+  if (!system) {
+    throw new Error("system not found"); // for TypeScript
+  }
+  const planet: Planet | undefined = system.getPlanets()[0];
+  expect(planet).toBeDefined();
+  if (!planet) {
+    throw new Error("planet not found"); // for TypeScript
+  }
+
+  const registry = new PlanetAttachmentRegistry().load(
+    { source: "my-source", packageId: "my-package-id" },
+    [
+      {
+        name: "my-name",
+        nsidName: "my-nsid-name",
+      },
+    ]
+  );
+  const token: MockGameObject = new MockGameObject({
+    id: "my-id",
+    templateMetadata: "token.attachment.planet:my-source/my-nsid-name",
+  });
+  const attachment: PlanetAttachment | undefined =
+    registry.getByPlanetAttachmentObjId("my-id");
+  expect(attachment).toBeDefined();
+  if (!attachment) {
+    throw new Error("attachment not found"); // for TypeScript
+  }
+
+  expect(planet.hasAttachment(attachment)).toBe(false);
+
+  const player: Player = new MockPlayer();
+  token._releaseAsPlayer(player, false);
+  expect(planet.hasAttachment(attachment)).toBe(true);
+
+  token._grabAsPlayer(player);
+  expect(planet.hasAttachment(attachment)).toBe(false);
+
+  registry.destroy();
+});
+
+it("getByPlanetAttachmentObjId", () => {
+  const registry = new PlanetAttachmentRegistry().load(
+    { source: "my-source", packageId: "my-package-id" },
+    [{ name: "my-name", nsidName: "my-nsid-name" }]
+  );
+  const token: GameObject = new MockGameObject({
+    id: "my-id",
+    templateMetadata: "token.attachment.planet:my-source/my-nsid-name",
+  });
+  expect(registry.getByPlanetAttachmentObjId("my-id")).toBeDefined();
+});
+
+it("init attaches", () => {
+  resetGlobalThisTI4(); // for TI4.systemRegistry
+  new MockGameObject({
+    templateMetadata: "tile.system:base/1",
+  });
+  const system: System | undefined = TI4.systemRegistry.getByPosition(
+    new Vector(0, 0, 0)
+  );
+  expect(system).toBeDefined();
+  if (!system) {
+    throw new Error("system not found"); // for TypeScript
+  }
+  const planet: Planet | undefined = system.getPlanets()[0];
+  expect(planet).toBeDefined();
+  if (!planet) {
+    throw new Error("planet not found"); // for TypeScript
+  }
+
+  // Token exists before registry is created.
+  new MockGameObject({
+    id: "my-id",
+    templateMetadata: "token.attachment.planet:my-source/my-nsid-name",
+  });
+  const registry = new PlanetAttachmentRegistry().load(
+    { source: "my-source", packageId: "my-package-id" },
+    [
+      {
+        name: "my-name",
+        nsidName: "my-nsid-name",
+      },
+    ]
+  );
+  const attachment: PlanetAttachment | undefined =
+    registry.getByPlanetAttachmentObjId("my-id");
+  expect(attachment).toBeDefined();
+  if (!attachment) {
+    throw new Error("attachment not found"); // for TypeScript
+  }
+
+  expect(planet.hasAttachment(attachment)).toBe(false);
+
+  registry.init();
+  expect(planet.hasAttachment(attachment)).toBe(true);
+
+  registry.destroy();
+});
+
+it("load (corrupt data)", () => {
+  expect(() => {
+    new PlanetAttachmentRegistry().load(
+      { source: "my-source", packageId: "my-package-id" },
+      [{ name: "my-name", nsidName: "@@invalid" }]
+    );
+  }).toThrow();
 });
