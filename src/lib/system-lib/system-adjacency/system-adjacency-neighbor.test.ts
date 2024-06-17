da9e7eb7f4ee86532280d3bd19932bb43fa037c4
darrell <darrell.anderson@gmail.com>
2024-06-16 23:14:47 -0700

    add an off-map system to test, verify not included

--- src/lib/system-lib/system-adjacency/system-adjacency-neighbor.test.ts
+++ src/lib/system-lib/system-adjacency/system-adjacency-neighbor.test.ts
@@ -1,5 +1,5 @@
 import { MockGameObject } from "ttpg-mock";
-import { Adjacency, HexType } from "ttpg-darrell";
+import { Adjacency, AdjacencyResult, HexType } from "ttpg-darrell";
 
 import { System } from "../system/system";
 import { SystemAdjacency } from "./system-adjacency";
@@ -20,13 +20,41 @@ it("addTags", () => {
     position: TI4.hex.toPosition("<1,0,-1>"),
     templateMetadata: "tile.system:base/2",
   });
+  new MockGameObject({
+    position: TI4.hex.toPosition("<2,0,-2>"),
+    templateMetadata: "tile.system:base/3",
+  });
+  new MockGameObject({
+    position: TI4.hex.toPosition("<3,0,-3>"),
+    templateMetadata: "tile.system:base/51", // off-map, never a neighbor
+  });
   const hexToSystem: Map<HexType, System> = SystemAdjacency.getHexToSystem();
   expect(hexToSystem.get("<0,0,0>")?.getSystemTileNumber()).toBe(1);
   expect(hexToSystem.get("<1,0,-1>")?.getSystemTileNumber()).toBe(2);
+  expect(hexToSystem.get("<2,0,-2>")?.getSystemTileNumber()).toBe(3);
 
   const adjacency: Adjacency = new Adjacency();
   new SystemAdjacencyNeighbor().addTags(hexToSystem, adjacency);
+
   expect(adjacency.hasNodeTag("<0,0,0>", "<0,0,0>-<1,0,-1>")).toBe(true);
   expect(adjacency.hasNodeTag("<1,0,-1>", "<0,0,0>-<1,0,-1>")).toBe(true);
   expect(adjacency.hasLink("<0,0,0>-<1,0,-1>", "<0,0,0>-<1,0,-1>")).toBe(true);
+
+  expect(adjacency.hasNodeTag("<1,0,-1>", "<1,0,-1>-<2,0,-2>")).toBe(true);
+  expect(adjacency.hasNodeTag("<2,0,-2>", "<1,0,-1>-<2,0,-2>")).toBe(true);
+  expect(adjacency.hasLink("<1,0,-1>-<2,0,-2>", "<1,0,-1>-<2,0,-2>")).toBe(
+    true
+  );
+
+  // Verify adjacency is being used correctly.
+  const paths: Array<AdjacencyResult> = adjacency.get("<0,0,0>", 100);
+  expect(paths).toEqual([
+    { distance: 0, node: "<0,0,0>", path: ["<0,0,0>"] },
+    { distance: 1, node: "<1,0,-1>", path: ["<0,0,0>", "<1,0,-1>"] },
+    {
+      distance: 2,
+      node: "<2,0,-2>",
+      path: ["<0,0,0>", "<1,0,-1>", "<2,0,-2>"],
+    },
+  ]);
 });
