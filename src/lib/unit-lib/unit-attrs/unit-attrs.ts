4785258c63c4d36455fb9fcb3ad97f8a47f305b5
Darrell <darrell.anderson@gmail.com>
2024-06-26 13:00:04 -0700

    progress

--- src/lib/unit-lib/unit-attrs/unit-attrs.ts
+++ src/lib/unit-lib/unit-attrs/unit-attrs.ts
@@ -21,24 +21,31 @@ export class CombatAttrs {
     this._critCount = params.critCount ?? 0; // N extra hits on crit
   }
 
-  applyUpgrade(upgrade: CombatAttrsSchemaType): this {
-    if (upgrade.crit) {
-      this._crit = upgrade.crit;
+  /**
+   * Apply overrides to the given attributes.
+   * If an attribute is missing, do not change it.
+   *
+   * @param override
+   * @returns
+   */
+  applyOverride(override: CombatAttrsSchemaType): this {
+    if (override.crit) {
+      this._crit = override.crit;
     }
-    if (upgrade.critCount) {
-      this._critCount = upgrade.critCount;
+    if (override.critCount) {
+      this._critCount = override.critCount;
     }
-    if (upgrade.dice) {
-      this._dice = upgrade.dice;
+    if (override.dice) {
+      this._dice = override.dice;
     }
-    if (upgrade.extraDice) {
-      this._extraDice = upgrade.extraDice;
+    if (override.extraDice) {
+      this._extraDice = override.extraDice;
     }
-    if (upgrade.hit) {
-      this._hit = upgrade.hit;
+    if (override.hit) {
+      this._hit = override.hit;
     }
-    if (upgrade.rerollMisses) {
-      this._rerollMisses = upgrade.rerollMisses;
+    if (override.rerollMisses) {
+      this._rerollMisses = override.rerollMisses;
     }
     return this;
   }
@@ -99,22 +106,121 @@ export class CombatAttrs {
 }
 
 export class UnitAttrs {
+  private readonly _name: string;
   private readonly _unit: UnitType;
-  private _count: number = 0;
+
+  private _unitCount: number; // component count, e.g. fighters = 10
+  private _cost: number | undefined;
+  private _producePerCost: number; // e.g. 2 for fighters
+
+  private _isShip: boolean;
+  private _isGround: boolean;
+
+  private _sustainDamage: boolean;
+  private _planetaryShild: boolean;
+  private _disablePlanetaryShield: boolean;
+
+  private _antiFighterBarrage: CombatAttrs | undefined;
+  private _bombardment: CombatAttrs | undefined;
+  private _spaceCannon: CombatAttrs | undefined;
+  private _spaceCombat: CombatAttrs | undefined;
+  private _groundCombat: CombatAttrs | undefined;
 
   constructor(params: UnitAttrsSchemaType) {
+    this._name = params.name;
     this._unit = params.unit;
-  }
 
-  getCount(): number {
-    return this._count;
-  }
+    this._unitCount = params.unitCount ?? 0;
+    this._producePerCost = params.producePerCost ?? 1;
 
-  setCount(value: number): this {
-    if (value < 0) {
-      throw new Error(`invalid value (${value})`);
+    this._isShip = params.isShip ?? false;
+    this._isGround = params.isGround ?? false;
+    this._sustainDamage = params.sustainDamage ?? false;
+    this._planetaryShild = params.planetaryShild ?? false;
+    this._disablePlanetaryShield = params.disablePlanetaryShield ?? false;
+
+    if (params.antiFighterBarrage) {
+      this._antiFighterBarrage = new CombatAttrs(params.antiFighterBarrage);
     }
-    this._count = value;
+    if (params.bombardment) {
+      this._bombardment = new CombatAttrs(params.bombardment);
+    }
+    if (params.spaceCannon) {
+      this._spaceCannon = new CombatAttrs(params.spaceCannon);
+    }
+    if (params.spaceCombat) {
+      this._spaceCombat = new CombatAttrs(params.spaceCombat);
+    }
+    if (params.groundCombat) {
+      this._groundCombat = new CombatAttrs(params.groundCombat);
+    }
+  }
+
+  /**
+   * Apply overrides to the given attributes.
+   * If an attribute is missing, do not change it.
+   *
+   * @param override
+   * @returns
+   */
+  applyOverride(override: UnitAttrsSchemaType): this {
+    if (override.cost) {
+      this._cost = override.cost;
+    }
+    if (override.isGround) {
+      this._isGround = override.isGround;
+    }
+    if (override.isShip) {
+      this._isShip = override.isShip;
+    }
+    if (override.planetaryShild) {
+      this._planetaryShild = override.planetaryShild;
+    }
+    if (override.producePerCost) {
+      this._producePerCost = override.producePerCost;
+    }
+    if (override.sustainDamage) {
+      this._sustainDamage = override.sustainDamage;
+    }
+    if (override.unitCount) {
+      this._unitCount = override.unitCount;
+    }
+    if (override.antiFighterBarrage) {
+      if (!this._antiFighterBarrage) {
+        this._antiFighterBarrage = new CombatAttrs(override.antiFighterBarrage);
+      } else {
+        this._antiFighterBarrage.applyOverride(override.antiFighterBarrage);
+      }
+    }
+    if (override.bombardment) {
+      if (!this._bombardment) {
+        this._bombardment = new CombatAttrs(override.bombardment);
+      } else {
+        this._bombardment.applyOverride(override.bombardment);
+      }
+    }
+    if (override.spaceCannon) {
+      if (!this._spaceCannon) {
+        this._spaceCannon = new CombatAttrs(override.spaceCannon);
+      } else {
+        this._spaceCannon.applyOverride(override.spaceCannon);
+      }
+    }
+    if (override.spaceCombat) {
+      if (!this._spaceCombat) {
+        this._spaceCombat = new CombatAttrs(override.spaceCombat);
+      } else {
+        this._spaceCombat.applyOverride(override.spaceCombat);
+      }
+    }
+    if (override.groundCombat) {
+      if (!this._groundCombat) {
+        this._groundCombat = new CombatAttrs(override.groundCombat);
+      } else {
+        this._groundCombat.applyOverride(override.groundCombat);
+      }
+    }
+
     return this;
   }
 }
