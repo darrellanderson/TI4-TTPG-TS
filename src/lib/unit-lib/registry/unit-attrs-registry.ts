078d2e0a474911c13385995334563cd5488679fa
darrell <darrell.anderson@gmail.com>
2024-06-26 21:55:07 -0700

    fix import

--- src/lib/unit-lib/registry/unit-attrs-registry.ts
+++ src/lib/unit-lib/registry/unit-attrs-registry.ts
@@ -1,1 +1,52 @@
-export class UnitAttrsRegistry {}
+import { NsidNameSchema } from "../../system-lib/schema/basic-types-schema";
+import {
+  UnitAttrsSchema,
+  UnitAttrsSchemaType,
+  UnitType,
+} from "../schema/unit-attrs-schema";
+import { SOURCE_TO_UNIT_ATTRS_DATA } from "../data/unit-attrs.data";
+
+export class UnitAttrsRegistry {
+  private readonly _unitToBaseAttrs: Map<UnitType, UnitAttrsSchemaType> =
+    new Map();
+  private readonly _nsidNameToOverrideAttrs: Map<string, UnitAttrsSchemaType> =
+    new Map();
+
+  constructor() {}
+
+  load(source: string, unitAttrsArray: Array<UnitAttrsSchemaType>): this {
+    for (const unitAttrs of unitAttrsArray) {
+      // Validate schema (oterhwise not validated until used).
+      try {
+        NsidNameSchema.parse(source);
+        UnitAttrsSchema.parse(unitAttrs);
+      } catch (e) {
+        const msg = `error: ${e.message}\nparsing: ${JSON.stringify(
+          unitAttrs
+        )}`;
+        throw new Error(msg);
+      }
+
+      if (unitAttrs.nsidName) {
+        this._nsidNameToOverrideAttrs.set(unitAttrs.nsidName, unitAttrs);
+      } else {
+        this._unitToBaseAttrs.set(unitAttrs.unit, unitAttrs);
+      }
+    }
+    return this;
+  }
+
+  /**
+   * Load the game data (base plus codices and expansions).
+   *
+   * @returns
+   */
+  public loadDefaultData(): this {
+    for (const [source, unitAttrsArray] of Object.entries(
+      SOURCE_TO_UNIT_ATTRS_DATA
+    )) {
+      this.load(source, unitAttrsArray);
+    }
+    return this;
+  }
+}
