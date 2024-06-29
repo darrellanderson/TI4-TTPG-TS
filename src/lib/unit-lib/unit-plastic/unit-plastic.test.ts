3c533234e48fc25fcd0bb3460d18200e51d22b97
darrell <darrell.anderson@gmail.com>
2024-06-28 23:10:19 -0700

    test coverage

--- src/lib/unit-lib/unit-plastic/unit-plastic.test.ts
+++ src/lib/unit-lib/unit-plastic/unit-plastic.test.ts
@@ -6,14 +6,69 @@ it("constructor", () => {
   new UnitPlastic();
 });
 
-it("getOne", () => {
-  const unitPlastic = new UnitPlastic();
-
+it("getOne (plastic)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "unit:base/infantry",
     owningPlayerSlot: 1,
   });
-  const entry: UnitPlasticEntry | undefined = unitPlastic.getOne(obj);
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
   expect(entry?.unit).toBe("infantry");
+  expect(entry?.count).toBe(1);
   expect(entry?.owningPlayerSlot).toBe(1);
+});
+
+it("getOne (unknown)", () => {
+  const obj: GameObject = new MockGameObject({
+    templateMetadata: "unit:base/__unknown__",
+  });
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  expect(entry).toBeUndefined();
+});
+
+it("getOne (invalid nsid, match enough to check)", () => {
+  const obj: GameObject = new MockGameObject({
+    templateMetadata: "unit:@@@",
+  });
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  expect(entry).toBeUndefined();
+});
+
+it("getOne (fighter-1 token)", () => {
+  const obj: GameObject = new MockGameObject({
+    templateMetadata: "token:base/fighter-1",
+  });
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  expect(entry?.unit).toBe("fighter");
+  expect(entry?.count).toBe(1);
+  expect(entry?.owningPlayerSlot).toBe(-1);
+});
+
+it("getOne (fighter-3 token)", () => {
+  const obj: GameObject = new MockGameObject({
+    templateMetadata: "token:base/fighter-3",
+  });
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  expect(entry?.unit).toBe("fighter");
+  expect(entry?.count).toBe(3);
+  expect(entry?.owningPlayerSlot).toBe(-1);
+});
+
+it("getOne (infantry-1 token)", () => {
+  const obj: GameObject = new MockGameObject({
+    templateMetadata: "token:base/infantry-1",
+  });
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  expect(entry?.unit).toBe("infantry");
+  expect(entry?.count).toBe(1);
+  expect(entry?.owningPlayerSlot).toBe(-1);
+});
+
+it("getOne (infantry-3 token)", () => {
+  const obj: GameObject = new MockGameObject({
+    templateMetadata: "token:base/infantry-3",
+  });
+  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  expect(entry?.unit).toBe("infantry");
+  expect(entry?.count).toBe(3);
+  expect(entry?.owningPlayerSlot).toBe(-1);
 });
