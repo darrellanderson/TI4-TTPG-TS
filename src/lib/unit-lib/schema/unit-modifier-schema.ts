63f7db49acb440a3f54b631fa238c9059a467ee2
darrell <darrell.anderson@gmail.com>
2024-06-28 21:10:33 -0700

    comments

--- src/lib/unit-lib/schema/unit-modifier-schema.ts
+++ src/lib/unit-lib/schema/unit-modifier-schema.ts
@@ -12,7 +12,12 @@ export const UnitModifierSchema = z.object({
   owner: z.enum(["self", "opponent", "any"]),
   priority: z.enum(["mutate", "adjust", "choose"]),
 
+  // Should this modifier be used?
+  // Zod supports z.* argument validation, use z.any() to allow something else.
   applies: z.function().args(z.any()).returns(z.boolean()).optional(),
+
+  // Apply the modifier to the unit attributes.
+  // Zod supports z.* argument validation, use z.any() to allow something else.
   apply: z.function().args(z.any()).returns(z.void()).optional(),
 });
 export type UnitModifierSchemaType = z.infer<typeof UnitModifierSchema>;
