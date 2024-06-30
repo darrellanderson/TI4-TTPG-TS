756305a267df58bc2a13ba2ecd36242b7129e4f6
darrell <darrell.anderson@gmail.com>
2024-06-29 20:17:11 -0700

    test coverage

--- src/lib/unit-lib/unit-plastic/unit-plastic.test.ts
+++ src/lib/unit-lib/unit-plastic/unit-plastic.test.ts
@@ -1,27 +1,38 @@
-import { GameObject } from "@tabletop-playground/api";
-import { UnitPlastic, UnitPlasticEntry } from "./unit-plastic";
+import { GameObject, Vector } from "@tabletop-playground/api";
 import { MockGameObject } from "ttpg-mock";
 
+import { UnitPlastic } from "./unit-plastic";
+import { UnitType } from "../schema/unit-attrs-schema";
+import exp from "constants";
+import { HexType } from "ttpg-darrell";
+
 it("constructor", () => {
-  new UnitPlastic();
+  const unit: UnitType = "infantry";
+  const count: number = 1;
+  const obj: GameObject = new MockGameObject();
+  new UnitPlastic(unit, count, obj);
 });
 
 it("getOne (plastic)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "unit:base/infantry",
-    owningPlayerSlot: 1,
+    owningPlayerSlot: 2,
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
-  expect(entry?.unit).toBe("infantry");
-  expect(entry?.count).toBe(1);
-  expect(entry?.owningPlayerSlot).toBe(1);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
+  expect(entry?.getCount()).toBe(1);
+  expect(entry?.getHex()).toBe("<0,0,0>");
+  expect(entry?.getObj()).toBe(obj);
+  expect(entry?.getOwningPlayerSlot()).toBe(2);
+  expect(entry?.getPlanetClosest()).toBeUndefined();
+  expect(entry?.getPlanetExact()).toBeUndefined();
+  expect(entry?.getUnit()).toBe("infantry");
 });
 
 it("getOne (unknown)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "unit:base/__unknown__",
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
   expect(entry).toBeUndefined();
 });
 
@@ -29,7 +40,7 @@ it("getOne (invalid nsid, match enough to check)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "unit:@@@",
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
   expect(entry).toBeUndefined();
 });
 
@@ -37,44 +48,60 @@ it("getOne (fighter-1 token)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "token:base/fighter-1",
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
-  expect(entry?.unit).toBe("fighter");
-  expect(entry?.count).toBe(1);
-  expect(entry?.owningPlayerSlot).toBe(-1);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
+  expect(entry?.getCount()).toBe(1);
+  expect(entry?.getHex()).toBe("<0,0,0>");
+  expect(entry?.getObj()).toBe(obj);
+  expect(entry?.getOwningPlayerSlot()).toBe(-1);
+  expect(entry?.getPlanetClosest()).toBeUndefined();
+  expect(entry?.getPlanetExact()).toBeUndefined();
+  expect(entry?.getUnit()).toBe("fighter");
 });
 
 it("getOne (fighter-3 token)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "token:base/fighter-3",
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
-  expect(entry?.unit).toBe("fighter");
-  expect(entry?.count).toBe(3);
-  expect(entry?.owningPlayerSlot).toBe(-1);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
+  expect(entry?.getCount()).toBe(3);
+  expect(entry?.getHex()).toBe("<0,0,0>");
+  expect(entry?.getObj()).toBe(obj);
+  expect(entry?.getOwningPlayerSlot()).toBe(-1);
+  expect(entry?.getPlanetClosest()).toBeUndefined();
+  expect(entry?.getPlanetExact()).toBeUndefined();
+  expect(entry?.getUnit()).toBe("fighter");
 });
 
 it("getOne (infantry-1 token)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "token:base/infantry-1",
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
-  expect(entry?.unit).toBe("infantry");
-  expect(entry?.count).toBe(1);
-  expect(entry?.owningPlayerSlot).toBe(-1);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
+  expect(entry?.getCount()).toBe(1);
+  expect(entry?.getHex()).toBe("<0,0,0>");
+  expect(entry?.getObj()).toBe(obj);
+  expect(entry?.getOwningPlayerSlot()).toBe(-1);
+  expect(entry?.getPlanetClosest()).toBeUndefined();
+  expect(entry?.getPlanetExact()).toBeUndefined();
+  expect(entry?.getUnit()).toBe("infantry");
 });
 
 it("getOne (infantry-3 token)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "token:base/infantry-3",
   });
-  const entry: UnitPlasticEntry | undefined = new UnitPlastic().getOne(obj);
-  expect(entry?.unit).toBe("infantry");
-  expect(entry?.count).toBe(3);
-  expect(entry?.owningPlayerSlot).toBe(-1);
+  const entry: UnitPlastic | undefined = UnitPlastic.getOne(obj);
+  expect(entry?.getCount()).toBe(3);
+  expect(entry?.getHex()).toBe("<0,0,0>");
+  expect(entry?.getObj()).toBe(obj);
+  expect(entry?.getOwningPlayerSlot()).toBe(-1);
+  expect(entry?.getPlanetClosest()).toBeUndefined();
+  expect(entry?.getPlanetExact()).toBeUndefined();
+  expect(entry?.getUnit()).toBe("infantry");
 });
 
 it("getAll (empty)", () => {
-  const result: Array<UnitPlasticEntry> = new UnitPlastic().getAll();
+  const result: Array<UnitPlastic> = UnitPlastic.getAll();
   expect(result).toEqual([]);
 });
 
@@ -82,8 +109,55 @@ it("getAll (single)", () => {
   const obj: GameObject = new MockGameObject({
     templateMetadata: "unit:base/infantry",
   });
-  const result: Array<UnitPlasticEntry> = new UnitPlastic().getAll();
+  const result: Array<UnitPlastic> = UnitPlastic.getAll();
   expect(result).toHaveLength(1);
-  expect(result[0]?.unit).toBe("infantry");
-  expect(result[0]?.count).toBe(1);
+  expect(result[0]?.getUnit()).toBe("infantry");
+});
+
+it("assignOwners", () => {
+  const plastic: UnitPlastic = new UnitPlastic(
+    "infantry",
+    1,
+    new MockGameObject({ owningPlayerSlot: 2 })
+  );
+  const token: UnitPlastic = new UnitPlastic(
+    "infantry",
+    1,
+    new MockGameObject()
+  );
+  expect(plastic.getOwningPlayerSlot()).toBe(2);
+  expect(token.getOwningPlayerSlot()).toBe(-1);
+
+  UnitPlastic.assignOwners([plastic, token]);
+  expect(plastic.getOwningPlayerSlot()).toBe(2);
+  expect(token.getOwningPlayerSlot()).toBe(2);
+});
+
+it("assignPlanets", () => {
+  new MockGameObject({ templateMetadata: "tile.system:base/1" });
+  const plastic: UnitPlastic = new UnitPlastic(
+    "infantry",
+    1,
+    new MockGameObject()
+  );
+
+  // Compute a position still inside the hex, but off the planet.
+  const pos: Vector = TI4.hex.toPosition("<1,0,-1>").multiply(0.45);
+  const plasticOffPlanet: UnitPlastic = new UnitPlastic(
+    "infantry",
+    1,
+    new MockGameObject({ position: pos })
+  );
+  expect(plasticOffPlanet.getHex()).toBe("<0,0,0>");
+
+  expect(plastic.getPlanetClosest()).toBeUndefined();
+  expect(plastic.getPlanetExact()).toBeUndefined();
+  expect(plasticOffPlanet.getPlanetClosest()).toBeUndefined();
+  expect(plasticOffPlanet.getPlanetExact()).toBeUndefined();
+
+  UnitPlastic.assignPlanets([plastic, plasticOffPlanet]);
+  expect(plastic.getPlanetClosest()).toBeDefined();
+  expect(plastic.getPlanetExact()).toBeDefined();
+  expect(plasticOffPlanet.getPlanetClosest()).toBeDefined();
+  expect(plasticOffPlanet.getPlanetExact()).toBeUndefined();
 });
--- src/lib/unit-lib/unit-plastic/unit-plastic.ts
+++ src/lib/unit-lib/unit-plastic/unit-plastic.ts
@@ -1,22 +1,32 @@
-import { HexType, NSID, ParsedNSID } from "ttpg-darrell";
-import { UnitSchema, UnitType } from "../schema/unit-attrs-schema";
-import { Planet } from "lib/system-lib/planet/planet";
 import { GameObject, Vector, world } from "@tabletop-playground/api";
-import { System } from "lib/system-lib/system/system";
+import { HexType, NSID, ParsedNSID } from "ttpg-darrell";
 
-export type UnitPlasticEntry = {
-  unit: UnitType;
-  count: number;
-  obj: GameObject;
-  hex: HexType;
-  owningPlayerSlot: number;
-  planetClosest?: Planet; // set on demand
-  planetExact?: Planet; // set on demand
-};
+import { UnitSchema, UnitType } from "../schema/unit-attrs-schema";
+import { Planet } from "../../system-lib/planet/planet";
+import { System } from "../../system-lib/system/system";
 
+/**
+ * Represents a single game object corresponding to a unit plastic.
+ * It might be an anonymous (no owning player slot) cardboard token,
+ * optionally assign those to the closest same-hex owned unit plastic.
+ */
 export class UnitPlastic {
-  public getOne(obj: GameObject): UnitPlasticEntry | undefined {
-    let unit: UnitType | undefined;
+  private readonly _unit: UnitType;
+  private readonly _count: number;
+  private readonly _obj: GameObject;
+  private readonly _hex: HexType;
+  private _owningPlayerSlot: number;
+  private _planetClosest: Planet | undefined;
+  private _planetExact: Planet | undefined;
+
+  /**
+   * Convert a game object to a unit plastic entry (is it applies).
+   *
+   * @param obj
+   * @returns
+   */
+  public static getOne(obj: GameObject): UnitPlastic | undefined {
+    let unit: UnitType | undefined = undefined;
     let count: number = 1;
     let owningPlayerSlot: number = obj.getOwningPlayerSlot(); // -1 for tokens
 
@@ -48,23 +58,24 @@ export class UnitPlastic {
       }
     }
 
-    if (!unit) {
-      return undefined;
+    if (unit) {
+      return new UnitPlastic(unit, count, obj);
     }
-    return {
-      unit,
-      count,
-      obj,
-      hex: TI4.hex.fromPosition(obj.getPosition()),
-      owningPlayerSlot,
-    };
+    return undefined;
   }
 
-  getAll(): Array<UnitPlasticEntry> {
-    const result: Array<UnitPlasticEntry> = [];
+  /**
+   * Find all unit plastics on the table (not in containers).
+   * Does not assign token owners or planets, expecting the
+   * caller to prune down to relevant entries and assign those.
+   *
+   * @returns
+   */
+  public static getAll(): Array<UnitPlastic> {
+    const result: Array<UnitPlastic> = [];
     const skipContained: boolean = true;
     for (const obj of world.getAllObjects(skipContained)) {
-      const maybeEntry: UnitPlasticEntry | undefined = this.getOne(obj);
+      const maybeEntry: UnitPlastic | undefined = this.getOne(obj);
       if (maybeEntry) {
         result.push(maybeEntry);
       }
@@ -72,7 +83,52 @@ export class UnitPlastic {
     return result;
   }
 
-  assignPlanets(entries: Array<UnitPlasticEntry>): void {
+  /**
+   * Assign ownership of anonymous cardboard tokens to the closest
+   * same-hex owned unit plastic.
+   *
+   * @param entries
+   */
+  public static assignOwners(entries: Array<UnitPlastic>): void {
+    const hexToEntries: Map<HexType, Array<UnitPlastic>> = new Map();
+    for (const entry of entries) {
+      const hex: HexType = entry._hex;
+      let entries: Array<UnitPlastic> | undefined = hexToEntries.get(hex);
+      if (!entries) {
+        entries = [];
+        hexToEntries.set(hex, entries);
+      }
+      entries.push(entry);
+    }
+    for (const entries of hexToEntries.values()) {
+      for (const entry of entries) {
+        if (entry._owningPlayerSlot < 0) {
+          let bestPlayerSlot: number = -1;
+          let bestDSq: number = Number.MAX_VALUE;
+          const p0: Vector = entry._obj.getPosition();
+          for (const other of entries) {
+            if (other._owningPlayerSlot >= 0) {
+              const p1: Vector = other._obj.getPosition();
+              const dSq: number = p0.subtract(p1).magnitudeSquared();
+              if (dSq < bestDSq) {
+                bestDSq = dSq;
+                bestPlayerSlot = other._owningPlayerSlot;
+              }
+            }
+          }
+          // May still be -1 if no other player-owned units are present.
+          entry._owningPlayerSlot = bestPlayerSlot;
+        }
+      }
+    }
+  }
+
+  /**
+   * Assign planets to unit plastics, both closest and exact.
+   *
+   * @param entries
+   */
+  public static assignPlanets(entries: Array<UnitPlastic>): void {
     const systems: Array<System> = TI4.systemRegistry.getAllSystemsWithObjs();
     const hexToSystem: Map<HexType, System> = new Map();
     for (const system of systems) {
@@ -80,12 +136,50 @@ export class UnitPlastic {
       hexToSystem.set(hex, system);
     }
     for (const entry of entries) {
-      const system: System | undefined = hexToSystem.get(entry.hex);
+      const system: System | undefined = hexToSystem.get(entry._hex);
       if (system) {
-        const pos: Vector = entry.obj.getPosition();
-        entry.planetClosest = system.getPlanetClosest(pos);
-        entry.planetExact = system.getPlanetExact(pos);
+        const pos: Vector = entry._obj.getPosition();
+        entry._planetClosest = system.getPlanetClosest(pos);
+        entry._planetExact = system.getPlanetExact(pos);
       }
     }
+  }
+
+  constructor(unit: UnitType, count: number, obj: GameObject) {
+    this._unit = unit;
+    this._count = count;
+    this._obj = obj;
+    this._hex = TI4.hex.fromPosition(obj.getPosition());
+    this._owningPlayerSlot = obj.getOwningPlayerSlot();
+    this._planetClosest = undefined;
+    this._planetExact = undefined;
+  }
+
+  getCount(): number {
+    return this._count;
+  }
+
+  getHex(): HexType {
+    return this._hex;
+  }
+
+  getObj(): GameObject {
+    return this._obj;
+  }
+
+  getOwningPlayerSlot(): number {
+    return this._owningPlayerSlot;
+  }
+
+  getPlanetClosest(): Planet | undefined {
+    return this._planetClosest;
+  }
+
+  getPlanetExact(): Planet | undefined {
+    return this._planetExact;
+  }
+
+  getUnit(): UnitType {
+    return this._unit;
   }
 }
