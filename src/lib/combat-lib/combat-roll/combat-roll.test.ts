16abe49e0869433a41b679559f3345381b5f42d8
darrell <darrell.anderson@gmail.com>
2024-06-29 23:21:54 -0700

    unittest

--- src/lib/combat-lib/combat-roll/combat-roll.test.ts
+++ src/lib/combat-lib/combat-roll/combat-roll.test.ts
@@ -10,3 +10,15 @@ it("constructor", () => {
   const combatRoll: CombatRoll = new CombatRoll(params);
   expect(combatRoll.getType()).toBe("spaceCombat");
 });
+
+it("_createUnitAttrsSet", () => {
+  const params: CombatRollParams = {
+    type: "spaceCombat",
+    hex: "<0,0,0>",
+    activatingPlayerSlot: 2,
+    rollingPlayerSlot: 3,
+  };
+  const combatRoll: CombatRoll = new CombatRoll(params);
+  const unitAttrsSet = combatRoll._createUnitAttrsSet(2);
+  expect(unitAttrsSet.get("carrier")).toBeDefined();
+});
--- src/lib/combat-lib/combat-roll/combat-roll.ts
+++ src/lib/combat-lib/combat-roll/combat-roll.ts
@@ -1,4 +1,12 @@
-import { HexType } from "ttpg-darrell";
+import { Vector, world } from "@tabletop-playground/api";
+import { Find, HexType, NSID, ParsedNSID } from "ttpg-darrell";
+
+import {
+  UnitAttrsSchemaType,
+  UnitType,
+} from "lib/unit-lib/schema/unit-attrs-schema";
+import { UnitAttrsSet } from "../../unit-lib/unit-attrs-set/unit-attrs-set";
+import { UnitAttrs } from "../../unit-lib/unit-attrs/unit-attrs";
 
 export type CombatRollType =
   | "antiFighterBarrage"
@@ -17,6 +25,50 @@ export type CombatRollParams = {
 
 export class CombatRoll {
   private readonly _params: CombatRollParams;
+  private readonly _find: Find = new Find();
+
+  _createUnitAttrsSet(playerSlot: number): UnitAttrsSet {
+    const baseAttrs: Array<UnitAttrsSchemaType> =
+      TI4.unitAttrsRegistry.getAllBaseAttrs();
+    const unitAttrsSet: UnitAttrsSet = new UnitAttrsSet(baseAttrs);
+
+    // Find unit upgrade cards owned by the player.
+    const nsidNames: Array<string> = [];
+    const skipContained: boolean = true;
+    for (const obj of world.getAllObjects(skipContained)) {
+      const nsid: string = NSID.get(obj);
+      if (nsid.startsWith("card.technology.unit-upgrade")) {
+        const pos: Vector = obj.getPosition();
+        const closest: number = this._find.closestOwnedCardHolderOwner(pos);
+        if (closest === playerSlot) {
+          const parsed: ParsedNSID | undefined = NSID.parse(nsid);
+          if (parsed) {
+            const nsidName: string = parsed.nameParts.join(".");
+            nsidNames.push(nsidName);
+          }
+        }
+      }
+    }
+
+    // nsidNames sort in override order, faction override 1 then 2.
+    nsidNames.sort();
+    for (const nsidName of nsidNames) {
+      const override: UnitAttrsSchemaType | undefined =
+        TI4.unitAttrsRegistry.getOverrideAttrs(nsidName);
+      if (override) {
+        const unit: UnitType = override.unit;
+        const unitAttrs: UnitAttrs | undefined = unitAttrsSet.get(unit);
+        if (unitAttrs) {
+          unitAttrs.applyOverride(override);
+        }
+      }
+    }
+
+    // Unit modifiers.
+    // TODO XXX
+
+    return unitAttrsSet;
+  }
 
   constructor(params: CombatRollParams) {
     this._params = params;
