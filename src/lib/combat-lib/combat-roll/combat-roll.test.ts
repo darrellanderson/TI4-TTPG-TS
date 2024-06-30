26425bdcf7ab5f82014f3f14d7ff46940c05ad7f
darrell <darrell.anderson@gmail.com>
2024-06-29 23:46:21 -0700

    test coverage

--- src/lib/combat-lib/combat-roll/combat-roll.test.ts
+++ src/lib/combat-lib/combat-roll/combat-roll.test.ts
@@ -1,4 +1,6 @@
+import { MockCard, MockCardHolder } from "ttpg-mock";
 import { CombatRoll, CombatRollParams } from "./combat-roll";
+import { UnitAttrsSet } from "lib/unit-lib/unit-attrs-set/unit-attrs-set";
 
 it("constructor", () => {
   const params: CombatRollParams = {
@@ -11,7 +13,9 @@ it("constructor", () => {
   expect(combatRoll.getType()).toBe("spaceCombat");
 });
 
-it("_createUnitAttrsSet", () => {
+it("_createUnitAttrsSet (standard unit upgrade)", () => {
+  let unitAttrsSet: UnitAttrsSet;
+  new MockCardHolder({ owningPlayerSlot: 2 });
   const params: CombatRollParams = {
     type: "spaceCombat",
     hex: "<0,0,0>",
@@ -19,6 +23,10 @@ it("_createUnitAttrsSet", () => {
     rollingPlayerSlot: 3,
   };
   const combatRoll: CombatRoll = new CombatRoll(params);
-  const unitAttrsSet = combatRoll._createUnitAttrsSet(2);
-  expect(unitAttrsSet.get("carrier")).toBeDefined();
+  unitAttrsSet = combatRoll._createUnitAttrsSet(2);
+  expect(unitAttrsSet.get("carrier")?.getName()).toBe("Carrier");
+
+  MockCard.simple("card.technology.unit-upgrade:base/carrier-2");
+  unitAttrsSet = combatRoll._createUnitAttrsSet(2);
+  expect(unitAttrsSet.get("carrier")?.getName()).toBe("Carrier II");
 });
