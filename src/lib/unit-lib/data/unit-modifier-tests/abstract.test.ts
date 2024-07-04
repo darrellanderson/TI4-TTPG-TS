694a7dd4879c9e42fd8cb960d5f2bb4d6c1a085c
Darrell <darrell.anderson@gmail.com>
2024-07-04 10:23:33 -0700

    verify placeObjects

--- src/lib/unit-lib/data/unit-modifier-tests/abstract.test.ts
+++ src/lib/unit-lib/data/unit-modifier-tests/abstract.test.ts
@@ -2,8 +2,9 @@
  * Utility functions to simplify unit modifier tests.
  * Use the ".test.ts" naming to prevent including in the mod build.
  */
-import { Vector } from "@tabletop-playground/api";
+import { GameObject, Vector } from "@tabletop-playground/api";
 import { MockCard, MockCardHolder, MockGameObject } from "ttpg-mock";
+import { Find } from "ttpg-darrell";
 import { UnitType } from "../../schema/unit-attrs-schema";
 
 export const SELF: number = 1;
@@ -13,11 +14,11 @@ export function placeGameObjects(params: {
   systemNsid?: string; // default is tile 1
   self?: Array<string>;
   selfUnits?: Map<UnitType, number>;
-  selfUnitsSpace?: Map<UnitType, number>; // in hex, not over planet
+  selfUnitsOffPlanet?: Map<UnitType, number>; // in hex, not over planet
   selfUnitsAdj?: Map<UnitType, number>;
   opponent?: Array<string>;
   opponentUnits?: Map<UnitType, number>;
-  opponentUnitsSpace?: Map<UnitType, number>;
+  opponentUnitsOffPlanet?: Map<UnitType, number>;
   opponentUnitsAdj?: Map<UnitType, number>;
 }) {
   // Player areas.
@@ -70,7 +71,7 @@ export function placeGameObjects(params: {
       });
     }
   }
-  for (const [unit, count] of params.selfUnitsSpace ?? []) {
+  for (const [unit, count] of params.selfUnitsOffPlanet ?? []) {
     const source: string = unit === "mech" ? "pok" : "base";
     for (let i = 0; i < count; i++) {
       MockGameObject.simple(`unit:${source}/${unit}`, {
@@ -88,7 +89,7 @@ export function placeGameObjects(params: {
       });
     }
   }
-  for (const [unit, count] of params.opponentUnitsSpace ?? []) {
+  for (const [unit, count] of params.opponentUnitsOffPlanet ?? []) {
     const source: string = unit === "mech" ? "pok" : "base";
     for (let i = 0; i < count; i++) {
       MockGameObject.simple(`unit:${source}/${unit}`, {
@@ -119,4 +120,61 @@ export function placeGameObjects(params: {
 
 it("placeGameObjects (empty)", () => {
   placeGameObjects({});
+});
+
+it("placeGameObjects (all)", () => {
+  placeGameObjects({
+    systemNsid: "tile.system:base/3",
+    self: ["card.action:base/direct-hit"],
+    selfUnits: new Map([
+      ["carrier", 1],
+      ["fighter", 1],
+    ]),
+    selfUnitsOffPlanet: new Map([["mech", 1]]),
+    selfUnitsAdj: new Map([["destroyer", 1]]),
+    opponent: ["card.action:base/sabotage"],
+    opponentUnits: new Map([["dreadnought", 1]]),
+    opponentUnitsOffPlanet: new Map([["infantry", 1]]),
+    opponentUnitsAdj: new Map([["cruiser", 1]]),
+  });
+
+  const find = new Find();
+  let obj: GameObject | undefined;
+
+  obj = find.findGameObject("tile.system:base/3");
+  expect(obj?.getPosition().toString()).toEqual("(X=0,Y=0,Z=0)");
+
+  obj = find.findGameObject("card.action:base/direct-hit");
+  expect(obj?.getPosition().toString()).toEqual("(X=100,Y=0,Z=0)");
+
+  obj = find.findGameObject("unit:base/carrier");
+  expect(obj?.getPosition().toString()).toEqual("(X=0,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(SELF);
+
+  obj = find.findGameObject("unit:base/fighter");
+  expect(obj?.getPosition().toString()).toEqual("(X=0,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(SELF);
+
+  obj = find.findGameObject("unit:pok/mech");
+  expect(obj?.getPosition().toString()).toEqual("(X=6.754,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(SELF);
+
+  obj = find.findGameObject("unit:base/destroyer");
+  expect(obj?.getPosition().toString()).toEqual("(X=15.01,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(SELF);
+
+  obj = find.findGameObject("card.action:base/sabotage");
+  expect(obj?.getPosition().toString()).toEqual("(X=-100,Y=0,Z=0)");
+
+  obj = find.findGameObject("unit:base/dreadnought");
+  expect(obj?.getPosition().toString()).toEqual("(X=0,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(OPPONENT);
+
+  obj = find.findGameObject("unit:base/infantry");
+  expect(obj?.getPosition().toString()).toEqual("(X=6.754,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(OPPONENT);
+
+  obj = find.findGameObject("unit:base/cruiser");
+  expect(obj?.getPosition().toString()).toEqual("(X=15.01,Y=0,Z=0)");
+  expect(obj?.getOwningPlayerSlot()).toEqual(OPPONENT);
 });
