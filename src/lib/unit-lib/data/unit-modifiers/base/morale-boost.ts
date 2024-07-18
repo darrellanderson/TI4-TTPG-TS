c58cca0645ea7c2e9a7685af5339944a02749179
darrell <darrell.anderson@gmail.com>
2024-07-18 00:33:26 -0700

    boilerplate

---
+++ src/lib/unit-lib/data/unit-modifiers/base/morale-boost.ts
@@ -0,0 +1,18 @@
+import { CombatAttrs } from "../../../unit-attrs/combat-attrs";
+import {
+  CombatRoll,
+  CombatRollType,
+} from "../../../../combat-lib/combat-roll/combat-roll";
+import { UnitModifierSchemaType } from "../../../schema/unit-modifier-schema";
+
+export const X: UnitModifierSchemaType = {
+  name: "",
+  description: "",
+  owner: "",
+  priority: "",
+  triggers: [],
+  applies: (combatRoll: CombatRoll): boolean => {
+    return false;
+  },
+  apply: (combatRoll: CombatRoll): void => {},
+};
