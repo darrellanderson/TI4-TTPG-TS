19ef07ae70d10b4221f568aaff4e517c3ca3af07
darrell <darrell.anderson@gmail.com>
2024-07-23 22:35:37 -0700

    test coverage

--- src/lib/faction-lib/faction/faction.test.ts
+++ src/lib/faction-lib/faction/faction.test.ts
@@ -23,12 +23,25 @@ it("constructor", () => {
   };
   const faction: Faction = new Faction("my-source", schema);
 
+  expect(faction.getAbbr()).toEqual("my-abbr");
   expect(faction.getAbilityNsids()).toEqual([
     "faction-ability:my-source/my-ability",
   ]);
+  expect(faction.getAgentNsids()).toEqual([
+    "card.leader.agent:my-source/my-agent",
+  ]);
+  expect(faction.getCommanderNsids()).toEqual([
+    "card.leader.commander:my-source/my-commander",
+  ]);
   expect(faction.getCommodities()).toEqual(1);
+  expect(faction.getHeroNsids()).toEqual([
+    "card.leader.hero:my-source/my-hero",
+  ]);
   expect(faction.getHomeSurrogateTileNumber()).toEqual(-1);
   expect(faction.getHomeSystemTileNumber()).toEqual(2);
+  expect(faction.getMechNsids()).toEqual([
+    "card.leader.mech:my-source/my-mech",
+  ]);
   expect(faction.getName()).toEqual("my-name");
   expect(faction.getNsid()).toEqual("faction:my-source/my-nsid-name");
   expect(faction.getPromissoryNsids()).toEqual([
@@ -48,6 +61,7 @@ it("constructor (base source get pok mech)", () => {
     abilities: ["my-ability"],
     commodities: 1,
     home: 2,
+    homeSurrogate: 3,
     leaders: {
       agents: ["my-agent"],
       commanders: ["my-commander"],
@@ -62,6 +76,7 @@ it("constructor (base source get pok mech)", () => {
   };
   const faction: Faction = new Faction("base", schema);
 
+  expect(faction.getHomeSurrogateTileNumber()).toEqual(3);
   expect(faction.getUnitOverrideNsids()).toEqual([
     "unit:base/my-unit-override",
     "card.leader.mech:pok/my-mech",
