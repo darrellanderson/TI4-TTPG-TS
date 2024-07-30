9f057726f71957994f4c03af139b6470f67a82ce
Darrell <darrell.anderson@gmail.com>
2024-07-30 09:19:49 -0700

    test coverage

--- src/lib/recycle-lib/handlers/strategy-card/recycle-strategy-card.test.ts
+++ src/lib/recycle-lib/handlers/strategy-card/recycle-strategy-card.test.ts
@@ -12,8 +12,55 @@ it("recycle", () => {
     templateMetadata: "mat:base/strategy",
     snapPoints: [new MockSnapPoint()],
   });
-
   const recycle: GarbageHandler = new RecycleStrategyCard();
   expect(recycle.canRecycle(obj)).toBe(true);
   expect(recycle.recycle(obj)).toBe(true);
+});
+
+it("recycle (not a strategy card)", () => {
+  const obj: GameObject = MockGameObject.simple("other:base/other");
+  const recycle: GarbageHandler = new RecycleStrategyCard();
+  expect(recycle.canRecycle(obj)).toBe(false);
+  expect(recycle.recycle(obj)).toBe(false);
+});
+
+it("recycle (invalid nsid)", () => {
+  const obj: GameObject = MockGameObject.simple("tile.strategy:");
+  const recycle: GarbageHandler = new RecycleStrategyCard();
+  expect(recycle.canRecycle(obj)).toBe(true);
+  expect(recycle.recycle(obj)).toBe(false);
+});
+
+it("recycle (unknown card)", () => {
+  const obj: GameObject = MockGameObject.simple(
+    "tile.strategy:base/__unknown__"
+  );
+  new MockGameObject({
+    templateMetadata: "mat:base/strategy",
+    snapPoints: [new MockSnapPoint()],
+  });
+  const recycle: GarbageHandler = new RecycleStrategyCard();
+  expect(recycle.canRecycle(obj)).toBe(true);
+  expect(recycle.recycle(obj)).toBe(false);
+});
+
+it("recycle (missing mat)", () => {
+  const obj: GameObject = MockGameObject.simple(
+    "tile.strategy:base/leadership"
+  );
+  const recycle: GarbageHandler = new RecycleStrategyCard();
+  expect(recycle.canRecycle(obj)).toBe(true);
+  expect(recycle.recycle(obj)).toBe(false);
+});
+
+it("recycle (missing snap point)", () => {
+  const obj: GameObject = MockGameObject.simple(
+    "tile.strategy:base/leadership"
+  );
+  new MockGameObject({
+    templateMetadata: "mat:base/strategy",
+  });
+  const recycle: GarbageHandler = new RecycleStrategyCard();
+  expect(recycle.canRecycle(obj)).toBe(true);
+  expect(recycle.recycle(obj)).toBe(false);
 });
