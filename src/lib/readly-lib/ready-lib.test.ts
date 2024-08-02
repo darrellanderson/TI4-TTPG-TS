1160c2f7805ebc3f556b5347e2133abb9bc04293
darrell <darrell.anderson@gmail.com>
2024-08-01 20:19:00 -0700

    ready-lib

--- src/lib/readly-lib/ready-lib.test.ts
+++ src/lib/readly-lib/ready-lib.test.ts
@@ -1,10 +1,110 @@
-import { GameObject } from "@tabletop-playground/api";
+import { Card, GameObject } from "@tabletop-playground/api";
 import { ReadyLib } from "./ready-lib";
-import { MockGameObject } from "ttpg-mock";
+import { MockCard, MockGameObject, MockSnapPoint } from "ttpg-mock";
 import { Facing } from "ttpg-darrell";
 
 it("constructor", () => {
   new ReadyLib();
+});
+
+it("ready leader-agent card", () => {
+  const faceUp: Card = MockCard.simple("card.leader.agent:my-source/my-name");
+  const faceDown: Card = MockCard.simple(
+    "card.leader.agent:my-source/my-name",
+    { isFaceUp: false }
+  );
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false);
+
+  new ReadyLib().readyAll();
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(true);
+});
+
+it("ready legendary-planet card", () => {
+  const faceUp: Card = MockCard.simple(
+    "card.legendary-planet:my-source/my-name"
+  );
+  const faceDown: Card = MockCard.simple(
+    "card.legendary-planet:my-source/my-name",
+    { isFaceUp: false }
+  );
+  const deck: Card = MockCard.simple(
+    "card.legendary-planet:my-source/my-name",
+    {
+      isFaceUp: false,
+      snappedToPoint: new MockSnapPoint({ tags: ["deck-legendary-planet"] }),
+    }
+  );
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false);
+  expect(Facing.isFaceUp(deck)).toBe(false);
+
+  new ReadyLib().readyAll();
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(true);
+  expect(Facing.isFaceUp(deck)).toBe(false);
+});
+
+it("ready planet card", () => {
+  const faceUp: Card = MockCard.simple("card.planet:my-source/my-name");
+  const faceDown: Card = MockCard.simple("card.planet:my-source/my-name", {
+    isFaceUp: false,
+  });
+  const deck: Card = MockCard.simple("card.planet:my-source/my-name", {
+    isFaceUp: false,
+    snappedToPoint: new MockSnapPoint({ tags: ["deck-planet"] }),
+  });
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false);
+  expect(Facing.isFaceUp(deck)).toBe(false);
+
+  new ReadyLib().readyAll();
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(true);
+  expect(Facing.isFaceUp(deck)).toBe(false);
+});
+
+it("ready relic card", () => {
+  const faceUp: Card = MockCard.simple("card.relic:my-source/my-name");
+  const faceDown: Card = MockCard.simple("card.relic:my-source/my-name", {
+    isFaceUp: false,
+  });
+  const deck: Card = MockCard.simple("card.relic:my-source/my-name", {
+    isFaceUp: false,
+    snappedToPoint: new MockSnapPoint({ tags: ["deck-relic"] }),
+  });
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false);
+  expect(Facing.isFaceUp(deck)).toBe(false);
+
+  new ReadyLib().readyAll();
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(true);
+  expect(Facing.isFaceUp(deck)).toBe(false);
+});
+
+it("ready technology card", () => {
+  const faceUp: Card = MockCard.simple("card.technology.red:my-source/my-name");
+  const faceDown: Card = MockCard.simple(
+    "card.technology.red:my-source/my-name",
+    { isFaceUp: false }
+  );
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false);
+
+  new ReadyLib().readyAll();
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(true);
 });
 
 it("ready units", () => {
@@ -23,4 +123,24 @@ it("ready units", () => {
 
   expect(Facing.isFaceUp(faceUp)).toBe(true);
   expect(Facing.isFaceUp(faceDown)).toBe(true);
+});
+
+it("ready units (held)", () => {
+  const faceUp: GameObject = new MockGameObject({
+    templateMetadata: "unit:base/dreadnought",
+  });
+  const faceDown: GameObject = new MockGameObject({
+    templateMetadata: "unit:base/dreadnought",
+    rotation: [0, 0, 180],
+    isHeld: true,
+  });
+
+  expect(faceDown.isHeld()).toBe(true);
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false);
+
+  new ReadyLib().readyAll();
+
+  expect(Facing.isFaceUp(faceUp)).toBe(true);
+  expect(Facing.isFaceUp(faceDown)).toBe(false); // held, not readied
 });
--- src/lib/readly-lib/ready-lib.ts
+++ src/lib/readly-lib/ready-lib.ts
@@ -1,31 +1,49 @@
 import { Card, world } from "@tabletop-playground/api";
-import { Facing, NSID } from "ttpg-darrell";
+import { CardUtil, Facing, NSID } from "ttpg-darrell";
 
 export class ReadyLib {
+  private readonly _cardUtil: CardUtil = new CardUtil();
   private readonly _readyNsidPrefixes: Array<string> = [
-    "unit:",
-    "card.planet:",
     "card.leader.agent",
     "card.legendary-planet:",
+    "card.planet:",
     "card.relic:",
     "card.technology",
+    "unit:",
   ];
 
   readyAll() {
-    const skipContained = true;
+    const skipContained: boolean = true;
     for (const obj of world.getAllObjects(skipContained)) {
       const nsid: string = NSID.get(obj);
 
-      if (obj.isHeld()) {
-        continue;
-      }
-
-      if (obj instanceof Card && (obj.isInHolder() || obj.getStackSize() > 1)) {
-        continue;
-      }
-
       if (Facing.isFaceUp(obj)) {
-        continue;
+        continue; // already face up
+      }
+
+      if (obj.isHeld()) {
+        continue; // held
+      }
+
+      if (obj instanceof Card) {
+        const rejectSnapPointTags: Array<string> = [];
+        if (nsid.startsWith("card.legendary-planet:")) {
+          rejectSnapPointTags[0] = "deck-legendary-planet";
+        } else if (nsid.startsWith("card.planet:")) {
+          rejectSnapPointTags[0] = "deck-planet";
+        } else if (nsid.startsWith("card.relic:")) {
+          rejectSnapPointTags[0] = "deck-relic";
+        }
+
+        const allowFaceDown: boolean = true;
+        const isLooseCard: boolean = this._cardUtil.isLooseCard(
+          obj,
+          allowFaceDown,
+          rejectSnapPointTags
+        );
+        if (!isLooseCard || obj.getStackSize() > 1) {
+          continue; // in deck location and/or has multiple cards
+        }
       }
 
       for (const prefix of this._readyNsidPrefixes) {
