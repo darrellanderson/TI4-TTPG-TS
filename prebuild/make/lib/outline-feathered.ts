dec50a4c12ff0a16935e423787b6bfe641368725
darrell <darrell.anderson@gmail.com>
2024-08-03 23:57:43 -0700

    progress

--- prebuild/make/lib/outline-feathered.ts
+++ prebuild/make/lib/outline-feathered.ts
@@ -1,4 +1,4 @@
-import sharp, { Sharp } from "sharp";
+import sharp, { Metadata } from "sharp";
 
 /**
  * Create an outlined version of a PNG with a feathered edge and white background.
@@ -9,15 +9,44 @@ import sharp, { Sharp } from "sharp";
  */
 export async function outlineFeatheredAsJpg(pngFilename: string) {
   const src: string = pngFilename;
-  const dst: string = pngFilename.replace(/.png$/, ".jpg");
+  const dst: string = pngFilename.replace(/.png$/, "-mask.png");
+  if (src === dst) {
+    throw new Error("src is dst???");
+  }
 
-  // Load the opaque area of the PNG.
-  const alphaMask: Buffer = await sharp(src)
-    .extractChannel("alpha")
-    .png()
+  const metadata: Metadata = await sharp(src).metadata();
+  const width: number = metadata.width || 1;
+  const height: number = metadata.height || 1;
+
+  const innerMask: Buffer = await sharp(src).extractChannel("alpha").toBuffer();
+
+  // Need two steps to remove blurred alpha.
+  const blurredOuterMask: Buffer = await sharp(src)
+    .blur(8)
+    .flatten(true)
+    .toColorspace("b-w")
     .toBuffer();
-  const outline: Buffer = await sharp(alphaMask).blur(5).toBuffer();
+  const outerMask: Buffer = await sharp(blurredOuterMask)
+    .threshold(1)
+    .unflatten()
+    .extractChannel("alpha")
+    .toBuffer();
 
-  await sharp(src).toFile(dst.replace(/.png$/, ".jpg"));
+  const mask: Buffer = await sharp(outerMask)
+    .composite([{ input: innerMask, blend: "add" }])
+    .toBuffer();
+
+  await sharp({
+    create: {
+      width,
+      height,
+      channels: 4,
+      background: { r: 255, g: 0, b: 0, alpha: 255 },
+    },
+  })
+    .composite([{ input: mask, blend: "multiply" }])
+    .png()
+    .toFile(dst);
+
   console.log(`Created: ${dst}`);
 }
