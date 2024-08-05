2e2226e51e42ff8ff7ca1a33a80bc605d3d40050
Darrell <darrell.anderson@gmail.com>
2024-08-04 17:27:54 -0700

    unit templates

--- prebuild/make/unit.ts
+++ prebuild/make/unit.ts
@@ -1,8 +1,15 @@
 /**
- * Create unit containers, assumes icons and masks are already created.
+ * Create unit templates.
+ *
+ * Assumes:
+ * - assets/Models/unit/${unit}.shared.obj (use a common texture for batch draw calls)
+ * - assets/Models/unit/${unit}.col.obj
+ * - assets/Textures/unit/shared.jpg
+ *
+ * Input: N/A
  *
  * Output:
- * - assets/Templates/containers/unit/*.json
+ * - assets/Templates/unit/${unit}.json
  */
 
 import crypto from "crypto";
@@ -11,7 +18,16 @@ import path from "path";
 
 import { SOURCE_TO_UNIT_ATTRS_DATA } from "../../src/lib/unit-lib/data/unit-attrs.data";
 import { UnitType } from "../../src/lib/unit-lib/schema/unit-attrs-schema";
-import { CONTAINER_TEMPLATE_DATA } from "./data/container.template-data";
+import { UNIT_TEMPLATE_DATA } from "./data/unit.template-data";
+
+// TODO set wrench Z to fit model
+
+function getWrenchZ(model: string): number {
+  if (!fs.existsSync(model)) {
+    throw new Error(`File not found: "${model}"`);
+  }
+  return 0;
+}
 
 const seen: Set<UnitType> = new Set();
 for (const [source, unitAttrsDataArray] of Object.entries(
@@ -24,9 +40,9 @@ for (const [source, unitAttrsDataArray] of Object.entries(
     }
     seen.add(unit);
 
-    const template = JSON.parse(JSON.stringify(CONTAINER_TEMPLATE_DATA));
+    const template = JSON.parse(JSON.stringify(UNIT_TEMPLATE_DATA));
 
-    let templateFile: string = `container/unit/${unit}.json`;
+    let templateFile: string = `unit/${unit}.json`;
 
     const guid: string = crypto
       .createHash("sha256")
@@ -37,9 +53,20 @@ for (const [source, unitAttrsDataArray] of Object.entries(
 
     template.GUID = guid;
     template.Name = `${unit}`;
-    template.Metadata = `container.unit:${source}/${unit}`;
-    template.Models[0].Texture = `icon/unit/${unit}.jpg`;
-    template.Models[0].ExtraMap = `icon/unit/${unit}-mask.png`;
+    template.Metadata = `unit:${source}/${unit}`;
+    template.Models[0].Model = `/unit/${unit}.shared.obj`;
+    template.Collision[0].Model = `/unit/${unit}.col.obj`;
+    template.Models[1].Offset.Z = getWrenchZ(
+      `assets/Models/unit/${unit}.shared.obj`
+    );
+
+    if (unit === "mech") {
+      template.Models[0].Scale = {
+        X: 1.1,
+        Y: 1.1,
+        Z: 1.1,
+      };
+    }
 
     templateFile = "./assets/Templates/" + templateFile;
     const templateDir = path.dirname(templateFile);
