505e3b5558e7ba068f3a2fac34005211ce14857e
Darrell <darrell.anderson@gmail.com>
2024-08-07 18:22:15 -0700

    tweak

--- src/obj/unit-container-obj.ts
+++ src/obj/unit-container-obj.ts
@@ -8,13 +8,15 @@ import {
 } from "@tabletop-playground/api";
 import { NSID, ParsedNSID } from "ttpg-darrell";
 
-const SCALE: number = 8;
+const SCALE: number = 4;
 
 const nsid: string = NSID.get(refObject);
 const parsed: ParsedNSID | undefined = NSID.parse(nsid);
 const unit: string = parsed?.nameParts[0] ?? "Unknown";
 
-const widget: ImageWidget = new ImageWidget().setImage(`icon/unit/${unit}.png`);
+const widget: ImageWidget = new ImageWidget()
+  .setImage(`icon/unit/${unit}.png`)
+  .setImageSize(128, 128);
 
 const ui: UIElement = new UIElement();
 ui.position = new Vector(0, 0, 1.7);
@@ -24,6 +26,6 @@ ui.widget = widget;
 
 const obj: GameObject = refObject;
 process.nextTick(() => {
-  //widget.setTintColor(obj.getPrimaryColor());
+  widget.setTintColor(obj.getPrimaryColor());
   obj.addUI(ui);
 });
