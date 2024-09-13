39234f9424541735a22ed33d4c6971b74dfdc006
darrell <darrell.anderson@gmail.com>
2024-09-13 16:10:24 -0700

    assign container colors

--- prebuild/make/container-circle-token.ts
+++ prebuild/make/container-circle-token.ts
@@ -38,6 +38,29 @@ for (const token of CLIP_CIRCLE_TOKENS) {
   template.Models[0].Texture = `icon/token/${token}.jpg`;
   template.Models[0].ExtraMap = `icon/token/circle-mask.png`;
 
+  if (token.includes("infantry")) {
+    template.Models[0].PrimaryColor = {
+      R: 255,
+      G: 141,
+      B: 58,
+    };
+    template.Models[0].UseOverrides = false;
+  } else if (token.includes("fighter")) {
+    template.Models[0].PrimaryColor = {
+      R: 55,
+      G: 149,
+      B: 255,
+    };
+    template.Models[0].UseOverrides = false;
+  } else if (token.includes("tradegood-commodity")) {
+    template.Models[0].PrimaryColor = {
+      R: 121,
+      G: 148,
+      B: 181,
+    };
+    template.Models[0].UseOverrides = false;
+  }
+
   templateFile = "./assets/Templates/" + templateFile;
   const templateDir = path.dirname(templateFile);
   const templateData = Buffer.from(JSON.stringify(template, null, 2));
--- src/obj/icon-container-obj.ts
+++ src/obj/icon-container-obj.ts
@@ -14,17 +14,23 @@ const SCALE: number = 4;
 let iconImage: string;
 const nsid: string = NSID.get(refObject);
 const parsed: ParsedNSID | undefined = NSID.parse(nsid);
+const firstPart: string = parsed?.nameParts[0] ?? "Unknown";
 if (nsid.startsWith("container.unit:")) {
-  const unit: string = parsed?.nameParts[0] ?? "Unknown";
-  iconImage = `icon/unit/${unit}.png`;
+  iconImage = `icon/unit/${firstPart}.png`;
 } else if (nsid === "container.token.command:base/generic") {
   iconImage = "icon/token/command.png";
 } else if (nsid === "container.token.control:base/generic") {
   iconImage = "icon/token/control.png";
+} else if (nsid.startsWith("container.token:")) {
+  iconImage = `icon/token/${firstPart}.png`;
 } else {
   throw new Error(`Unknown NSID: ${nsid}`);
 }
-const outlineImage: string = iconImage.replace(".png", "-outline-only.png");
+
+let outlineImage: string = iconImage.replace(".png", "-outline-only.png");
+if (nsid.startsWith("container.token:")) {
+  outlineImage = "icon/token/circle-outline-only.png";
+}
 
 const widget: ImageWidget = new ImageWidget()
   .setImage(iconImage)
@@ -36,6 +42,10 @@ ui.position = new Vector(0, 0, -1.7);
 ui.presentationStyle = UIPresentationStyle.ViewAligned;
 ui.scale = 1 / SCALE;
 ui.widget = widget;
+
+if (firstPart.endsWith("-1")) {
+  ui.scale = ui.scale * 0.75;
+}
 
 const outline: UIElement = new UIElement();
 outline.position = ui.position;
