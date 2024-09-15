71eca8b3ddc2db41919fe238932ba34ac50c1c1e
darrell <darrell.anderson@gmail.com>
2024-09-14 23:03:51 -0700

    wire table containers

--- src/setup/layout/layout-table-containers/layout-table-containers.ts
+++ src/setup/layout/layout-table-containers/layout-table-containers.ts
@@ -1,4 +1,7 @@
 import { LayoutObjects } from "ttpg-darrell";
+import { LayoutConfig } from "../layout-config";
+import { LayoutSystemContainer } from "./layout-system-container";
+import { LayoutExplorationContainer } from "./layout-exploration-container";
 
 /**
  * Misc containers.
@@ -7,7 +10,13 @@ export class LayoutTableContainers {
   private readonly _layout: LayoutObjects;
 
   constructor() {
-    this._layout = new LayoutObjects();
+    this._layout = new LayoutObjects()
+      .setChildDistance(LayoutConfig.spacing)
+      .setIsVertical(true);
+
+    this._layout
+      .add(new LayoutSystemContainer().getLayout())
+      .add(new LayoutExplorationContainer().getLayout());
   }
 
   public getLayout() {
