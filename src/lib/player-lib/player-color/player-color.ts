a9de5b9f190c096e2cfc58c09225c5b452c47dcf
darrell <darrell.anderson@gmail.com>
2024-10-04 22:09:27 -0700

    progress

--- src/lib/player-lib/player-color/player-color.ts
+++ src/lib/player-lib/player-color/player-color.ts
@@ -3,14 +3,17 @@ import { ColorLib, ColorsType } from "ttpg-darrell";
 
 export class PlayerColor {
   private static readonly KEY: string = "player-colors";
+  private readonly _colorLib: ColorLib = new ColorLib();
 
   setSlotColor(slot: number, colorName: string, colorHex?: string): void {
-    const colorLib: ColorLib = new ColorLib();
-    let colorsType: ColorsType = colorLib.getColorsByNameOrThrow(colorName, 0);
+    let colorsType: ColorsType = this._colorLib.getColorsByNameOrThrow(
+      colorName,
+      0
+    );
     if (colorHex) {
-      colorsType = colorLib.getColorsByTargetOrThrow(colorHex);
+      colorsType = this._colorLib.getColorsByTargetOrThrow(colorHex);
     }
-    const slotColor: Color = colorLib.parseColorOrThrow(colorsType.slot);
+    const slotColor: Color = this._colorLib.parseColorOrThrow(colorsType.slot);
     world.setSlotColor(slot, slotColor);
 
     let json: string = world.getSavedData(PlayerColor.KEY);
@@ -30,11 +33,37 @@ export class PlayerColor {
 
   getSlotColorName(slot: number): string | undefined {
     const json: string = world.getSavedData(PlayerColor.KEY);
-    if (!json || json.length === 0) {
-      return undefined;
+    if (json && json.length > 0) {
+      const parsed = JSON.parse(json);
+      const colorName: string | undefined = parsed[slot]?.name;
+      if (colorName) {
+        return colorName;
+      }
     }
-    const parsed = JSON.parse(json);
-    const colorName: string | undefined = parsed[slot]?.name;
-    return colorName;
+    return undefined;
+  }
+
+  getSlotWidgetColor(slot: number): Color {
+    const json: string = world.getSavedData(PlayerColor.KEY);
+    if (json && json.length > 0) {
+      const parsed = JSON.parse(json);
+      const colorHex: string | undefined = parsed[slot]?.widget;
+      if (colorHex) {
+        return this._colorLib.parseColorOrThrow(colorHex);
+      }
+    }
+    return new Color(1, 1, 1, 1);
+  }
+
+  getSlotPlasticColor(slot: number): Color {
+    const json: string = world.getSavedData(PlayerColor.KEY);
+    if (json && json.length > 0) {
+      const parsed = JSON.parse(json);
+      const colorHex: string | undefined = parsed[slot]?.plastic;
+      if (colorHex) {
+        return this._colorLib.parseColorOrThrow(colorHex);
+      }
+    }
+    return new Color(1, 1, 1, 1);
   }
 }
