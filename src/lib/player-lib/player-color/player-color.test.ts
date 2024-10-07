e171b00824e10e91c46f8ba12247a0546612539e
darrell <darrell.anderson@gmail.com>
2024-10-06 23:15:07 -0700

    test coverage

--- src/lib/player-lib/player-color/player-color.test.ts
+++ src/lib/player-lib/player-color/player-color.test.ts
@@ -1,30 +1,39 @@
+import { Color } from "@tabletop-playground/api";
 import { PlayerColor } from "./player-color";
 
 it("set/get", () => {
   const playerColor: PlayerColor = new PlayerColor("@test/test");
   let colorName: string | undefined;
+  let plasticColor: Color | undefined;
+  let widgetColor: Color | undefined;
 
   colorName = playerColor.getSlotColorName(10);
+  plasticColor = playerColor.getSlotPlasticColor(10);
+  widgetColor = playerColor.getSlotWidgetColor(10);
+
   expect(colorName).toBeUndefined();
-  expect(playerColor.getSlotWidgetColor(10)).toBeUndefined();
-  expect(playerColor.getSlotPlasticColor(10)).toBeUndefined();
+  expect(plasticColor).toBeUndefined();
+  expect(widgetColor).toBeUndefined();
 
   expect(() => {
     playerColor.getSlotColorNameOrThrow(10);
   }).toThrow();
   expect(() => {
-    playerColor.getSlotWidgetColorOrThrow(10);
+    playerColor.getSlotPlasticColorOrThrow(10);
   }).toThrow();
   expect(() => {
-    playerColor.getSlotPlasticColorOrThrow(10);
+    playerColor.getSlotWidgetColorOrThrow(10);
   }).toThrow();
 
   playerColor.setSlotColor(10, "green");
 
-  colorName = playerColor.getSlotColorName(10);
+  colorName = playerColor.getSlotColorNameOrThrow(10);
+  plasticColor = playerColor.getSlotPlasticColorOrThrow(10);
+  widgetColor = playerColor.getSlotWidgetColorOrThrow(10);
+
   expect(colorName).toBe("green");
-  expect(playerColor.getSlotWidgetColor(10)?.toHex()).toBe("02B615FF");
-  expect(playerColor.getSlotPlasticColor(10)?.toHex()).toBe("06CC44FF");
+  expect(plasticColor?.toHex()).toBe("06CC44FF");
+  expect(widgetColor?.toHex()).toBe("02B615FF");
 
   playerColor.setSlotColor(10, "yellow", "#FFFF00");
   colorName = playerColor.getSlotColorName(10);
