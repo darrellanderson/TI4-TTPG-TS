750bceb4d4e50b2cbbc3f0439ee30458b3c03917
Darrell <darrell.anderson@gmail.com>
2024-10-19 17:56:51 -0700

    progress

--- src/lib/faction-lib/unpack/unpack-home-system/unpack-home-system.ts
+++ src/lib/faction-lib/unpack/unpack-home-system/unpack-home-system.ts
@@ -1,7 +1,7 @@
-import { Find } from "ttpg-darrell";
+import { DeletedItemsContainer, Find } from "ttpg-darrell";
 import { Faction } from "../../faction/faction";
 import { AbstractUnpack } from "../abstract-unpack/abstract-unpack";
-import { GameObject } from "@tabletop-playground/api";
+import { Container, GameObject, Vector } from "@tabletop-playground/api";
 
 export class UnpackHomeSystem extends AbstractUnpack {
   private readonly _find: Find = new Find();
@@ -14,36 +14,65 @@ export class UnpackHomeSystem extends AbstractUnpack {
     const home: number = this.getFaction().getHomeSystemTileNumber();
     const surrogate: number = this.getFaction().getHomeSurrogateTileNumber();
 
-    const homeNsid: string | undefined =
+    const homeSystemTileNsid: string | undefined =
       TI4.systemRegistry.tileNumberToSystemTileObjNsid(home);
-    const surrogateNsid: string | undefined =
+    const surrogateSystemTileNsid: string | undefined =
       TI4.systemRegistry.tileNumberToSystemTileObjNsid(surrogate);
 
-    const nsid: string = "tile.system:base/000";
+    // Get generic home system tile.
+    const genericHomeSystemTileNsid: string = "tile.system:base/000";
     const skipContained: boolean = true;
-    const generic: GameObject | undefined = this._find.findGameObject(
-      nsid,
-      this.getPlayerSlot(),
-      skipContained
-    );
-    if (!generic) {
+    const genericHomeSystemTile: GameObject | undefined =
+      this._find.findGameObject(
+        genericHomeSystemTileNsid,
+        this.getPlayerSlot(),
+        skipContained
+      );
+    if (!genericHomeSystemTile) {
       throw new Error(
         `Could not find generic home system tile for ${this.getPlayerSlot()}`
       );
     }
 
-    let pos = generic.getPosition().add([0, 0, 10]);
+    // Get faction sheet (for surrogate, but always do for code exercise in all cases).
+    const factionSheetNsid: string = this.getFaction().getFactionSheetNsid();
+    const factionSheet: GameObject | undefined = this._find.findGameObject(
+      factionSheetNsid,
+      this.getPlayerSlot(),
+      skipContained
+    );
+    if (!factionSheet) {
+      throw new Error(
+        `Could not find faction sheet for ${this.getPlayerSlot()}`
+      );
+    }
 
-    if (surrogateNsid) {
+    DeletedItemsContainer.destroyWithoutCopying(genericHomeSystemTile);
+
+    let pos: Vector = genericHomeSystemTile.getPosition().add([0, 0, 10]);
+    let surrogatePos: Vector = new Vector(0, 0, 0);
+
+    // Place surrogate, if any.
+    if (surrogateSystemTileNsid) {
+      surrogatePos = pos;
+      pos = factionSheet.getPosition().add([0, 0, 10]);
       const surrogateObj: GameObject | undefined = this._find.findGameObject(
-        surrogateNsid,
+        surrogateSystemTileNsid,
         this.getPlayerSlot(),
         skipContained
       );
       if (surrogateObj) {
-        pos = surrogateObj.getPosition().add([0, 0, 10]);
+        const container: Container | undefined = surrogateObj.getContainer();
+        if (container) {
+          container.take(surrogateObj, surrogatePos);
+        } else {
+          surrogateObj.setPosition(surrogatePos);
+        }
+        surrogateObj.snapToGround();
       }
     }
+
+    // Place home system tile.
     // TODO XXX
   }
 
