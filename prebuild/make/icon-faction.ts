3e7834b5bd9d23fd76025608bad6867eea11dce8
Darrell <darrell.anderson@gmail.com>
2024-10-22 17:43:50 -0700

    generate faction icon outlines

--- prebuild/make/icon-faction.ts
+++ prebuild/make/icon-faction.ts
@@ -11,21 +11,31 @@ import fs from "fs";
 import path from "path";
 
 import { SOURCE_TO_FACTION_DATA } from "../../src/lib/faction-lib/data/faction.data";
+import { outlineOnly } from "./lib/outline-mask";
+import { FactionSchemaType } from "../../src/lib/faction-lib/schema/faction-schema";
+
+async function processFaction(factionData: FactionSchemaType) {
+  let srcNsidName = factionData.nsidName;
+  if (srcNsidName.includes("keleres")) {
+    srcNsidName = "keleres";
+  }
+  const src: string = `./prebuild/icon/faction/${srcNsidName}.png`;
+  const dst: string = `./assets/Textures/icon/faction/${factionData.nsidName}.png`;
+
+  if (!fs.existsSync(src)) {
+    throw new Error(`File not found: "${src}"`);
+  }
+
+  const dstDir: string = path.dirname(dst);
+  fs.mkdirSync(dstDir, { recursive: true });
+  fs.cpSync(src, dst);
+
+  // Version which is only the outline.
+  await outlineOnly(dst);
+}
+
 for (const factionDataArray of Object.values(SOURCE_TO_FACTION_DATA)) {
   for (const factionData of factionDataArray) {
-    let srcNsidName = factionData.nsidName;
-    if (srcNsidName.includes("keleres")) {
-      srcNsidName = "keleres";
-    }
-    const src: string = `./prebuild/icon/faction/${srcNsidName}.png`;
-    const dst: string = `./assets/Textures/icon/faction/${factionData.nsidName}.png`;
-
-    if (!fs.existsSync(src)) {
-      throw new Error(`File not found: "${src}"`);
-    }
-
-    const dstDir: string = path.dirname(dst);
-    fs.mkdirSync(dstDir, { recursive: true });
-    fs.cpSync(src, dst);
+    processFaction(factionData);
   }
 }
