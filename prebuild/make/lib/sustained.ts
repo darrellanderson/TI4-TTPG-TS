46746ce4dbd9164b74ba003f7103094585d375b7
darrell <darrell.anderson@gmail.com>
2024-12-12 20:41:03 -0800

    prebuild image progress

--- prebuild/make/lib/sustained.ts
+++ prebuild/make/lib/sustained.ts
@@ -2,6 +2,87 @@ import sharp, { Metadata } from "sharp";
 
 const OUTLINE_WIDTH: number = 5;
 
+/**
+ * Get a solid red version of the tranparent png input.
+ *
+ * @param input
+ */
+async function redMask(input: Buffer): Promise<Buffer> {
+  const metadata: Metadata = await sharp(input).metadata();
+  const width: number = metadata.width || 1;
+  const height: number = metadata.height || 1;
+
+  const red: Buffer = await sharp({
+    create: {
+      width,
+      height,
+      channels: 4,
+      background: { r: 255, g: 0, b: 0, alpha: 255 },
+    },
+  })
+    .png()
+    .toBuffer();
+
+  const redMask: Buffer = await sharp(red)
+    .composite([{ input, blend: "dest-in" }])
+    .png()
+    .toBuffer();
+  return redMask;
+}
+
+/**
+ * Get image with a solid white outline with transparent background.
+ */
+async function whiteOutlinedMask(input: Buffer): Promise<Buffer> {
+  const metadata: Metadata = await sharp(input).metadata();
+  const width: number = metadata.width || 1;
+  const height: number = metadata.height || 1;
+
+  const white: Buffer = await sharp({
+    create: {
+      width,
+      height,
+      channels: 4,
+      background: { r: 255, g: 255, b: 255, alpha: 255 },
+    },
+  })
+    .png()
+    .toBuffer();
+
+  const fuzzyOutline: Buffer = await sharp(input)
+    .blur(OUTLINE_WIDTH)
+    .flatten(true)
+    .toColorspace("b-w")
+    .png()
+    .toBuffer();
+
+  const flatOutline: Buffer = await sharp(fuzzyOutline)
+    .threshold(1) // force black and white
+    .negate() // invert black and white
+    .unflatten() // convert to transparency
+    .png()
+    .toBuffer();
+
+  const whiteOutline: Buffer = await sharp(white)
+    .composite([
+      { input: flatOutline, blend: "dest-in" },
+      { input, blend: "atop" },
+    ])
+    .png()
+    .toBuffer();
+
+  return whiteOutline;
+}
+
+async function punchHoleInMask(redMask: Buffer, hole: Buffer): Promise<Buffer> {
+  const punchedHole: Buffer = await sharp(redMask)
+    .composite([{ input: hole, blend: "dest-out" }]) // original drawn where hole is not
+    .png()
+    .toBuffer();
+
+  return punchedHole;
+}
+
 async function outline(input: Buffer): Promise<Buffer> {
   const blurredInnerMask: Buffer = await sharp(input)
     .blur(OUTLINE_WIDTH)
@@ -28,6 +109,7 @@ export async function sustained(
   sustainedPngFilename: string
 ) {
   const src: string = pngFilename;
+  const dstOutlined: string = pngFilename.replace(/.png$/, "-outlined.png");
   const dst: string = pngFilename.replace(/.png$/, "-sustained.png");
   const dstMask: string = pngFilename.replace(/.png$/, "-sustained-mask.png");
 
@@ -36,12 +118,35 @@ export async function sustained(
   const height: number = metadata.height || 1;
 
   const unit: Buffer = await sharp(src).png().toBuffer();
+
+  const outlined: Buffer = await whiteOutlinedMask(unit);
+  punchHoleInMask(outlined, unit);
+
   const unitOutline: Buffer = await outline(unit);
   const unitMask: Buffer = await sharp(unit)
     .extractChannel("alpha")
     .unflatten()
     .png()
     .toBuffer();
+
+  const unitOutlineWithTransparency: Buffer = await sharp(unitOutline)
+    .unflatten()
+    .negate()
+    .png()
+    .toBuffer();
+  const unitOutlined: Buffer = await sharp({
+    create: {
+      width,
+      height,
+      channels: 4,
+      background: { r: 255, g: 255, b: 255, alpha: 0 },
+    },
+  })
+    .composite([{ input: unitOutlineWithTransparency, blend: "add" }])
+    //.composite([{ input: unit, blend: "atop" }])
+    .png()
+    .toBuffer();
+  await sharp(unitOutlineWithTransparency).png().toFile(dstOutlined);
 
   const sustained: Buffer = await sharp(sustainedPngFilename).toBuffer();
   const sustainedOutline: Buffer = await outline(sustained);
