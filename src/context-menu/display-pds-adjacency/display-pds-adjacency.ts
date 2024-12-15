e783a533369ab6032f4bf08ce21e23ee18f1900e
darrell <darrell.anderson@gmail.com>
2024-12-14 21:10:39 -0800

    adjacency path nodes on tile edges

--- src/context-menu/display-pds-adjacency/display-pds-adjacency.ts
+++ src/context-menu/display-pds-adjacency/display-pds-adjacency.ts
@@ -21,8 +21,6 @@ const ADJACENCY_ACTION_TOOLTIP: string =
  * This is mostly for debugging and verifying hyperlanes.
  */
 export class DisplayPDSAdjacency implements IGlobal {
-  public _toggleCount: number = 0; // for testing
-
   private readonly _onObjectCreatedHandler = (obj: GameObject): void => {
     this._maybeAddContextMenu(obj);
   };
@@ -71,12 +69,66 @@ export class DisplayPDSAdjacency implements IGlobal {
   }
 
   _toggleAdjacencyLines(obj: GameObject): void {
-    this._toggleCount++;
     if (this._hasAdjacencyLines(obj)) {
       this._removeAdajecncyLines(obj);
     } else {
       this._addAdjacencyLines(obj);
     }
+  }
+
+  _parseAdjencyNodeOrThrow(node: string): {
+    hex: HexType;
+    direction: string | undefined;
+  } {
+    const re: RegExp = /^(<-?\d+,-?\d+,-?\d+>)-?(n|ne|e|se|s|sw|w|nw)?$/;
+    const match: RegExpMatchArray | null = node.match(re);
+    if (!match) {
+      throw new Error(`Invalid node: ${node}`);
+    }
+    const hex: HexType = match[1] as HexType;
+    return { hex, direction: match[2] };
+  }
+
+  _getLinePoints(adjacencyNodePath: Array<string>): Array<Vector> {
+    // Path includes start and end as well as middle (if any).
+    return adjacencyNodePath.map((node: string): Vector => {
+      const parsed: { hex: HexType; direction: string | undefined } =
+        this._parseAdjencyNodeOrThrow(node);
+      const pos: Vector = TI4.hex.toPosition(parsed.hex);
+
+      // Hyperlanes add nodes for the hyperlane hex edges traversed.
+      if (parsed.direction) {
+        // Corners start with "top right", winding counterclockwise.
+        const corners: Array<Vector> = TI4.hex.corners("<0,0,0>");
+        let a: Vector | undefined = undefined;
+        let b: Vector | undefined = undefined;
+        if (parsed.direction === "n") {
+          a = corners[0];
+          b = corners[1];
+        } else if (parsed.direction === "nw") {
+          a = corners[1];
+          b = corners[2];
+        } else if (parsed.direction === "sw") {
+          a = corners[2];
+          b = corners[3];
+        } else if (parsed.direction === "s") {
+          a = corners[3];
+          b = corners[4];
+        } else if (parsed.direction === "se") {
+          a = corners[4];
+          b = corners[5];
+        } else if (parsed.direction === "ne") {
+          a = corners[5];
+          b = corners[0];
+        }
+        if (a && b) {
+          pos.x += (a.x + b.x) / 2;
+          pos.y += (a.y + b.y) / 2;
+        }
+      }
+
+      return pos;
+    });
   }
 
   _addAdjacencyLines(obj: GameObject): void {
@@ -88,24 +140,21 @@ export class DisplayPDSAdjacency implements IGlobal {
       .filter((adjacencyResult: AdjacencyResult): boolean => {
         return adjacencyResult.distance === 1;
       });
-    adjacencyResults.forEach((result: AdjacencyResult): void => {
+    adjacencyResults.forEach((adjacencyResult: AdjacencyResult): void => {
       const line: DrawingLine = new DrawingLine();
       line.color = new Color(1, 0, 0, 1);
       line.normals = [new Vector(0, 0, 1)];
       line.tag = ADJACENCY_LINE_TAG;
       line.thickness = 1;
 
-      // Path includes start and end as well as middle (if any).
-      line.points = result.path.map((node: string): Vector => {
-        // Extract the hex portion of the path entry.
-        // (Intermediate nodes are "<hex>-nw", "<hex>-ne", etc.)
-        const nodeHex: HexType = node.replace(/-[a-z]*$/, "") as HexType;
+      line.points = this._getLinePoints(adjacencyResult.path).map(
+        (point: Vector): Vector => {
+          const pos: Vector = obj.worldPositionToLocal(point);
+          pos.z = 0; // same plane as the PDS.
+          return pos;
+        }
+      );
 
-        const nodePos: Vector = TI4.hex.toPosition(nodeHex);
-        const localPos: Vector = obj.worldPositionToLocal(nodePos);
-        localPos.z = 0;
-        return localPos;
-      });
       obj.addDrawingLine(line);
     });
   }
--- src/context-menu/display-pds-adjacency/display-pds.adjacency.test.ts
+++ src/context-menu/display-pds-adjacency/display-pds.adjacency.test.ts
@@ -1,4 +1,4 @@
-import { globalEvents, Player } from "@tabletop-playground/api";
+import { globalEvents, Player, Vector } from "@tabletop-playground/api";
 import { HexType } from "ttpg-darrell";
 import { DrawingLine, MockGameObject, MockPlayer } from "ttpg-mock";
 
@@ -73,4 +73,47 @@ it("toggle on/off", () => {
   pds._customActionAsPlayer(player, ADJACENCY_ACTION_NAME);
   expect(pds.getDrawingLines().length).toBe(0);
   expect(displayPdsAdjacency._hasAdjacencyLines(pds)).toBe(false);
+});
+
+it("_parseAdjencyNodeOrThrow", () => {
+  const displayPdsAdjacency = new DisplayPDSAdjacency();
+  expect(() => displayPdsAdjacency._parseAdjencyNodeOrThrow("")).toThrow();
+
+  let parsed: { hex: HexType; direction: string | undefined };
+  parsed = displayPdsAdjacency._parseAdjencyNodeOrThrow("<0,0,0>");
+  expect(parsed.hex).toBe("<0,0,0>");
+  expect(parsed.direction).toBeUndefined();
+
+  parsed = displayPdsAdjacency._parseAdjencyNodeOrThrow("<0,0,0>-n");
+  expect(parsed.hex).toBe("<0,0,0>");
+  expect(parsed.direction).toBe("n");
+});
+
+it("_getLinePoints", () => {
+  const displayPdsAdjacency = new DisplayPDSAdjacency();
+  const p1 = displayPdsAdjacency
+    ._getLinePoints(["<0,0,0>", "<1,0,-1>"])
+    .map((pos: Vector) => pos.toString());
+  expect(p1).toEqual(["(X=0,Y=0,Z=0)", "(X=15.01,Y=0,Z=0)"]);
+
+  const p2 = displayPdsAdjacency
+    ._getLinePoints([
+      "<0,0,0>",
+      "<0,0,0>-n",
+      "<0,0,0>-nw",
+      "<0,0,0>-sw",
+      "<0,0,0>-s",
+      "<0,0,0>-se",
+      "<0,0,0>-ne",
+    ])
+    .map((pos: Vector) => pos.toString());
+  expect(p2).toEqual([
+    "(X=0,Y=0,Z=0)",
+    "(X=7.505,Y=0,Z=0)",
+    "(X=3.752,Y=-6.5,Z=0)",
+    "(X=-3.752,Y=-6.5,Z=0)",
+    "(X=-7.505,Y=0,Z=0)",
+    "(X=-3.752,Y=6.5,Z=0)",
+    "(X=3.752,Y=6.5,Z=0)",
+  ]);
 });
