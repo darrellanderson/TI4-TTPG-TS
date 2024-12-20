f8508a7b8a10b33392127d4d84ce992d6836855c
darrell <darrell.anderson@gmail.com>
2024-12-20 00:26:05 -0800

    test coverage

--- src/lib/draft-lib/drafts/milty.test.ts
+++ src/lib/draft-lib/drafts/milty.test.ts
@@ -1,4 +1,17 @@
+import { MockGameObject } from "ttpg-mock";
+import { CreateDraftParams } from "./abstract-draft";
 import { Milty } from "./milty";
+
+// Systems must exist for registry to know about them.
+beforeEach(() => {
+  for (const tile of TI4.systemRegistry.getAllSystemTileNumbers()) {
+    const nsid: string | undefined =
+      TI4.systemRegistry.tileNumberToSystemTileObjNsid(tile);
+    if (nsid) {
+      MockGameObject.simple(nsid);
+    }
+  }
+});
 
 it("constructor", () => {
   new Milty();
@@ -20,4 +33,17 @@ it("createDraftEmptyState", () => {
 
   TI4.config.setPlayerCount(8);
   milty.createEmptyDraftState("@test/milty");
+});
+
+it("createDraftState", () => {
+  const createDraftParams: CreateDraftParams = {
+    namespaceId: "@test/milty",
+    numSlices: 3,
+    numFactions: 3,
+    config: "labels=a",
+  };
+
+  const milty = new Milty();
+  const draftState = milty.createDraftState(createDraftParams);
+  expect(draftState).toBeDefined();
 });
