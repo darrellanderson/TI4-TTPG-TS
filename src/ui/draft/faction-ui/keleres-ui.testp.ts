4717b876c70f6ff2fe0a7148697a6f7e9dd9683b
darrell <darrell.anderson@gmail.com>
2024-12-25 00:43:29 -0800

    custom keleres button progress

--- src/ui/draft/faction-ui/keleres-ui.testp.ts
+++ src/ui/draft/faction-ui/keleres-ui.testp.ts
@@ -2,7 +2,6 @@ import { ScreenUIElement, Widget, world } from "@tabletop-playground/api";
 import { Faction } from "../../../lib/faction-lib/faction/faction";
 import { KeleresUI } from "./keleres-ui";
 import { DraftState } from "../../../lib/draft-lib/draft-state/draft-state";
-import { WrappedClickableUI } from "ui/wrapped-clickable-ui/wrapped-clickable-ui";
 function go() {
   const faction: Faction | undefined =
     TI4.factionRegistry.getByNsidName("arborec");
@@ -13,8 +12,7 @@ function go() {
   const draftState = new DraftState("@test/test");
 
   const keleresUi = new KeleresUI(draftState, 1);
-  const wrappedClickableUI = new WrappedClickableUI(keleresUi, 1);
-  const widget: Widget = wrappedClickableUI.getWidget();
+  const widget: Widget = keleresUi.getWidget();
 
   const screenUI = new ScreenUIElement();
   screenUI.positionX = 0.5;
@@ -22,8 +20,8 @@ function go() {
   screenUI.relativePositionX = true;
   screenUI.relativePositionY = true;
 
-  screenUI.width = wrappedClickableUI.getSize().w;
-  screenUI.height = wrappedClickableUI.getSize().h;
+  screenUI.width = keleresUi.getSize().w;
+  screenUI.height = keleresUi.getSize().h;
   screenUI.relativeWidth = false;
   screenUI.relativeHeight = false;
 
--- src/ui/draft/faction-ui/keleres-ui.ts
+++ src/ui/draft/faction-ui/keleres-ui.ts
@@ -1,8 +1,8 @@
 import {
+  Border,
   Canvas,
+  ContentButton,
   HorizontalAlignment,
-  HorizontalBox,
-  ImageWidget,
   LayoutBox,
   Text,
   TextJustification,
@@ -10,10 +10,14 @@ import {
   Widget,
 } from "@tabletop-playground/api";
 
-import { AbstractUI } from "../../abstract-ui/abtract-ui";
+import { AbstractUI, UI_SIZE } from "../../abstract-ui/abtract-ui";
 import { DraftState } from "../../../lib/draft-lib/draft-state/draft-state";
 import { Faction } from "../../../lib/faction-lib/faction/faction";
-import { BOX_W, BOX_H, FONT_SIZE, SPACING } from "./faction-ui";
+import { FONT_SIZE, FactionUI } from "./faction-ui";
+import {
+  WRAPPED_BORDER_WIDTH,
+  WrappedClickableUI,
+} from "ui/wrapped-clickable-ui/wrapped-clickable-ui";
 
 /**
  * Keleres has three flavors, based on Argent, Mentak, and Xxcha.
@@ -25,6 +29,9 @@ import { BOX_W, BOX_H, FONT_SIZE, SPACING } from "./faction-ui";
  * inside a content button.
  */
 export class KeleresUI extends AbstractUI {
+  private readonly _contentButton: ContentButton;
+  private readonly _border: Border = new Border();
+  private _owningPlayerSlot: number = -1;
   private readonly _draftState: DraftState;
 
   private readonly _keleresArgent: Faction =
@@ -34,6 +41,13 @@ export class KeleresUI extends AbstractUI {
   private readonly _keleresXxcha: Faction =
     TI4.factionRegistry.getByNsidOrThrow("faction:codex.vigil/keleres-xxcha");
 
+  private readonly _argentBorder: Border = new Border();
+  private readonly _argentButton: ContentButton = new ContentButton();
+  private readonly _mentakBorder: Border = new Border();
+  private readonly _mentakButton: ContentButton = new ContentButton();
+  private readonly _xxchaBorder: Border = new Border();
+  private readonly _xxchaButton: ContentButton = new ContentButton();
+
   private readonly _onDraftStateChanged = (): void => {};
 
   destroy(): void {
@@ -41,49 +55,112 @@ export class KeleresUI extends AbstractUI {
   }
 
   constructor(draftState: DraftState, scale: number) {
-    const w: number = BOX_W * scale;
-    const h: number = BOX_H * scale;
-    const fontSize = FONT_SIZE * scale;
-    const spacing = SPACING * scale;
+    // Use the same size as a "regular" faction button.
+    const dummyFaction: Faction = TI4.factionRegistry.getByNsidOrThrow(
+      "faction:base/arborec"
+    );
+    const dummyFactionUi: AbstractUI = new FactionUI(dummyFaction, scale);
+    const dummyWrappedUi: AbstractUI = new WrappedClickableUI(
+      dummyFactionUi,
+      scale
+    );
+    const size: UI_SIZE = dummyWrappedUi.getSize();
 
-    // TODO use clickable button size.
+    const fontSize: number = FONT_SIZE * scale;
+    const borderWidth: number = WRAPPED_BORDER_WIDTH * scale;
 
     // This one is complicated, use a canvas for more positional control.
     const canvas: Canvas = new Canvas();
     const box: Widget = new LayoutBox()
-      .setOverrideWidth(w)
-      .setOverrideHeight(h)
+      .setOverrideWidth(size.w)
+      .setOverrideHeight(size.h)
       .setChild(canvas);
-    super(box, { w, h });
+    super(box, size);
 
     this._draftState = draftState;
     this._draftState.onDraftStateChanged.add(this._onDraftStateChanged);
 
     // Now fill in the canvas.
+    const reserveW: number = size.w * 0.4;
 
-    const s: number = h - spacing * 2;
-    const icon: Widget = new ImageWidget()
-      .setImageSize(s, s)
-      .setImage(
-        this._keleresArgent.getIcon(),
-        this._keleresArgent.getIconPackageId()
-      );
-
+    // Label, skip the icon for the reduced size.
+    const nameW: number = size.w - reserveW - borderWidth * 2;
+    const nameH: number = size.h - borderWidth * 2;
     const name: Widget = new Text()
       .setBold(true)
       .setFontSize(fontSize)
       .setJustification(TextJustification.Center)
       .setText("Keleres".toUpperCase());
-
-    const panel: Widget = new HorizontalBox()
-      .setChildDistance(spacing)
-      .addChild(icon)
-      .addChild(name);
-    const panelBox: LayoutBox = new LayoutBox()
+    const nameBox: Widget = new LayoutBox()
+      .setOverrideWidth(nameW)
+      .setOverrideHeight(nameH)
       .setHorizontalAlignment(HorizontalAlignment.Center)
       .setVerticalAlignment(VerticalAlignment.Center)
-      .setChild(panel);
+      .setChild(name);
+    this._contentButton = new ContentButton().setChild(nameBox);
 
-    canvas.addChild(panelBox, 0, 0, w, h);
+    const flavorLeft: number = size.w - reserveW;
+    const flavorH: number = size.h / 3;
+    const flavorFontSize: number = fontSize * 0.8;
+
+    const createFlavorButton = (
+      name: string,
+      border: Border,
+      button: ContentButton
+    ): Widget => {
+      const flavorLabel: Widget = new Text()
+        .setFontSize(flavorFontSize)
+        .setText(name.toUpperCase());
+      const flavorLabelBox: Widget = new LayoutBox()
+        .setOverrideWidth(reserveW)
+        .setOverrideHeight(flavorH)
+        .setHorizontalAlignment(HorizontalAlignment.Center)
+        .setVerticalAlignment(VerticalAlignment.Center)
+        .setChild(flavorLabel);
+      border.setChild(flavorLabelBox);
+      button.setChild(border);
+
+      // Strip off content button edges.  LayoutBox negative padding
+      // fails here, displaying on the canvas.
+      const flavorBox: Widget = new Canvas().addChild(
+        button,
+        -4,
+        -4,
+        reserveW + 8,
+        flavorH + 8
+      );
+      return flavorBox;
+    };
+
+    const argent: Widget = createFlavorButton(
+      "argent",
+      this._argentBorder,
+      this._argentButton
+    );
+    canvas.addChild(argent, flavorLeft, 0, reserveW, flavorH);
+
+    const mentak: Widget = createFlavorButton(
+      "mentak",
+      this._mentakBorder,
+      this._mentakButton
+    );
+    canvas.addChild(mentak, flavorLeft, flavorH, reserveW, flavorH);
+
+    const xxcha: Widget = createFlavorButton(
+      "xxcha",
+      this._xxchaBorder,
+      this._xxchaButton
+    );
+    canvas.addChild(xxcha, flavorLeft, flavorH * 2, reserveW, flavorH);
+
+    // Add left button last to draw on top of flavor bleed left.
+    canvas.addChild(this._border, 0, 0, size.w - reserveW, size.h);
+    canvas.addChild(
+      this._contentButton,
+      borderWidth,
+      borderWidth,
+      nameW,
+      nameH
+    );
   }
 }
