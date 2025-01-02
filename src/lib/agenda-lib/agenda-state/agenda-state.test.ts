969b079e73d8c36c94358990b05650d40b84d679
darrell <darrell.anderson@gmail.com>
2025-01-01 22:12:22 -0800

    agenda state

--- src/lib/agenda-lib/agenda-state/agenda-state.test.ts
+++ src/lib/agenda-lib/agenda-state/agenda-state.test.ts
@@ -7,7 +7,18 @@ it("constructor/destroy/static isAgendaInProgress", () => {
 
   const agendaState: AgendaState = new AgendaState(id);
   expect(AgendaState.isAgendaInProgress(id)).toBe(true);
+  expect(agendaState.isActive()).toBe(true);
 
   agendaState.destroy();
   expect(AgendaState.isAgendaInProgress(id)).toBe(false);
+  expect(agendaState.isActive()).toBe(false);
+});
+
+it("agenda obj id", () => {
+  const id: NamespaceId = "@test/test";
+  const agendaState: AgendaState = new AgendaState(id);
+  expect(agendaState.getAgendaObjId()).toBe("");
+  agendaState.setAgendaObjId("agenda");
+  expect(agendaState.getAgendaObjId()).toBe("agenda");
+  agendaState.destroy();
 });
--- src/lib/agenda-lib/agenda-state/agenda-state.ts
+++ src/lib/agenda-lib/agenda-state/agenda-state.ts
@@ -2,8 +2,26 @@ import { world } from "@tabletop-playground/api";
 import { NamespaceId, TriggerableMulticastDelegate } from "ttpg-darrell";
 import { z } from "zod";
 
-export const AgendaStateSchema = z.object({});
-export type AgendaStateSchemaType = z.infer<typeof AgendaStateSchema>;
+const AgendaRiderSchema = z.object({
+  objId: z.string(),
+  outcome: z.number(),
+});
+type AgendaRiderSchemaType = z.infer<typeof AgendaRiderSchema>;
+
+const AgendaSeatStateSchema = z.object({
+  avail: z.number().default(0),
+  outcome: z.number().default(-1),
+  votes: z.number().default(0),
+  riders: z.array(AgendaRiderSchema).default([]),
+});
+type AgendaSeatStateSchemaType = z.infer<typeof AgendaSeatStateSchema>;
+
+const AgendaStateSchema = z.object({
+  agendaObjId: z.string().default(""),
+  outcomeNames: z.array(z.string().nullable()).default([]),
+  seatIndexToState: z.array(AgendaSeatStateSchema).default([]),
+});
+type AgendaStateSchemaType = z.infer<typeof AgendaStateSchema>;
 
 export class AgendaState {
   public readonly onAgendaStateChanged: TriggerableMulticastDelegate<
@@ -46,5 +64,93 @@ export class AgendaState {
 
   isActive(): boolean {
     return AgendaState.isAgendaInProgress(this._namespaceId);
+  }
+
+  getAgendaObjId(): string {
+    return this._data.agendaObjId;
+  }
+
+  setAgendaObjId(agendaObjId: string): void {
+    this._data.agendaObjId = agendaObjId;
+    this._save();
+    this.onAgendaStateChanged.trigger(this);
+  }
+
+  getOutcomeName(index: number): string | undefined {
+    return this._data.outcomeNames[index] ?? undefined;
+  }
+
+  setOutcomeName(index: number, name: string): void {
+    this._data.outcomeNames[index] = name;
+    this._save();
+    this.onAgendaStateChanged.trigger(this);
+  }
+
+  _getSeatState(seatIndex: number): AgendaSeatStateSchemaType {
+    let seatState: AgendaSeatStateSchemaType | undefined =
+      this._data.seatIndexToState[seatIndex];
+    if (!seatState) {
+      seatState = AgendaSeatStateSchema.parse({});
+      this._data.seatIndexToState[seatIndex] = seatState;
+    }
+    return seatState;
+  }
+
+  getAvailableVotes(seatIndex: number): number {
+    const seatState = this._getSeatState(seatIndex);
+    return seatState.avail;
+  }
+
+  setAvailableVotes(seatIndex: number, votes: number): void {
+    const seatState = this._getSeatState(seatIndex);
+    seatState.avail = votes;
+    this._save();
+    this.onAgendaStateChanged.trigger(this);
+  }
+
+  getOutcomeChoice(seatIndex: number): number {
+    const seatState = this._getSeatState(seatIndex);
+    return seatState.outcome;
+  }
+
+  setOutcomeChoice(seatIndex: number, outcome: number): void {
+    const seatState = this._getSeatState(seatIndex);
+    seatState.outcome = outcome;
+    this._save();
+    this.onAgendaStateChanged.trigger(this);
+  }
+
+  getVotesForOutcome(seatIndex: number): number {
+    const seatState = this._getSeatState(seatIndex);
+    return seatState.votes;
+  }
+
+  setVotesForOutcome(seatIndex: number, votes: number): void {
+    const seatState = this._getSeatState(seatIndex);
+    seatState.votes = votes;
+    this._save();
+    this.onAgendaStateChanged.trigger(this);
+  }
+
+  addRider(seatIndex: number, objId: string, outcome: number): void {
+    this.removeRider(objId);
+    const seatState = this._getSeatState(seatIndex);
+    seatState.riders.push({ objId, outcome });
+    this._save();
+    this.onAgendaStateChanged.trigger(this);
+  }
+
+  removeRider(objId: string): void {
+    for (const seatState of this._data.seatIndexToState) {
+      seatState.riders.forEach(
+        (rider: AgendaRiderSchemaType, index: number) => {
+          if (rider.objId === objId) {
+            seatState.riders.splice(index, 1);
+            this._save();
+            this.onAgendaStateChanged.trigger(this);
+          }
+        }
+      );
+    }
   }
 }
