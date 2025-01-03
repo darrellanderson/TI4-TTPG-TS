63f5ce41025accde1347018ac0af89e4cd10e3b2
darrell <darrell.anderson@gmail.com>
2025-01-02 21:06:36 -0800

    agenda turn order

--- src/lib/agenda-lib/agenda-turn-order/agenda-turn-order.test.ts
+++ src/lib/agenda-lib/agenda-turn-order/agenda-turn-order.test.ts
@@ -1,5 +1,140 @@
+import { MockCardHolder, MockGameObject } from "ttpg-mock";
 import { AgendaTurnOrder } from "./agenda-turn-order";
+import { GameObject } from "@tabletop-playground/api";
 
 it("constructor", () => {
   new AgendaTurnOrder();
+});
+
+it("_getSpeakerTokenOrThrow (found)", () => {
+  const speakerToken: GameObject = MockGameObject.simple("token:base/speaker");
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found: GameObject = agendaTurnOrder._getSpeakerTokenOrThrow();
+  expect(found).toBe(speakerToken);
+});
+
+it("_getSpeakerTokenOrThrow (throw)", () => {
+  const agendaTurnOrder = new AgendaTurnOrder();
+  expect(() => agendaTurnOrder._getSpeakerTokenOrThrow()).toThrow(
+    "missing speaker token"
+  );
+});
+
+it("_getSpeakerTokenSeatIndexOrThrow (found)", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+  });
+  MockGameObject.simple("token:base/speaker");
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found: number = agendaTurnOrder._getSpeakerTokenSeatIndexOrThrow();
+  expect(found).toBe(0);
+});
+
+it("_getSpeakerTokenSeatIndexOrThrow (missing card holder)", () => {
+  MockGameObject.simple("token:base/speaker");
+  const agendaTurnOrder = new AgendaTurnOrder();
+  expect(() => {
+    agendaTurnOrder._getSpeakerTokenSeatIndexOrThrow();
+  }).toThrow();
+});
+
+it("_getZealPlayerSlots", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+  });
+  MockGameObject.simple("sheet.faction:pok/argent");
+  expect(TI4.factionRegistry.getPlayerSlotToFaction().size).toBe(1);
+
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found = agendaTurnOrder._getZealPlayerSlots();
+  expect(found).toEqual([10]);
+});
+
+it("getWhensOrAftersOrder", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+    position: [0, 1, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 11,
+    position: [0, -1, 0],
+  });
+  MockGameObject.simple("token:base/speaker", { position: [0, 1, 0] });
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found = agendaTurnOrder.getWhensOrAftersOrder();
+  expect(found).toEqual([10, 11]);
+});
+
+it("getVotingOrder", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+    position: [0, 1, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 11,
+    position: [-1, 0, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 12,
+    position: [0, -1, 0],
+  });
+  MockGameObject.simple("token:base/speaker", { position: [0, 1, 0] });
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found = agendaTurnOrder.getVotingOrder();
+  expect(found).toEqual([11, 12, 10]);
+});
+
+it("getVotingOrder (reverse)", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+    position: [0, 1, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 11,
+    position: [-1, 0, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 12,
+    position: [0, -1, 0],
+  });
+  MockGameObject.simple("token:base/speaker", { position: [0, 1, 0] });
+  MockGameObject.simple("card.action:codex.ordinian/hack-election");
+
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found = agendaTurnOrder.getVotingOrder();
+  expect(found).toEqual([12, 11, 10]);
+});
+
+it("getVotingOrder (zeal)", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+    position: [0, 1, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 11,
+    position: [-1, 0, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 12,
+    position: [0, -1, 0],
+  });
+  MockGameObject.simple("token:base/speaker", { position: [0, 1, 0] });
+  MockGameObject.simple("sheet.faction:pok/argent", { position: [0, 1, 0] });
+
+  const agendaTurnOrder = new AgendaTurnOrder();
+  const found = agendaTurnOrder.getVotingOrder();
+  expect(found).toEqual([10, 11, 12]);
 });
--- src/lib/agenda-lib/agenda-turn-order/agenda-turn-order.ts
+++ src/lib/agenda-lib/agenda-turn-order/agenda-turn-order.ts
@@ -1,1 +1,130 @@
-export class AgendaTurnOrder {}
+import { GameObject, Vector, world } from "@tabletop-playground/api";
+import { Find, NSID, PlayerSlot } from "ttpg-darrell";
+import { Faction } from "../../faction-lib/faction/faction";
+
+// Hack Election: During this agenda, voting begins with the player to the
+// right of the speaker and continues counterclockwise.
+const REVERSE_ORDER_NSIDS: Set<string> = new Set<string>([
+  "card.action:codex.ordinian/hack-election",
+]);
+
+export class AgendaTurnOrder {
+  private readonly _find: Find = new Find();
+
+  public _getSpeakerTokenOrThrow(): GameObject {
+    const nsid: string = "token:base/speaker";
+    const playerSlot = undefined;
+    const skipContained = true;
+    const speakerToken = this._find.findGameObject(
+      nsid,
+      playerSlot,
+      skipContained
+    );
+    if (!speakerToken) {
+      throw new Error("missing speaker token");
+    }
+    return speakerToken;
+  }
+
+  public _getSpeakerTokenSeatIndexOrThrow(): number {
+    const speakerToken: GameObject = this._getSpeakerTokenOrThrow();
+    const pos: Vector = speakerToken.getPosition();
+    const playerSlot: number = this._find.closestOwnedCardHolderOwner(pos);
+    const seatIndex: number = TI4.playerSeats
+      .getAllSeats()
+      .findIndex((seat) => seat.playerSlot === playerSlot);
+    if (seatIndex === -1) {
+      throw new Error("missing speaker token seat index");
+    }
+    return seatIndex;
+  }
+
+  public _getVotingDirection(): 1 | -1 {
+    let dir: 1 | -1 = 1;
+    const skipContained = true;
+    for (const obj of world.getAllObjects(skipContained)) {
+      const nsid: string = NSID.get(obj);
+      if (REVERSE_ORDER_NSIDS.has(nsid)) {
+        dir = -1;
+        break;
+      }
+    }
+    return dir;
+  }
+
+  public _getZealPlayerSlots(): Array<PlayerSlot> {
+    const zealPlayerSlots: Array<PlayerSlot> = [];
+
+    const playerSlotToFaction: Map<PlayerSlot, Faction> =
+      TI4.factionRegistry.getPlayerSlotToFaction();
+    for (const [playerSlot, faction] of playerSlotToFaction) {
+      if (faction.getAbilityNsids().includes("faction-ability:pok/zeal")) {
+        zealPlayerSlots.push(playerSlot);
+      }
+    }
+    return zealPlayerSlots;
+  }
+
+  /**
+   * "When" and "after" resolve order.
+   *
+   * [2.9] Players take turns resolving action cards starting with the
+   * speaker and proceeding clockwise.
+   *
+   * @returns {Array.{PlayerDesk}}
+   */
+  public getWhensOrAftersOrder(): Array<PlayerSlot> {
+    const speakerSeatIndex: number = this._getSpeakerTokenSeatIndexOrThrow();
+    const seatPlayerSlots: Array<PlayerSlot> = TI4.playerSeats
+      .getAllSeats()
+      .map((seat) => seat.playerSlot);
+    const order: Array<PlayerSlot> = [
+      ...seatPlayerSlots.slice(speakerSeatIndex),
+      ...seatPlayerSlots.slice(0, speakerSeatIndex),
+    ];
+    return order;
+  }
+
+  /**
+   * [8.2] Each player, starting with the player to the left of the
+   * speaker and continuing clockwise, can cast votes for an outcome
+   * of the current agenda.
+   */
+  public getVotingOrder(): Array<PlayerSlot> {
+    // Check if reversed direction.
+    const dir: 1 | -1 = this._getVotingDirection();
+
+    const seatPlayerSlots: Array<PlayerSlot> = TI4.playerSeats
+      .getAllSeats()
+      .map((seat) => seat.playerSlot);
+    const speakerSeatIndex: number = this._getSpeakerTokenSeatIndexOrThrow();
+    const firstVoterIndex: number =
+      (speakerSeatIndex + dir) % seatPlayerSlots.length;
+
+    const order: Array<PlayerSlot> = [];
+    for (let i = 0; i < seatPlayerSlots.length; i++) {
+      const index: number =
+        (firstVoterIndex + i * dir + seatPlayerSlots.length) %
+        seatPlayerSlots.length;
+      const playerSlot: number | undefined = seatPlayerSlots[index];
+      if (playerSlot !== undefined) {
+        order.push(playerSlot);
+      }
+    }
+
+    // If "Zeal" (Argent) is in game, they always vote first.
+    // Allow more than one, preserving order.
+    const zealPlayerSlots: Array<PlayerSlot> = this._getZealPlayerSlots();
+    for (const zealPlayerSlot of zealPlayerSlots) {
+      const index: number = order.indexOf(zealPlayerSlot);
+      if (index !== -1) {
+        order.splice(index, 1);
+      }
+    }
+    for (const zealPlayerSlot of zealPlayerSlots) {
+      order.unshift(zealPlayerSlot);
+    }
+
+    return order;
+  }
+}
