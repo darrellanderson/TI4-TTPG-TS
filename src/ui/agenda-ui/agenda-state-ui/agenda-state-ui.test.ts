b0f2737666da9abd34f69459bf3a0439455cc6f0
darrell <darrell.anderson@gmail.com>
2025-01-08 23:55:54 -0800

    progress

--- src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.test.ts
+++ src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.test.ts
@@ -1,8 +1,10 @@
 import { AgendaState } from "../../../lib/agenda-lib/agenda-state/agenda-state";
 import { AgendaStateUI } from "./agenda-state-ui";
 
-it("constructor/destroy", () => {
+it("constructor/event/destroy", () => {
   const agendaState: AgendaState = new AgendaState("@test/test");
   const scale: number = 1;
-  new AgendaStateUI(agendaState, scale).destroy();
+  const agendaStateUI: AgendaStateUI = new AgendaStateUI(agendaState, scale);
+  agendaState.onAgendaStateChanged.trigger(agendaState);
+  agendaStateUI.destroy();
 });
