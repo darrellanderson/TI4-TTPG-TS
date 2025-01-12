a1ee1fbfbeeb174c46ac20dc52cf003b17079f67
darrell <darrell.anderson@gmail.com>
2025-01-11 21:26:09 -0800

    tests

--- src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.test.ts
+++ src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.test.ts
@@ -1,10 +1,37 @@
+import { Card } from "@tabletop-playground/api";
 import { AgendaState } from "../../../lib/agenda-lib/agenda-state/agenda-state";
 import { AgendaStateUI } from "./agenda-state-ui";
+import { MockCard } from "ttpg-mock";
 
-it("constructor/event/destroy", () => {
+it("static _createAgendaCardUI", () => {
+  const agendaCard: Card = MockCard.simple("card.agenda:my-source/my-name");
+  const agendaState: AgendaState = new AgendaState("@test/test").setAgendaObjId(
+    agendaCard.getId()
+  );
+  const scale: number = 1;
+  AgendaStateUI._createAgendaCardUI(agendaState, scale);
+});
+
+it("static _createAgendaCardUI (no agenda card)", () => {
   const agendaState: AgendaState = new AgendaState("@test/test");
   const scale: number = 1;
-  const agendaStateUI: AgendaStateUI = new AgendaStateUI(agendaState, scale);
+  expect(() => {
+    AgendaStateUI._createAgendaCardUI(agendaState, scale);
+  }).toThrow("Agenda card missing or not card");
+});
+
+it("constructor/event/destroy", () => {
+  const agendaCard: Card = MockCard.simple("card.agenda:my-source/my-name");
+  const agendaState: AgendaState = new AgendaState("@test/test").setAgendaObjId(
+    agendaCard.getId()
+  );
+  const seatIndex: number = 0;
+  const scale: number = 1;
+  const agendaStateUI: AgendaStateUI = new AgendaStateUI(
+    agendaState,
+    seatIndex,
+    scale
+  );
   agendaState.onAgendaStateChanged.trigger(agendaState);
   agendaStateUI.destroy();
 });
--- src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.ts
+++ src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.ts
@@ -1,15 +1,101 @@
+import {
+  Card,
+  GameObject,
+  VerticalAlignment,
+  world,
+} from "@tabletop-playground/api";
 import { AbstractUI } from "../../abstract-ui/abtract-ui";
 import { AgendaState } from "../../../lib/agenda-lib/agenda-state/agenda-state";
 import { CONFIG } from "../../config/config";
 import { HorizontalUIBuilder } from "../../panel/horizontal-ui-builder";
+import { AgendaCardUI } from "../agenda-card-ui/agenda-card-ui";
+import { ButtonUI } from "../../button-ui/button-ui";
 
 export class AgendaStateUI extends AbstractUI {
   private readonly _agendaState: AgendaState;
   private readonly _onAgendaStateChangedHandler = () => {};
 
-  constructor(agendaState: AgendaState, scale: number) {
+  static _createAgendaCardUI(
+    agendaState: AgendaState,
+    scale: number
+  ): AbstractUI {
+    const objId: string = agendaState.getAgendaObjId();
+    const agendaCard: GameObject | undefined = world.getObjectById(objId);
+    if (!agendaCard || !(agendaCard instanceof Card)) {
+      throw new Error("Agenda card missing or not card");
+    }
+    return new AgendaCardUI(agendaCard, scale);
+  }
+
+  static _createWhensUI(
+    agendaState: AgendaState,
+    seatIndex: number,
+    scale: number
+  ): AbstractUI {
+    const noWhens: ButtonUI = new ButtonUI(scale);
+    noWhens.getButton().setText("No whens");
+    noWhens.getButton().onClicked.add(() => {
+      agendaState.setSeatNoWhens(seatIndex, "no");
+    });
+
+    const neverWhens: ButtonUI = new ButtonUI(scale);
+    neverWhens.getButton().setText("Never whens");
+    neverWhens.getButton().onClicked.add(() => {
+      agendaState.setSeatNoWhens(seatIndex, "never");
+    });
+
+    const reset: ButtonUI = new ButtonUI(scale);
+    reset.getButton().setText("Reset whens");
+    reset.getButton().onClicked.add(() => {
+      agendaState.setSeatNoWhens(seatIndex, "unknown");
+    });
+
+    agendaState.onAgendaStateChanged.add(() => {
+      const noNeverUnknown: "no" | "never" | "unknown" =
+        agendaState.getSeatNoWhens(seatIndex);
+      noWhens.getButton().setEnabled(noNeverUnknown === "unknown");
+      neverWhens.getButton().setEnabled(noNeverUnknown !== "never");
+    });
+
+    return new HorizontalUIBuilder()
+      .setSpacing(CONFIG.SPACING)
+      .addUIs([noWhens, neverWhens, reset])
+      .build();
+  }
+
+  static _createTopRowUI(
+    agendaState: AgendaState,
+    seatIndex: number,
+    scale: number
+  ): AbstractUI {
+    const agendaCardUI: AbstractUI = AgendaStateUI._createAgendaCardUI(
+      agendaState,
+      scale
+    );
+
+    const whensUI: AbstractUI = AgendaStateUI._createWhensUI(
+      agendaState,
+      seatIndex,
+      scale
+    );
+
+    return new HorizontalUIBuilder()
+      .setSpacing(CONFIG.SPACING)
+      .setVerticalAlignment(VerticalAlignment.Top)
+      .addUIs([agendaCardUI, whensUI])
+      .build();
+  }
+
+  constructor(agendaState: AgendaState, seatIndex: number, scale: number) {
+    const topRowUI: AbstractUI = AgendaStateUI._createTopRowUI(
+      agendaState,
+      seatIndex,
+      scale
+    );
+
     const abstractUi: AbstractUI = new HorizontalUIBuilder()
       .setSpacing(CONFIG.SPACING * scale)
+      .addUIs([topRowUI])
       .build();
     super(abstractUi.getWidget(), abstractUi.getSize());
 
