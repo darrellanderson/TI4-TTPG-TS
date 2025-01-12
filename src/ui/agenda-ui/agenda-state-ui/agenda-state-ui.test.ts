d80d0dbd2682082a0674e25abcc5f5a39398be81
darrell <darrell.anderson@gmail.com>
2025-01-11 21:47:03 -0800

    test

--- src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.test.ts
+++ src/ui/agenda-ui/agenda-state-ui/agenda-state-ui.test.ts
@@ -1,7 +1,9 @@
 import { Card } from "@tabletop-playground/api";
+import { clickAll, MockCard } from "ttpg-mock";
+
 import { AgendaState } from "../../../lib/agenda-lib/agenda-state/agenda-state";
 import { AgendaStateUI } from "./agenda-state-ui";
-import { MockCard } from "ttpg-mock";
+import { AbstractUI } from "../../abstract-ui/abtract-ui";
 
 it("static _createAgendaCardUI", () => {
   const agendaCard: Card = MockCard.simple("card.agenda:my-source/my-name");
@@ -9,7 +11,11 @@ it("static _createAgendaCardUI", () => {
     agendaCard.getId()
   );
   const scale: number = 1;
-  AgendaStateUI._createAgendaCardUI(agendaState, scale);
+  const abstractUi: AbstractUI = AgendaStateUI._createAgendaCardUI(
+    agendaState,
+    scale
+  );
+  clickAll(abstractUi.getWidget());
 });
 
 it("static _createAgendaCardUI (no agenda card)", () => {
@@ -18,6 +24,18 @@ it("static _createAgendaCardUI (no agenda card)", () => {
   expect(() => {
     AgendaStateUI._createAgendaCardUI(agendaState, scale);
   }).toThrow("Agenda card missing or not card");
+});
+
+it("static _createWhensUI", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const seatIndex: number = 0;
+  const scale: number = 1;
+  const abstractUi: AbstractUI = AgendaStateUI._createWhensUI(
+    agendaState,
+    seatIndex,
+    scale
+  );
+  clickAll(abstractUi.getWidget());
 });
 
 it("constructor/event/destroy", () => {
