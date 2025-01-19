138d26ad6a72e3e0605d1c1fe054cbbe66474a30
darrell <darrell.anderson@gmail.com>
2025-01-19 01:17:23 -0800

    reset planet cards in progress

---
+++ src/lib/agenda-lib/reset-planet-cards/reset-planet-cards.test.ts
@@ -0,0 +1,5 @@
+import { ResetPlanetCards } from "./reset-planet-cards";
+
+it("constructor", () => {
+  new ResetPlanetCards();
+});
---
+++ src/lib/agenda-lib/reset-planet-cards/reset-planet-cards.ts
@@ -0,0 +1,40 @@
+import { GameObject, world } from "@tabletop-playground/api";
+import { CardUtil, Facing, HexType, NSID } from "ttpg-darrell";
+import { Vector } from "ttpg-mock";
+
+export class ResetPlanetCards {
+  private readonly _cardUtil: CardUtil = new CardUtil();
+
+  reset(): void {
+    // Planet cards on system hexes remain.
+    const systemHexes: Set<HexType> = new Set();
+    for (const system of TI4.systemRegistry.getAllSystemsWithObjs()) {
+      const obj: GameObject = system.getObj();
+      if (!obj.getContainer()) {
+        const pos: Vector = obj.getPosition();
+        const hex: HexType = TI4.hex.fromPosition(pos);
+        systemHexes.add(hex);
+      }
+    }
+
+    const skipContained: boolean = true;
+    const allowFaceDown: boolean = true;
+    for (const obj of world.getAllObjects(skipContained)) {
+      const nsid: string = NSID.get(obj);
+      const isCandidate: boolean =
+        nsid.startsWith("card.planet:") ||
+        nsid.startsWith("card.legendary-planet:");
+      const pos: Vector = obj.getPosition();
+      const hex: HexType = TI4.hex.fromPosition(pos);
+
+      if (
+        isCandidate &&
+        this._cardUtil.isLooseCard(obj, allowFaceDown) &&
+        !Facing.isFaceUp(obj) &&
+        !systemHexes.has(hex)
+      ) {
+        //
+      }
+    }
+  }
+}
