2a8cd29d9f575ae55181f7dd2fa14321e03b5b0f
darrell <darrell.anderson@gmail.com>
2025-01-27 22:47:55 -0800

    factor out several functions

--- src/lib/agenda-lib/agenda-state/advance-no-whens-afters.test.ts
+++ src/lib/agenda-lib/agenda-state/advance-no-whens-afters.test.ts
@@ -1,12 +1,134 @@
+import { MockCardHolder, MockGameObject } from "ttpg-mock";
 import { AdvanceNoWhensAfters } from "./advance-no-whens-afters";
 import { AgendaState } from "./agenda-state";
-import { MockCardHolder, MockGameObject } from "ttpg-mock";
 
 it("constructor", () => {
   const agendaState: AgendaState = new AgendaState("@test/test");
   new AdvanceNoWhensAfters(agendaState);
 });
 
+it("_isLastPlayerInTurnOrder", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const advanceNoWhensAfters: AdvanceNoWhensAfters = new AdvanceNoWhensAfters(
+    agendaState
+  );
+
+  TI4.turnOrder.setTurnOrder([10, 11], "forward", 10);
+  expect(advanceNoWhensAfters._isLastPlayerInTurnOrder()).toBe(false);
+
+  TI4.turnOrder.setTurnOrder([10, 11], "forward", 11);
+  expect(advanceNoWhensAfters._isLastPlayerInTurnOrder()).toBe(true);
+});
+
+it("_isWhenPlayed", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const advanceNoWhensAfters: AdvanceNoWhensAfters = new AdvanceNoWhensAfters(
+    agendaState
+  );
+  expect(advanceNoWhensAfters._isWhenPlayed()).toBe(true);
+
+  for (let seatIndex = 0; seatIndex < TI4.config.playerCount; seatIndex++) {
+    agendaState.setSeatNoWhens(seatIndex, "no");
+  }
+  expect(advanceNoWhensAfters._isWhenPlayed()).toBe(false);
+});
+
+it("_isAfterPlayed", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const advanceNoWhensAfters: AdvanceNoWhensAfters = new AdvanceNoWhensAfters(
+    agendaState
+  );
+  expect(advanceNoWhensAfters._isAfterPlayed()).toBe(true);
+
+  for (let seatIndex = 0; seatIndex < TI4.config.playerCount; seatIndex++) {
+    agendaState.setSeatNoAfters(seatIndex, "no");
+  }
+  expect(advanceNoWhensAfters._isAfterPlayed()).toBe(false);
+});
+
+it("_isSkipTurnWhen", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const advanceNoWhensAfters: AdvanceNoWhensAfters = new AdvanceNoWhensAfters(
+    agendaState
+  );
+
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+    position: [-1, 1, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 11,
+    position: [-1, -1, 0],
+  });
+  const seats: Array<number> = TI4.playerSeats
+    .getAllSeats()
+    .map((seat) => seat.playerSlot);
+  expect(seats).toEqual([10, 11]);
+
+  TI4.turnOrder.setTurnOrder([10, 11], "forward", 10);
+
+  agendaState.setSeatNoWhens(0, "unknown");
+  expect(advanceNoWhensAfters._isSkipTurnWhen()).toBe(false);
+
+  agendaState.setSeatNoWhens(0, "no");
+  expect(advanceNoWhensAfters._isSkipTurnWhen()).toBe(true);
+
+  agendaState.setSeatNoWhens(0, "never");
+  expect(advanceNoWhensAfters._isSkipTurnWhen()).toBe(true);
+});
+
+it("_isSkipTurnAfter", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const advanceNoWhensAfters: AdvanceNoWhensAfters = new AdvanceNoWhensAfters(
+    agendaState
+  );
+
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 10,
+    position: [-1, 1, 0],
+  });
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 11,
+    position: [-1, -1, 0],
+  });
+  const seats: Array<number> = TI4.playerSeats
+    .getAllSeats()
+    .map((seat) => seat.playerSlot);
+  expect(seats).toEqual([10, 11]);
+
+  TI4.turnOrder.setTurnOrder([10, 11], "forward", 10);
+
+  agendaState.setSeatNoAfters(0, "unknown");
+  expect(advanceNoWhensAfters._isSkipTurnAfter()).toBe(false);
+
+  agendaState.setSeatNoAfters(0, "no");
+  expect(advanceNoWhensAfters._isSkipTurnAfter()).toBe(true);
+
+  agendaState.setSeatNoAfters(0, "never");
+  expect(advanceNoWhensAfters._isSkipTurnAfter()).toBe(true);
+});
+
+it("_isSkipTurn", () => {
+  const agendaState: AgendaState = new AgendaState("@test/test");
+  const advanceNoWhensAfters: AdvanceNoWhensAfters = new AdvanceNoWhensAfters(
+    agendaState
+  );
+
+  agendaState.setPhase("whens");
+  expect(advanceNoWhensAfters._isSkipTurn()).toBe(false);
+
+  agendaState.setPhase("afters");
+  expect(advanceNoWhensAfters._isSkipTurn()).toBe(false);
+
+  agendaState.setPhase("voting");
+  expect(advanceNoWhensAfters._isSkipTurn()).toBe(false);
+});
+
+/*
 it("advance next turn, next phase", () => {
   new MockCardHolder({
     templateMetadata: "card-holder:base/player-hand",
@@ -66,3 +188,4 @@ it("advance next turn, next phase", () => {
   expect(agendaState.getPhase()).toBe("voting");
   expect(TI4.turnOrder.getCurrentTurn()).toBe(10);
 });
+*/
--- src/lib/agenda-lib/agenda-state/advance-no-whens-afters.ts
+++ src/lib/agenda-lib/agenda-state/advance-no-whens-afters.ts
@@ -1,6 +1,6 @@
 import { Broadcast, PlayerSlot } from "ttpg-darrell";
 import { AgendaState } from "./agenda-state";
-import { PlayerSeatType } from "lib/player-lib/player-seats/player-seats";
+import { PlayerSeatType } from "../../player-lib/player-seats/player-seats";
 import { AgendaTurnOrder } from "../agenda-turn-order/agenda-turn-order";
 
 /**
@@ -22,11 +22,77 @@ export class AdvanceNoWhensAfters {
     this.maybeAdvance();
   }
 
+  _isLastPlayerInTurnOrder(): boolean {
+    const turnOrder: Array<PlayerSlot> = TI4.turnOrder.getTurnOrder();
+    const currentTurn: PlayerSlot = TI4.turnOrder.getCurrentTurn();
+    return currentTurn === turnOrder[turnOrder.length - 1];
+  }
+
+  _isWhenPlayed(): boolean {
+    const playerCount: number = TI4.config.playerCount;
+    for (let seatIndex = 0; seatIndex < playerCount; seatIndex++) {
+      if (this._agendaState.getSeatNoWhens(seatIndex) === "unknown") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  _isAfterPlayed(): boolean {
+    const playerCount: number = TI4.config.playerCount;
+    for (let seatIndex = 0; seatIndex < playerCount; seatIndex++) {
+      if (this._agendaState.getSeatNoAfters(seatIndex) === "unknown") {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  _isSkipTurnWhen(): boolean {
+    const current: PlayerSlot = TI4.turnOrder.getCurrentTurn();
+    const seatIndex: number = TI4.playerSeats.getSeatIndexByPlayerSlot(current);
+    const unknownNoNever: "unknown" | "no" | "never" =
+      this._agendaState.getSeatNoWhens(seatIndex);
+    return unknownNoNever === "no" || unknownNoNever === "never";
+  }
+
+  _isSkipTurnAfter(): boolean {
+    const current: PlayerSlot = TI4.turnOrder.getCurrentTurn();
+    const seatIndex: number = TI4.playerSeats.getSeatIndexByPlayerSlot(current);
+    const unknownNoNever: "unknown" | "no" | "never" =
+      this._agendaState.getSeatNoAfters(seatIndex);
+    return unknownNoNever === "no" || unknownNoNever === "never";
+  }
+
+  _isSkipTurn(): boolean {
+    if (this._agendaState.getPhase() === "whens") {
+      return this._isSkipTurnWhen();
+    } else if (this._agendaState.getPhase() === "afters") {
+      return this._isSkipTurnAfter();
+    } else {
+      return false;
+    }
+  }
+
   maybeAdvance(): void {
+    /*
     const turnOrder: Array<PlayerSlot> = TI4.turnOrder.getTurnOrder();
     const currentTurn: PlayerSlot = TI4.turnOrder.getCurrentTurn();
     const first: PlayerSlot | undefined = turnOrder[0];
     const isLast: boolean = currentTurn === turnOrder[turnOrder.length - 1];
+
+    // Has any player played a when or after?
+    let playedWhen: boolean = false;
+    let playedAfter: boolean = false;
+    const playerCount: number = TI4.config.playerCount;
+    for (let seatIndex = 0; seatIndex < playerCount; seatIndex++) {
+      if (this._agendaState.getSeatNoWhens(seatIndex) === "unknown") {
+        playedWhen = true;
+      }
+      if (this._agendaState.getSeatNoAfters(seatIndex) === "unknown") {
+        playedAfter = true;
+      }
+    }
 
     let seat: number = -1;
     TI4.playerSeats
@@ -39,7 +105,6 @@ export class AdvanceNoWhensAfters {
 
     let unknownNoNever: "unknown" | "no" | "never" = "unknown";
     let skipTurn: boolean = false;
-
     if (this._agendaState.getPhase() === "whens") {
       unknownNoNever = this._agendaState.getSeatNoWhens(seat);
       skipTurn = unknownNoNever === "no" || unknownNoNever === "never";
@@ -48,6 +113,16 @@ export class AdvanceNoWhensAfters {
       skipTurn = unknownNoNever === "no" || unknownNoNever === "never";
     }
 
+    let advancePhase: boolean = true;
+    if (isLast) {
+      if (this._agendaState.getPhase() === "whens" && playedWhen) {
+        advancePhase = false;
+      } else if (this._agendaState.getPhase() === "afters" && playedAfter) {
+        advancePhase = false;
+      }
+    }
+
+    // Skip the turn if the player has no whens or afters.
     if (skipTurn) {
       const name: string = TI4.playerName.getBySlot(currentTurn);
       Broadcast.chatAll(`${name} plays no ${this._agendaState.getPhase()}`);
@@ -55,16 +130,28 @@ export class AdvanceNoWhensAfters {
       if (isLast && first !== undefined) {
         // Skipping the last player in the turn order advances the phase.
         if (this._agendaState.getPhase() === "whens") {
-          this._agendaState.setPhase("afters");
-          TI4.turnOrder.setCurrentTurn(first);
+          if (playedWhen) {
+            for (let seatIndex = 0; seatIndex < playerCount; seatIndex++) {
+              this._agendaState.setSeatNoWhens(seatIndex, "unknown");
+            }
+          } else {
+            this._agendaState.setPhase("afters");
+            TI4.turnOrder.setCurrentTurn(first);
+          }
         } else if (this._agendaState.getPhase() === "afters") {
-          this._agendaState.setPhase("voting");
-          // Reset turn order to voting.
-          const order: Array<PlayerSlot> =
-            new AgendaTurnOrder().getVotingOrder();
-          const first: PlayerSlot | undefined = order[0];
-          if (first !== undefined) {
-            TI4.turnOrder.setTurnOrder(order, "forward", first);
+          if (playedAfter) {
+            for (let seatIndex = 0; seatIndex < playerCount; seatIndex++) {
+              this._agendaState.setSeatNoAfters(seatIndex, "unknown");
+            }
+          } else {
+            this._agendaState.setPhase("voting");
+            // Reset turn order to voting.
+            const order: Array<PlayerSlot> =
+              new AgendaTurnOrder().getVotingOrder();
+            const first: PlayerSlot | undefined = order[0];
+            if (first !== undefined) {
+              TI4.turnOrder.setTurnOrder(order, "forward", first);
+            }
           }
         }
       } else {
@@ -72,5 +159,6 @@ export class AdvanceNoWhensAfters {
         TI4.turnOrder.nextTurn();
       }
     }
+      */
   }
 }
