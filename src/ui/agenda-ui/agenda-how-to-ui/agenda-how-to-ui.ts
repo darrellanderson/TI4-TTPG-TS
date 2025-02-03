a4f3a59315500319c897688eb19e1f88cb783c1d
darrell <darrell.anderson@gmail.com>
2025-02-02 21:59:44 -0800

    partial text

--- src/ui/agenda-ui/agenda-how-to-ui/agenda-how-to-ui.ts
+++ src/ui/agenda-ui/agenda-how-to-ui/agenda-how-to-ui.ts
@@ -1,4 +1,4 @@
-import { Border, LayoutBox, Widget } from "@tabletop-playground/api";
+import { Border, LayoutBox, RichText, Widget } from "@tabletop-playground/api";
 import { AbstractUI, UI_SIZE } from "../../abstract-ui/abtract-ui";
 import { CONFIG } from "../../config/config";
 
@@ -8,7 +8,7 @@ export class AgendaHowToUI extends AbstractUI {
       w: CONFIG.BUTTON_WIDTH * 5 * scale + CONFIG.SPACING * 4 * scale,
       h: CONFIG.BUTTON_HEIGHT * 2 * scale + CONFIG.SPACING * 1 * scale,
     };
-    const box: Widget = new LayoutBox()
+    const box: LayoutBox = new LayoutBox()
       .setOverrideWidth(size.w)
       .setOverrideHeight(size.h)
       .setPadding(
@@ -18,7 +18,15 @@ export class AgendaHowToUI extends AbstractUI {
         CONFIG.SPACING * scale
       );
     const widget: Widget = new Border().setColor([1, 0, 0, 1]).setChild(box);
-
     super(widget, size);
+
+    const msg: string =
+      "Click 'no whens' to skip your whens this round, you get another chance next round if anyone plays a when.  Click 'never whens' to skip every time.  Click 'play when' to skip to the next turn, handle then when with the table.";
+
+    const richText: Widget = new RichText()
+      .setAutoWrap(true)
+      .setFontSize(CONFIG.FONT_SIZE * scale)
+      .setText(msg);
+    box.setChild(richText);
   }
 }
