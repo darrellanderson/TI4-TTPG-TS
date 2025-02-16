b1bec4a9fc8302cf811680d16642fbdd2a3c6e66
darrell <darrell.anderson@gmail.com>
2025-02-15 23:54:30 -0800

    progress

--- src/context-menu/right-click-rift/right-click-rift.test.ts
+++ src/context-menu/right-click-rift/right-click-rift.test.ts
@@ -1,16 +1,22 @@
-import { GameObject, MockGameObject } from "ttpg-mock";
+import { Player } from "@tabletop-playground/api";
+import { GameObject, MockGameObject, MockPlayer } from "ttpg-mock";
+
 import { RightClickRift } from "./right-click-rift";
 
 it("static applyRiftResult (yes)", () => {
-  const unitObj: GameObject = MockGameObject.simple("unit:base/destroyer");
-  const isSurvivor: boolean = true;
-  RightClickRift.applyRiftResult(unitObj, isSurvivor);
+  const unitObj: MockGameObject = MockGameObject.simple("unit:base/destroyer");
+  const rollValue: number = 4;
+  RightClickRift.applyRiftResult(unitObj, rollValue);
+
+  const player: Player = new MockPlayer();
+  const thrown: boolean = false;
+  unitObj._releaseAsPlayer(player, thrown);
 });
 
 it("static applyRiftResult (no)", () => {
   const unitObj: GameObject = MockGameObject.simple("unit:base/destroyer");
-  const isSurvivor: boolean = false;
-  RightClickRift.applyRiftResult(unitObj, isSurvivor);
+  const rollValue: number = 1;
+  RightClickRift.applyRiftResult(unitObj, rollValue);
 });
 
 it("static isRiftSystemTile", () => {
@@ -22,7 +28,9 @@ it("static isRiftSystemTile", () => {
 });
 
 it("constructor/init", () => {
+  new MockGameObject(); // for an object to find
   const rift = new RightClickRift();
   expect(rift).toBeDefined();
   rift.init();
+  new MockGameObject(); // for object created
 });
--- src/context-menu/right-click-rift/right-click-rift.ts
+++ src/context-menu/right-click-rift/right-click-rift.ts
@@ -1,11 +1,55 @@
-import { GameObject, globalEvents, world } from "@tabletop-playground/api";
+import {
+  GameObject,
+  globalEvents,
+  RichText,
+  UIPresentationStyle,
+  Vector,
+  Widget,
+  world,
+} from "@tabletop-playground/api";
 import { IGlobal } from "ttpg-darrell";
 
 import { System } from "../../lib/system-lib/system/system";
+import { UIElement } from "ttpg-mock";
 
 export class RightClickRift implements IGlobal {
-  static applyRiftResult(unitObj: GameObject, isSurvivor: boolean): void {
-    //
+  /**
+   * Display the rift result as UI, goes away on object drop.
+   *
+   * @param unitObj
+   * @param rollValue
+   * @param surviveOn
+   */
+  static applyRiftResult(
+    unitObj: GameObject,
+    rollValue: number,
+    surviveOn: number = 4
+  ): void {
+    const extraZ: number = 0.5;
+    const currentRotation: boolean = true;
+    const includeGeometry: boolean = false;
+    const extent: Vector = unitObj.getExtent(currentRotation, includeGeometry);
+    const above: Vector = unitObj.getPosition().add([0, 0, extent.z + extraZ]);
+    const localAbove: Vector = unitObj.worldPositionToLocal(above);
+
+    const isSurvivor: boolean = rollValue >= surviveOn;
+    const bbColor: string = isSurvivor ? "#00ff00" : "#ff0000";
+    const symbol: string = isSurvivor ? "√" : "X";
+    const widget: Widget = new RichText()
+      .setFontSize(18)
+      .setText(`[color=${bbColor}]${rollValue}${symbol}[/color]`);
+
+    const ui: UIElement = new UIElement();
+    ui.position = localAbove;
+    ui.presentationStyle = UIPresentationStyle.ViewAligned;
+    ui.widget = widget;
+    unitObj.addUI(ui);
+
+    const onReleasedHandler = (obj: GameObject): void => {
+      obj.removeUIElement(ui);
+      obj.onReleased.remove(onReleasedHandler);
+    };
+    unitObj.onReleased.add(onReleasedHandler);
   }
 
   static isRiftSystemTile(obj: GameObject): boolean {
