ca51d3d01340ce922fab8e701185d16ea3531386
Darrell <darrell.anderson@gmail.com>
2025-02-28 11:14:23 -0800

    clean up; test coverage

--- src/lib/map-string-lib/map-place-planet-cards.test.ts
+++ src/lib/map-string-lib/map-place-planet-cards.test.ts
@@ -1,12 +1,160 @@
-import { MockGameObject } from "ttpg-mock";
+import {
+  MockCard,
+  MockCardDetails,
+  MockGameObject,
+  MockSnapPoint,
+} from "ttpg-mock";
 import { MapPlacePlanetCards } from "./map-place-planet-cards";
-
-it("static getAllPlanets", () => {
-  MockGameObject.simple("tile.system:base/18");
-  const planets = MapPlacePlanetCards.getAllPlanets();
-  expect(planets.length).toBe(1);
-});
+import { Card } from "@tabletop-playground/api";
+import { Find, NSID } from "ttpg-darrell";
 
 it("constructor", () => {
   new MapPlacePlanetCards();
+});
+
+it("_getAllPlanets", () => {
+  MockGameObject.simple("tile.system:base/18");
+  const mapPlacePlanetCards = new MapPlacePlanetCards();
+  const planets = mapPlacePlanetCards._getAllPlanets();
+  expect(planets.length).toBe(1);
+});
+
+it("_getCardNsidToPlanet", () => {
+  MockGameObject.simple("tile.system:pok/65");
+  const mapPlacePlanetCards = new MapPlacePlanetCards();
+  const nsidToPlanet = mapPlacePlanetCards._getCardNsidToPlanet();
+  expect(nsidToPlanet.size).toBe(2);
+  expect([...nsidToPlanet.keys()].sort()).toEqual([
+    "card.legendary_planet:pok/the-atrament",
+    "card.planet:pok/primor",
+  ]);
+});
+
+it("_getActivePlanetsDecks", () => {
+  const legednaryDeck = new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.legendary_planet:pok/the-atrament",
+      }),
+      new MockCardDetails({
+        metadata: "card.legendary_planet:pok/__some-other-legendary__",
+      }),
+    ],
+  });
+  const _legendaryMat = new MockGameObject({
+    snapPoints: [
+      new MockSnapPoint({
+        snappedObject: legednaryDeck,
+        tags: ["deck-legendary-planet"],
+      }),
+    ],
+  });
+
+  const planetDeck = new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.planet:pok/primor",
+      }),
+      new MockCardDetails({
+        metadata: "card.planet:pok/__some-other-planet__",
+      }),
+    ],
+  });
+  const _planetMat = new MockGameObject({
+    snapPoints: [
+      new MockSnapPoint({
+        snappedObject: planetDeck,
+        tags: ["deck-planet"],
+      }),
+    ],
+  });
+
+  MockGameObject.simple("tile.system:pok/65");
+  const mapPlacePlanetCards = new MapPlacePlanetCards();
+  const nsidToPlanet = mapPlacePlanetCards._getCardNsidToPlanet();
+  const nsids = new Set<string>(nsidToPlanet.keys());
+  expect(nsids.size).toBe(2);
+  const decks: Array<Card> = mapPlacePlanetCards._getActivePlanetsDecks(nsids);
+  expect(decks.length).toBe(2);
+
+  const deck0: Card | undefined = decks[0];
+  if (!deck0) {
+    throw new Error("deck0 is undefined");
+  }
+  expect(NSID.getDeck(deck0)).toEqual([
+    "card.legendary_planet:pok/the-atrament",
+  ]);
+  expect(NSID.getDeck(legednaryDeck)).toEqual([
+    "card.legendary_planet:pok/__some-other-legendary__", // unused remains
+  ]);
+
+  const deck1: Card | undefined = decks[1];
+  if (!deck1) {
+    throw new Error("deck1 is undefined");
+  }
+  expect(NSID.getDeck(deck1)).toEqual(["card.planet:pok/primor"]);
+  expect(NSID.getDeck(planetDeck)).toEqual([
+    "card.planet:pok/__some-other-planet__", // unused remains
+  ]);
+});
+
+it("placePlanetCards", () => {
+  const legednaryDeck = new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.legendary_planet:pok/the-atrament",
+      }),
+      new MockCardDetails({
+        metadata: "card.legendary_planet:pok/__some-other-legendary__",
+      }),
+    ],
+  });
+  const _legendaryMat = new MockGameObject({
+    snapPoints: [
+      new MockSnapPoint({
+        snappedObject: legednaryDeck,
+        tags: ["deck-legendary-planet"],
+      }),
+    ],
+  });
+
+  const planetDeck = new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.planet:pok/primor",
+      }),
+      new MockCardDetails({
+        metadata: "card.planet:pok/__some-other-planet__",
+      }),
+    ],
+  });
+  const _planetMat = new MockGameObject({
+    snapPoints: [
+      new MockSnapPoint({
+        snappedObject: planetDeck,
+        tags: ["deck-planet"],
+      }),
+    ],
+  });
+
+  MockGameObject.simple("tile.system:pok/65");
+  const mapPlacePlanetCards = new MapPlacePlanetCards();
+  mapPlacePlanetCards.placePlanetCards();
+
+  expect(NSID.getDeck(legednaryDeck)).toEqual([
+    "card.legendary_planet:pok/__some-other-legendary__", // unused remains
+  ]);
+  expect(NSID.getDeck(planetDeck)).toEqual([
+    "card.planet:pok/__some-other-planet__", // unused remains
+  ]);
+
+  const legendaryCard: Card | undefined = new Find().findCard(
+    "card.legendary_planet:pok/the-atrament"
+  );
+  expect(legendaryCard).toBeDefined();
+
+  const planetCard: Card | undefined = new Find().findCard(
+    "card.planet:pok/primor"
+  );
+  expect(planetCard).toBeDefined();
 });
--- src/lib/map-string-lib/map-place-planet-cards.ts
+++ src/lib/map-string-lib/map-place-planet-cards.ts
@@ -1,79 +1,116 @@
-import { Card } from "@tabletop-playground/api";
+import { Card, Vector } from "@tabletop-playground/api";
 import { CardUtil, Find, NSID } from "ttpg-darrell";
 import { Planet } from "../system-lib/planet/planet";
 
 export class MapPlacePlanetCards {
-  static getAllPlanets(): Array<Planet> {
+  private readonly _cardUtil: CardUtil = new CardUtil();
+  private readonly _find: Find = new Find();
+
+  /**
+   * Get all non-home planets in the game.
+   *
+   * @returns
+   */
+  _getAllPlanets(): Array<Planet> {
     const planets: Array<Planet> = [];
     const skipContained: boolean = true;
     TI4.systemRegistry
       .getAllSystemsWithObjs(skipContained)
       .forEach((system) => {
-        system.getPlanets().forEach((planet) => {
-          planets.push(planet);
-        });
+        if (!system.isHome()) {
+          system.getPlanets().forEach((planet) => {
+            planets.push(planet);
+          });
+        }
       });
     return planets;
   }
 
-  static getPlanetNsidSet(planets: Array<Planet>): Set<string> {
-    const nsids: Set<string> = new Set();
+  /**
+   * Get map from card nsid to planet.  Legendary planets have multiple cards.
+   *
+   * @returns
+   */
+  _getCardNsidToPlanet(): Map<string, Planet> {
+    const nsidToPlanet: Map<string, Planet> = new Map();
+    const planets: Array<Planet> = this._getAllPlanets();
     planets.forEach((planet) => {
       const nsid: string = planet.getPlanetCardNsid();
-      nsids.add(nsid);
+      nsidToPlanet.set(nsid, planet);
+
+      const legenedaryNsids: Array<string> = planet.getLegendaryCardNsids();
+      legenedaryNsids.forEach((legendaryNsid: string): void => {
+        nsidToPlanet.set(legendaryNsid, planet);
+      });
     });
-    return nsids;
+    return nsidToPlanet;
   }
 
-  static getActivePlanetsDeck(nsids: Set<string>): Card | undefined {
-    // Find the total planets deck.
-    let planetsDeck: Card | undefined = new Find().findDeckOrDiscard(
-      "deck-planet"
+  /**
+   * Get subset decks of planet/legedary decks for the given card nsids.
+   * Other (inactive) planet cards stay in their original decks.
+   *
+   * @param nsids
+   * @returns
+   */
+  _getActivePlanetsDecks(nsids: Set<string>): Array<Card> {
+    const originalDecks: Array<Card> = [];
+
+    // Add legendary planets first, cards dealt first (below normal planets).
+    const legednaryDeck: Card | undefined = this._find.findDeckOrDiscard(
+      "deck-legendary-planet"
     );
+    if (legednaryDeck) {
+      originalDecks.push(legednaryDeck);
+    }
+
+    const planetsDeck: Card | undefined =
+      this._find.findDeckOrDiscard("deck-planet");
+    if (planetsDeck) {
+      originalDecks.push(planetsDeck);
+    }
 
     // Filter down to the planets that are in the game.
-    if (planetsDeck) {
-      planetsDeck = new CardUtil().filterCards(
-        planetsDeck,
+    const activeCardsDecks: Array<Card> = [];
+    for (const originalDeck of originalDecks) {
+      const filteredDeck: Card | undefined = this._cardUtil.filterCards(
+        originalDeck,
         (nsid: string): boolean => {
           return nsids.has(nsid);
         }
       );
+      if (filteredDeck && filteredDeck.getStackSize() > 0) {
+        activeCardsDecks.push(filteredDeck);
+      }
     }
-    return planetsDeck;
+    return activeCardsDecks;
   }
 
-  static getPlanetCards(): Map<Planet, Card> {
-    const planetToCard: Map<Planet, Card> = new Map();
+  /**
+   * Place planet cards above their respective planets.
+   */
+  placePlanetCards(): void {
+    // Get planets, indexed by nsids (legendary planets can have multiple).
+    const nsidToPlanet: Map<string, Planet> = this._getCardNsidToPlanet();
 
-    const nsids: Set<string> = new Set();
-    const planets: Array<Planet> = MapPlacePlanetCards.getAllPlanets();
-    planets.forEach((planet) => {
-      const nsid: string = planet.getPlanetCardNsid();
-      nsids.add(nsid);
-    });
-
-    const planetsDeck: Card | undefined =
-      MapPlacePlanetCards.getActivePlanetsDeck(nsids);
-    if (planetsDeck) {
-      // Get separate cards, index by nsid.
-      const nsidToCard: Map<string, Card> = new Map();
-      const planetCards: Array<Card> = new CardUtil().separateDeck(planetsDeck);
+    // Get planet cards.
+    const nsids: Set<string> = new Set(nsidToPlanet.keys());
+    const decks: Array<Card> = this._getActivePlanetsDecks(nsids);
+    for (const deck of decks) {
+      const planetCards: Array<Card> = new CardUtil().separateDeck(deck);
       planetCards.forEach((card) => {
         const nsid: string = NSID.get(card);
-        nsidToCard.set(nsid, card);
-      });
-
-      // Map planets to cards.
-      planets.forEach((planet) => {
-        const nsid: string = planet.getPlanetCardNsid();
-        const card: Card | undefined = nsidToCard.get(nsid);
-        if (card) {
-          planetToCard.set(planet, card);
+        const planet: Planet | undefined = nsidToPlanet.get(nsid);
+        if (planet) {
+          this._placePlanetCard(planet, card);
         }
       });
     }
+  }
 
-    return planetToCard;
+  _placePlanetCard(planet: Planet, card: Card): void {
+    const above: Vector = planet.getPosition().add([0, 0, 10]);
+    card.setPosition(above);
+    card.snapToGround();
   }
 }
