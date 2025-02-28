3251877b2cf97c36eab518a48b80b12f508dd2d5
darrell <darrell.anderson@gmail.com>
2025-02-28 09:36:22 -0800

    broken; commit to switch computers

--- src/lib/map-string-lib/map-place-planet-cards.test.ts
+++ src/lib/map-string-lib/map-place-planet-cards.test.ts
@@ -1,4 +1,11 @@
+import { MockGameObject } from "ttpg-mock";
 import { MapPlacePlanetCards } from "./map-place-planet-cards";
+
+it("static getAllPlanets", () => {
+  MockGameObject.simple("tile.system:base/18");
+  const planets = MapPlacePlanetCards.getAllPlanets();
+  expect(planets.length).toBe(1);
+});
 
 it("constructor", () => {
   new MapPlacePlanetCards();
--- src/lib/map-string-lib/map-place-planet-cards.ts
+++ src/lib/map-string-lib/map-place-planet-cards.ts
@@ -1,26 +1,71 @@
 import { Card } from "@tabletop-playground/api";
-import { Planet } from "lib/system-lib/planet/planet";
-
-type PlanetAndCardType = {
-  planet: Planet;
-  card?: Card;
-};
+import { CardUtil, Find, NSID } from "ttpg-darrell";
+import { Planet } from "../system-lib/planet/planet";
 
 export class MapPlacePlanetCards {
-  static getPlanetAndCardEntries(): Array<PlanetAndCardType> {
-    const entries: Array<PlanetAndCardType> = [];
-
+  static getAllPlanets(): Array<Planet> {
+    const planets: Array<Planet> = [];
     const skipContained: boolean = true;
     TI4.systemRegistry
       .getAllSystemsWithObjs(skipContained)
       .forEach((system) => {
         system.getPlanets().forEach((planet) => {
-          entries.push({
-            planet,
-          });
+          planets.push(planet);
         });
       });
+    return planets;
+  }
 
-    return entries;
+  static getPlanetNsidSet(planets: Array<Planet>): Set<string> {
+    const nsids: Set<string> = new Set();
+    planets.forEach((planet) => {
+      const nsid: string = planet.getPlanetCardNsid();
+      nsids.add(nsid);
+    });
+    return nsids;
+  }
+
+  static getActivePlanetsDeck(nsids: Set<string>): Card | undefined {
+    let planetsDeck: Card | undefined = new Find().findDeckOrDiscard(
+      "deck-planet"
+    );
+    if (planetsDeck) {
+      planetsDeck = new CardUtil().filterCards(
+        planetsDeck,
+        (nsid: string): boolean => {
+          return nsids.has(nsid);
+        }
+      );
+    }
+    return planetsDeck;
+  }
+
+  static getPlanetCards(): Map<Planet, Card> {
+    const planetToCard: Map<Planet, Card> = new Map();
+
+    const nsids: Set<string> = new Set();
+    const planets: Array<Planet> = MapPlacePlanetCards.getAllPlanets();
+    planets.forEach((planet) => {
+      const nsid: string = planet.getPlanetCardNsid();
+      nsids.add(nsid);
+    });
+
+    if (planetsDeck) {
+      const planetCards: Array<Card> = new CardUtil().separateDeck(planetsDeck);
+      const nsidToCard: Map<string, Card> = new Map();
+      planetCards.forEach((card) => {
+        const nsid: string = NSID.get(card);
+        nsidToCard.set(nsid, card);
+      });
+      planets.forEach((planet) => {
+        const nsid: string = planet.getPlanetCardNsid();
+        const card: Card | undefined = nsidToCard.get(nsid);
+        if (card) {
+          planetToCard.set(planet, card);
+        }
+      });
+    }
+
+    return planetToCard;
   }
 }
