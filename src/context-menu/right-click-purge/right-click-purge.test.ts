04b27013d8287e82c6cbc130d421995f81de38cb
darrell <darrell.anderson@gmail.com>
2025-03-02 22:12:54 -0800

    test coverage

--- src/context-menu/right-click-purge/right-click-purge.test.ts
+++ src/context-menu/right-click-purge/right-click-purge.test.ts
@@ -1,6 +1,45 @@
-import { RightClickPurge } from "./right-click-purge";
+import { Card, GameObject, Player } from "@tabletop-playground/api";
+import { PURGE_ACTION_NAME, RightClickPurge } from "./right-click-purge";
+import { MockCard, MockContainer, MockGameObject, MockPlayer } from "ttpg-mock";
 
-it("constructor", () => {
-  const rightClickPurge = new RightClickPurge();
-  expect(rightClickPurge).toBeDefined();
+it("static _isPurgeable", () => {
+  const objYes: GameObject = MockGameObject.simple("type:source/name|purge");
+  const objNo: GameObject = MockGameObject.simple("type:source/name");
+  expect(RightClickPurge._isPurgeable(objYes)).toBe(true);
+  expect(RightClickPurge._isPurgeable(objNo)).toBe(false);
+});
+
+it("constructor/init", () => {
+  new MockGameObject();
+  new RightClickPurge().init();
+});
+
+it("purge", () => {
+  const obj: GameObject = MockGameObject.simple("type:source/name|purge");
+  new MockContainer({ templateMetadata: "container:base/purged" });
+  new RightClickPurge()._purge(obj);
+});
+
+it("_onObjectCreatedHandler", () => {
+  new RightClickPurge().init();
+  MockGameObject.simple("type:source/name|purge");
+});
+
+it("_onCardMadeSingletonHandler", () => {
+  new RightClickPurge().init();
+  MockCard.simple("type:source/name|purge");
+});
+
+it("_onCardMakeDeckHandler", () => {
+  new RightClickPurge().init();
+  const card1: Card = MockCard.simple("type:source/name|purge");
+  const card2: Card = MockCard.simple("type:source/name|purge");
+  card1.addCards(card2);
+});
+
+it("_onCustomActionHandler", () => {
+  new RightClickPurge().init();
+  const obj: MockGameObject = MockGameObject.simple("type:source/name|purge");
+  const player: Player = new MockPlayer();
+  obj._customActionAsPlayer(player, PURGE_ACTION_NAME);
 });
--- src/context-menu/right-click-purge/right-click-purge.ts
+++ src/context-menu/right-click-purge/right-click-purge.ts
@@ -1,13 +1,80 @@
-import { world } from "@tabletop-playground/api";
-import { Find, IGlobal } from "ttpg-darrell";
+import {
+  Card,
+  Container,
+  GameObject,
+  globalEvents,
+  Player,
+  world,
+} from "@tabletop-playground/api";
+import { Find, IGlobal, NSID, OnCardBecameSingletonOrDeck } from "ttpg-darrell";
+
+export const PURGE_ACTION_NAME: string = "*Purge";
 
 export class RightClickPurge implements IGlobal {
   private readonly _find: Find = new Find();
 
+  private readonly _onObjectCreatedHandler = (obj: GameObject): void => {
+    this._maybeAddPurge(obj);
+  };
+
+  private readonly _onCardMadeSingletonHandler = (card: Card): void => {
+    this._maybeAddPurge(card);
+  };
+
+  private readonly _onCardMakeDeckHandler = (card: Card): void => {
+    card.removeCustomAction(PURGE_ACTION_NAME);
+    card.onCustomAction.remove(this._onCustomActionHandler);
+  };
+
+  private readonly _onCustomActionHandler = (
+    object: GameObject,
+    _player: Player,
+    identifier: string
+  ): void => {
+    if (identifier === PURGE_ACTION_NAME) {
+      this._purge(object);
+    }
+  };
+
+  public static _isPurgeable(obj: GameObject): boolean {
+    const nsid: string = NSID.get(obj);
+    return nsid.endsWith("|purge");
+  }
+
   init(): void {
     const skipContained: boolean = false;
     for (const obj of world.getAllObjects(skipContained)) {
-      //
+      this._maybeAddPurge(obj);
+    }
+    globalEvents.onObjectCreated.add(this._onObjectCreatedHandler);
+    OnCardBecameSingletonOrDeck.onSingletonCardCreated.add(
+      this._onCardMadeSingletonHandler
+    );
+    OnCardBecameSingletonOrDeck.onSingletonCardMadeDeck.add(
+      this._onCardMakeDeckHandler
+    );
+  }
+
+  _maybeAddPurge(obj: GameObject): void {
+    if (RightClickPurge._isPurgeable(obj)) {
+      obj.removeCustomAction(PURGE_ACTION_NAME);
+      obj.addCustomAction(PURGE_ACTION_NAME);
+      obj.onCustomAction.remove(this._onCustomActionHandler);
+      obj.onCustomAction.add(this._onCustomActionHandler);
+    }
+  }
+
+  _purge(obj: GameObject): void {
+    const nsid: string = "container:base/purged";
+    const playerSlot: number | undefined = undefined;
+    const skipContained: boolean = true;
+    const purgeContainer: Container | undefined = this._find.findContainer(
+      nsid,
+      playerSlot,
+      skipContained
+    );
+    if (purgeContainer) {
+      purgeContainer.addObjects([obj]);
     }
   }
 }
