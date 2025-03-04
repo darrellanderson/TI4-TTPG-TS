ba62a2c70a6e1247bde70d3b10deea4319ae955f
darrell <darrell.anderson@gmail.com>
2025-03-03 22:24:41 -0800

    filter premade maps

--- src/ui/map-premade-ui/map-premade-ui.ts
+++ src/ui/map-premade-ui/map-premade-ui.ts
@@ -2,6 +2,8 @@ import {
   Button,
   HorizontalAlignment,
   Player,
+  TextBox,
+  TextJustification,
   VerticalAlignment,
 } from "@tabletop-playground/api";
 import {
@@ -22,10 +24,11 @@ import { PREMADE_MAPS_8P } from "../../lib/map-string-lib/data/premade-maps-8p.d
 import { AbstractUI } from "../abstract-ui/abtract-ui";
 import { ButtonUI } from "../button-ui/button-ui";
 import { CONFIG } from "../config/config";
+import { EditableUI } from "../button-ui/editable-ui";
 import { HorizontalUIBuilder } from "../panel/horizontal-ui-builder";
+import { LabelUI } from "../button-ui/label-ui";
 import { MapUI } from "../map-ui/map-ui";
 import { VerticalUIBuilder } from "../panel/vertical-ui-builder";
-import { CreateZoomedUiType, ZoomableUI } from "../zoomable-ui/zoomable-ui";
 
 export class MapPremadeUI extends AbstractUI {
   public readonly onMapString = new TriggerableMulticastDelegate<
@@ -65,11 +68,7 @@ export class MapPremadeUI extends AbstractUI {
     const mapString: string = MapPremadeUI._emptyMapString(playerCount);
     const hexToLabel: Map<HexType, string> = new Map();
 
-    const createMapUi: CreateZoomedUiType = (scale: number): AbstractUI => {
-      return new MapUI(this._mapString, hexToLabel, scale * 2);
-    };
-    const mapUi: MapUI = new MapUI(mapString, hexToLabel, scale);
-    const zoomableMapUi: AbstractUI = new ZoomableUI(mapUi, scale, createMapUi);
+    const mapUi: MapUI = new MapUI(mapString, hexToLabel, scale * 1.7);
 
     const useMapButton: ButtonUI = new ButtonUI(scale);
     useMapButton.getButton().setText("Use Map");
@@ -82,17 +81,26 @@ export class MapPremadeUI extends AbstractUI {
     );
     useMapButton.getButton().setEnabled(false);
 
-    const mapAndUseButton: AbstractUI = new VerticalUIBuilder()
+    const filterLabel: LabelUI = new LabelUI(scale);
+    filterLabel.getText().setText("Filter:");
+    filterLabel.getText().setJustification(TextJustification.Right);
+    const filter: EditableUI = new EditableUI(scale);
+    const filterLabelAndFilter: AbstractUI = new HorizontalUIBuilder()
       .setSpacing(CONFIG.SPACING * scale)
-      .addUIs([zoomableMapUi, useMapButton])
+      .addUIs([filterLabel, filter])
+      .build();
+
+    const mapLeft: AbstractUI = new VerticalUIBuilder()
+      .setSpacing(CONFIG.SPACING * scale)
+      .addUIs([filterLabelAndFilter, mapUi, useMapButton])
       .setHorizontalAlignment(HorizontalAlignment.Center)
       .build();
 
     // Show premade maps.
     const premadeMaps: Array<PremadeMapType> =
       MapPremadeUI.getPremadeMaps(playerCount);
-    const premadeMapButtons: Array<AbstractUI> = premadeMaps.map(
-      (premadeMap: PremadeMapType): AbstractUI => {
+    const premadeMapButtons: Array<ButtonUI> = premadeMaps.map(
+      (premadeMap: PremadeMapType): ButtonUI => {
         const buttonUi: ButtonUI = new ButtonUI(scale);
         buttonUi
           .getButton()
@@ -108,17 +116,32 @@ export class MapPremadeUI extends AbstractUI {
     );
     const premadeMapsList: AbstractUI = new VerticalUIBuilder()
       .setSpacing(CONFIG.SPACING * scale)
-      .setOverrideHeight(mapAndUseButton.getSize().h)
+      .setOverrideHeight(mapLeft.getSize().h)
       .addUIs(premadeMapButtons)
       .build();
 
     const abstractUi: AbstractUI = new HorizontalUIBuilder()
       .setSpacing(CONFIG.SPACING * scale)
-      .addUIs([mapAndUseButton, premadeMapsList])
+      .addUIs([mapLeft, premadeMapsList])
       .setVerticalAlignment(VerticalAlignment.Top)
       .build();
     super(abstractUi.getWidget(), abstractUi.getSize());
 
     this._mapString = mapString;
+
+    filter
+      .getEditText()
+      .onTextChanged.add(
+        (_textBox: TextBox, _player: Player, text: string): void => {
+          text = text.toLowerCase();
+          premadeMapButtons.forEach((buttonUi: ButtonUI): void => {
+            buttonUi
+              .getWidget()
+              .setVisible(
+                buttonUi.getButton().getText().toLowerCase().includes(text)
+              );
+          });
+        }
+      );
   }
 }
