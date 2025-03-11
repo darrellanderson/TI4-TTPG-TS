420e598c97431103a503ad9c1c94c9c80a437660
Darrell <darrell.anderson@gmail.com>
2025-03-10 17:30:59 -0700

    move params to class entry

--- src/ui/draft/draft-start-ui/draft-start-ui.ts
+++ src/ui/draft/draft-start-ui/draft-start-ui.ts
@@ -7,9 +7,16 @@ import {
 } from "../../../lib/draft-lib/draft-activity-start/draft-activity-start";
 import { IDraft } from "../../../lib/draft-lib/drafts/idraft";
 import { VerticalUIBuilder } from "../../panel/vertical-ui-builder";
+import { Broadcast } from "ttpg-darrell";
 
 export class DraftStartUI extends AbstractUI {
   private readonly _idraft: IDraft;
+  private readonly _params: DraftActivityStartParams = {
+    namespaceId: DRAFT_NAMESPACE_ID,
+    numSlices: 6,
+    numFactions: 6,
+    config: "",
+  };
 
   constructor(scale: number, idraft: IDraft) {
     const ui: AbstractUI = new VerticalUIBuilder()
@@ -22,12 +29,10 @@ export class DraftStartUI extends AbstractUI {
 
   start(): void {
     const errors: Array<string> = new Array<string>();
-    const params: DraftActivityStartParams = {
-      namespaceId: DRAFT_NAMESPACE_ID,
-      numSlices: 6,
-      numFactions: 6,
-      config: "",
-    };
-    new DraftActivityStart().start(this._idraft, params, errors);
+    new DraftActivityStart().start(this._idraft, this._params, errors);
+    if (errors.length > 0) {
+      const msg: string = errors.join("\n");
+      Broadcast.chatAll("Draft start errors:\n" + msg);
+    }
   }
 }
