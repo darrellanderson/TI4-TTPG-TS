6d0732cfe1968fd511a9e36c3b834a34976b9d45
Darrell <darrell.anderson@gmail.com>
2025-03-14 13:40:42 -0700

    test coverage

--- src/ui/draft/draft-start-ui/scpt-draft-button-ui.test.ts
+++ src/ui/draft/draft-start-ui/scpt-draft-button-ui.test.ts
@@ -1,5 +1,5 @@
-import { Widget } from "@tabletop-playground/api";
-import { clickAll } from "ttpg-mock";
+import { Button, Player, Widget } from "@tabletop-playground/api";
+import { clickAll, MockButton, MockPlayer } from "ttpg-mock";
 import { ScptDraftButtonUI, ScptDraftParams } from "./scpt-draft-button-ui";
 import { IDraft } from "../../../lib/draft-lib/drafts/idraft";
 import { Milty } from "../../../lib/draft-lib/drafts/milty";
@@ -23,6 +23,20 @@ it("clickAllButtons", () => {
     semi: idraft,
     final: idraft,
   };
-  const widget: Widget = new ScptDraftButtonUI(scale, params).getWidget();
-  clickAll(widget);
+  const scptDraftButtonUI: ScptDraftButtonUI = new ScptDraftButtonUI(
+    scale,
+    params
+  );
+
+  // Click the "confirm" buttons.
+  clickAll(scptDraftButtonUI.getWidget());
+
+  // Click the underlying buttons manually.
+  const button: Button = new MockButton();
+  const player: Player = new MockPlayer();
+
+  scptDraftButtonUI._qualHandler(button, player);
+  scptDraftButtonUI._prelimHandler(button, player);
+  scptDraftButtonUI._semiHandler(button, player);
+  scptDraftButtonUI._finalHandler(button, player);
 });
