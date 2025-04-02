ee06f9704b2727c41865133e964f20cb1dcadb58
Darrell <darrell.anderson@gmail.com>
2025-04-01 17:47:19 -0700

    scpt 2025 semis

--- src/lib/draft-lib/scpt/scpt-2025/scpt-2025.ts
+++ src/lib/draft-lib/scpt/scpt-2025/scpt-2025.ts
@@ -69,7 +69,149 @@ export class Scpt2025 extends AbstractScpt {
   }
 
   getSemi(): DraftActivityStartParams | undefined {
-    return undefined;
+    type FactionsSlicesLabels = {
+      factions: Array<string>;
+      slices: Array<string>;
+      labels: Array<string>;
+    };
+
+    const semis: Array<FactionsSlicesLabels> = [
+      // 1
+      {
+        factions: ["naalu", "hacan", "nekro", "jolnar", "letnev", "l1z1x"],
+        slices: [
+          "40,37,33,21,44",
+          "42,50,30,26,36",
+          "48,31,29,22,45",
+          "39,46,35,24,34",
+          "49,38,32,25,41",
+          "43,47,28,19,27",
+        ],
+        labels: ["Vaunt", "Mage", "9 of Spades", "Schroeder", "Magi", "Jaynor"],
+      },
+      // 2
+      {
+        factions: ["yin", "hacan", "l1z1x", "mentak", "creuss", "xxcha"],
+        slices: [
+          "28,30,19,42,48",
+          "22,33,37,26,46",
+          "36,27,24,40,45",
+          "29,31,20,41,48",
+          "21,35,32,39,50",
+          "34,38,23,25,44",
+        ],
+        labels: ["Moose", "Brian", "Ginger", "John", "Duke LukeM", "Shorty55"],
+      },
+      // 3
+      {
+        factions: [
+          "muaat",
+          "argent",
+          "empyrean",
+          "vuilraith",
+          "naazrokha",
+          "saar",
+        ],
+        slices: [
+          "21,69,68,39,47",
+          "29,74,46,64,67",
+          "27,37,45,26,78",
+          "73,28,43,40,60",
+          "19,76,41,79,65",
+          "50,70,72,25,20",
+        ],
+        labels: [
+          "Ondra",
+          "Wolphin",
+          "The Humble Checkmate",
+          "teddysjamforyou",
+          "HG Kool aid",
+          "telagos",
+        ],
+      },
+      // 4
+      {
+        factions: ["ul", "nomad", "norr", "yssaril", "sol", "mahact"],
+        slices: [
+          "67,28,50,73,26",
+          "27,29,46,45,25",
+          "40,37,31,65,41",
+          "77,76,68,20,19",
+          "79,35,34,64,78",
+          "39,66,69,47,74",
+        ],
+        labels: [
+          "codytct",
+          "NerfZerg",
+          "mantis",
+          "nerdY2K",
+          "NekroDiesTwice",
+          "Jonno",
+        ],
+      },
+      // 5
+      {
+        factions: ["mahact", "creuss", "yssaril", "empyrean", "l1z1x", "hacan"],
+        slices: [
+          "73,39,24,80,71",
+          "49,79,30,62,76",
+          "50,22,59,67,65",
+          "47,64,74,37,41",
+          "42,26,72,35,77",
+          "61,40,70,68,36",
+        ],
+        labels: [
+          "jadiimjedi",
+          "Sir Craigsworth",
+          "Deshuga",
+          "aljce",
+          "dzhiriki",
+          "Arratain",
+        ],
+      },
+      // 6
+      {
+        factions: [
+          "mentak",
+          "yin",
+          "keleres-argent",
+          "norr",
+          "muaat",
+          "arborec",
+        ],
+        slices: [
+          "35,25,44,73,49",
+          "34,22,67,47,66",
+          "27,40,72,79,65",
+          "41,32,48,59,69",
+          "78,75,42,24,26",
+          "39,76,62,80,64",
+        ],
+        labels: [
+          "Walrus Berzerker",
+          "The Meta",
+          "GreenFlame",
+          "catoftheyear",
+          "dzhiriki",
+          "Fingolfin",
+        ],
+      },
+    ];
+
+    // Technically this should be a player-driven choice, but the compact UI
+    // makes that challenging. So we will just pick a random one.
+    const choice: number = Math.floor(Math.random() * semis.length);
+    const factions: Array<string> = semis[choice]!.factions;
+    const slices: Array<string> = semis[choice]!.slices;
+    const labels: Array<string> = semis[choice]!.labels;
+
+    return {
+      namespaceId: DRAFT_NAMESPACE_ID,
+      draft: new Milty(),
+      numSlices: 6,
+      numFactions: 6,
+      config: `${slices.join("|")}&labels=${labels.join("|")}&factions=${factions.join("|")}`,
+    };
   }
 
   getFinal(): DraftActivityStartParams | undefined {
