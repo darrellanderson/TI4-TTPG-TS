2bf4231c2fcb64a5a074ce71398849a54aead1ab
Darrell <darrell.anderson@gmail.com>
2025-04-03 10:41:00 -0700

    wire up checkboxes, tests

--- src/ui/streamer-tool-ui/streamer-tool-ui.test.ts
+++ src/ui/streamer-tool-ui/streamer-tool-ui.test.ts
@@ -1,14 +1,34 @@
-import { Player } from "@tabletop-playground/api";
+import { Player, TextBox } from "@tabletop-playground/api";
 import { AbstractUI } from "../abstract-ui/abtract-ui";
 import { StreamerToolUI } from "./streamer-tool-ui";
-import { MockPlayer, mockWorld } from "ttpg-mock";
+import { clickAll, MockPlayer, MockTextBox, mockWorld } from "ttpg-mock";
 
 it("constructor", () => {
+  const scale: number = 1;
+  const playerSlot: number = 10;
+  const streamerUi: AbstractUI = new StreamerToolUI(scale, playerSlot);
+  streamerUi.destroy();
+});
+
+it("clickAll", () => {
+  const scale: number = 1;
   const playerSlot: number = 10;
   const player: Player = new MockPlayer({ slot: playerSlot });
   mockWorld._addPlayer(player);
 
-  const scale: number = 1;
   const streamerUi: AbstractUI = new StreamerToolUI(scale, playerSlot);
+  clickAll(streamerUi.getWidget());
+  clickAll(streamerUi.getWidget()); // again (flip checkboxes)
+  streamerUi.destroy();
+});
+
+it("_editableTimestampCommitted", () => {
+  const scale: number = 1;
+  const playerSlot: number = 10;
+  const streamerUi: StreamerToolUI = new StreamerToolUI(scale, playerSlot);
+
+  const textBox: TextBox = new MockTextBox();
+  const player: Player = new MockPlayer({ slot: playerSlot });
+  streamerUi._editableTimestampCommitted(textBox, player, "text", true);
   streamerUi.destroy();
 });
--- src/ui/streamer-tool-ui/streamer-tool-ui.ts
+++ src/ui/streamer-tool-ui/streamer-tool-ui.ts
@@ -1,14 +1,23 @@
-import { PlayerSlot } from "ttpg-darrell";
+import { Broadcast, PlayerSlot } from "ttpg-darrell";
 import { AbstractUI } from "../abstract-ui/abtract-ui";
 import { CheckBoxUI } from "../button-ui/checkbox-ui";
 import { CONFIG } from "../config/config";
 import { EditableUI } from "../button-ui/editable-ui";
 import { LabelUI } from "../button-ui/label-ui";
 import { VerticalUIBuilder } from "../panel/vertical-ui-builder";
-import { Player, world } from "@tabletop-playground/api";
+import { CheckBox, Player, TextBox, world } from "@tabletop-playground/api";
 
 export class StreamerToolUI extends AbstractUI {
   private readonly _ui: AbstractUI;
+
+  readonly _editableTimestampCommitted = (
+    textBox: TextBox,
+    _player: Player,
+    _text: string,
+    _usingEnter: boolean
+  ): void => {
+    textBox.setText(`${TI4.config.timestamp}`);
+  };
 
   constructor(scale: number, playerSlot: PlayerSlot) {
     const player: Player | undefined = world.getPlayerBySlot(playerSlot);
@@ -26,18 +35,63 @@ export class StreamerToolUI extends AbstractUI {
         .getCheckBox()
         .setIsChecked(TI4.hideMouseCursor.hasHideCursor(player));
     }
+    hideMouseCursor
+      .getCheckBox()
+      .onCheckStateChanged.add(
+        (
+          _checkBox: CheckBox,
+          clickingPlayer: Player,
+          isChecked: boolean
+        ): void => {
+          if (isChecked) {
+            TI4.hideMouseCursor.addHideCursor(clickingPlayer);
+          } else {
+            TI4.hideMouseCursor.removeHideCursor(clickingPlayer);
+          }
+        }
+      );
 
     const useStreamerBuddy: CheckBoxUI = new CheckBoxUI(scale);
     useStreamerBuddy.getCheckBox().setText("Streamer buddy");
     useStreamerBuddy
       .getCheckBox()
       .setIsChecked(TI4.useStreamerBuddy.getUseStreamerBuddy());
+    useStreamerBuddy
+      .getCheckBox()
+      .onCheckStateChanged.add(
+        (
+          _checkBox: CheckBox,
+          clickingPlayer: Player,
+          isChecked: boolean
+        ): void => {
+          const playerName: string = TI4.playerName.getByPlayer(clickingPlayer);
+          const msg: string = `Streamer buddy ${isChecked ? "enabled" : "disabled"} by ${playerName}`;
+          Broadcast.chatAll(msg);
+          TI4.useStreamerBuddy.setUseStreamerBuddy(isChecked);
+        }
+      );
 
     const autoStreamerCamera: CheckBoxUI = new CheckBoxUI(scale);
-    autoStreamerCamera.getCheckBox().setText("Auto streamer camera");
+    autoStreamerCamera.getCheckBox().setText("Auto move camera");
     autoStreamerCamera
       .getCheckBox()
       .setIsChecked(TI4.autoStreamerCamera.hasStreamerPlayerSlot(playerSlot));
+    autoStreamerCamera
+      .getCheckBox()
+      .onCheckStateChanged.add(
+        (
+          _checkBox: CheckBox,
+          clickingPlayer: Player,
+          isChecked: boolean
+        ): void => {
+          const clickingPlayerSlot: PlayerSlot = clickingPlayer.getSlot();
+          if (isChecked) {
+            TI4.autoStreamerCamera.addStreamerPlayerSlot(clickingPlayerSlot);
+          } else {
+            TI4.autoStreamerCamera.removeStreamerPlayerSlot(clickingPlayerSlot);
+          }
+        }
+      );
 
     const ui: AbstractUI = new VerticalUIBuilder()
       .setSpacing(CONFIG.SPACING * scale)
@@ -45,12 +99,17 @@ export class StreamerToolUI extends AbstractUI {
         labelTimestamp,
         editableTimestamp,
         hideMouseCursor,
+        useStreamerBuddy,
         autoStreamerCamera,
       ])
       .build();
 
     super(ui.getWidget(), ui.getSize());
     this._ui = ui;
+
+    editableTimestamp
+      .getEditText()
+      .onTextCommitted.add(this._editableTimestampCommitted);
   }
 
   destroy(): void {
