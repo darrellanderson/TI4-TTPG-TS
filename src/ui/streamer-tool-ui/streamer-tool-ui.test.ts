0bcd8e3a371381293ad8677c7c85050d2c690295
darrell <darrell.anderson@gmail.com>
2025-04-02 23:10:15 -0700

    progress

--- src/ui/streamer-tool-ui/streamer-tool-ui.test.ts
+++ src/ui/streamer-tool-ui/streamer-tool-ui.test.ts
@@ -1,9 +1,14 @@
+import { Player } from "@tabletop-playground/api";
 import { AbstractUI } from "../abstract-ui/abtract-ui";
 import { StreamerToolUI } from "./streamer-tool-ui";
+import { MockPlayer, mockWorld } from "ttpg-mock";
 
 it("constructor", () => {
-  const scale: number = 1;
   const playerSlot: number = 10;
+  const player: Player = new MockPlayer({ slot: playerSlot });
+  mockWorld._addPlayer(player);
+
+  const scale: number = 1;
   const streamerUi: AbstractUI = new StreamerToolUI(scale, playerSlot);
   streamerUi.destroy();
 });
--- src/ui/streamer-tool-ui/streamer-tool-ui.ts
+++ src/ui/streamer-tool-ui/streamer-tool-ui.ts
@@ -5,11 +5,14 @@ import { CONFIG } from "../config/config";
 import { EditableUI } from "../button-ui/editable-ui";
 import { LabelUI } from "../button-ui/label-ui";
 import { VerticalUIBuilder } from "../panel/vertical-ui-builder";
+import { Player, world } from "@tabletop-playground/api";
 
 export class StreamerToolUI extends AbstractUI {
   private readonly _ui: AbstractUI;
 
-  constructor(scale: number, _playerSlot: PlayerSlot) {
+  constructor(scale: number, playerSlot: PlayerSlot) {
+    const player: Player | undefined = world.getPlayerBySlot(playerSlot);
+
     const labelTimestamp: LabelUI = new LabelUI(scale);
     labelTimestamp.getText().setText("Game timestamp:");
 
@@ -18,12 +21,23 @@ export class StreamerToolUI extends AbstractUI {
 
     const hideMouseCursor: CheckBoxUI = new CheckBoxUI(scale);
     hideMouseCursor.getCheckBox().setText("Hide mouse cursor");
+    if (player) {
+      hideMouseCursor
+        .getCheckBox()
+        .setIsChecked(TI4.hideMouseCursor.hasHideCursor(player));
+    }
 
     const useStreamerBuddy: CheckBoxUI = new CheckBoxUI(scale);
     useStreamerBuddy.getCheckBox().setText("Streamer buddy");
+    useStreamerBuddy
+      .getCheckBox()
+      .setIsChecked(TI4.useStreamerBuddy.getUseStreamerBuddy());
 
     const autoStreamerCamera: CheckBoxUI = new CheckBoxUI(scale);
     autoStreamerCamera.getCheckBox().setText("Auto streamer camera");
+    autoStreamerCamera
+      .getCheckBox()
+      .setIsChecked(TI4.autoStreamerCamera.hasStreamerPlayerSlot(playerSlot));
 
     const ui: AbstractUI = new VerticalUIBuilder()
       .setSpacing(CONFIG.SPACING * scale)
