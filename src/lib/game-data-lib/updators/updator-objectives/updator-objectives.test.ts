1a6181f539378dcb2260c9d7acf5a32d321faef4
darrell <darrell.anderson@gmail.com>
2025-04-09 23:21:28 -0700

    progress

--- src/lib/game-data-lib/updators/updator-objectives/updator-objectives.test.ts
+++ src/lib/game-data-lib/updators/updator-objectives/updator-objectives.test.ts
@@ -3,3 +3,5 @@ import { UpdatorObjectives } from "./updator-objectives";
 it("constructor", () => {
   new UpdatorObjectives();
 });
+
+it("_getObjectiveCards", () => {});
--- src/lib/game-data-lib/updators/updator-objectives/updator-objectives.ts
+++ src/lib/game-data-lib/updators/updator-objectives/updator-objectives.ts
@@ -1,7 +1,9 @@
-import { Card, GameObject } from "@tabletop-playground/api";
+import { Card, GameObject, world } from "@tabletop-playground/api";
 import { GameData } from "../../game-data/game-data";
 import { IGameDataUpdator } from "../../i-game-data-updator/i-game-data-updator";
 import { UpdatorObjectivesType } from "./updator-objectives-type";
+import { RightClickScorePrivate } from "../../../../context-menu/right-click-score/right-click-score-private";
+import { RightClickScorePublic } from "../../../../context-menu/right-click-score/right-click-score-public";
 
 export class UpdatorObjectives implements IGameDataUpdator {
   update(gameData: GameData): void {
@@ -13,6 +15,23 @@ export class UpdatorObjectives implements IGameDataUpdator {
 
     // If a secret objective is on an "extra" slot on the stage 1/2 mat count it as a public.
     // TODO XXX
+  }
+
+  _getRelevanCards(): Array<Card> {
+    const objectiveCards: Array<Card> = [];
+
+    const skipContained: boolean = true;
+    for (const obj of world.getAllObjects(skipContained)) {
+      if (obj instanceof Card) {
+        if (
+          RightClickScorePrivate.isScorablePrivate(obj) ||
+          RightClickScorePublic.isScorablePublic(obj)
+        ) {
+          objectiveCards.push(obj);
+        }
+      }
+    }
+    return objectiveCards;
   }
 
   _getAvailableObjectives(): UpdatorObjectivesType {
