e1c281d6463b1d5d4eb4935bc708d3836dadc1ce
Darrell <darrell.anderson@gmail.com>
2025-04-10 14:01:40 -0700

    objectives progress

--- src/lib/game-data-lib/updators/updator-objectives/updator-objectives.test.ts
+++ src/lib/game-data-lib/updators/updator-objectives/updator-objectives.test.ts
@@ -1,7 +1,120 @@
+import { Card } from "@tabletop-playground/api";
+import { NSID } from "ttpg-darrell";
+import { MockCard, MockCardDetails, MockSnapPoint } from "ttpg-mock";
 import { UpdatorObjectives } from "./updator-objectives";
+import { UpdatorObjectivesType } from "./updator-objectives-type";
 
 it("constructor", () => {
   new UpdatorObjectives();
 });
 
-it("_getObjectiveCards", () => {});
+it("_getRelevantCards", () => {
+  MockCard.simple("card.objective.public-1:pok/amass-wealth");
+  MockCard.simple("card.objective.public-2:pok/achieve-supremacy");
+  MockCard.simple("card.objective.secret:base/adapt-new-strategies");
+  MockCard.simple("card.relic:pok/the-obsidian");
+  MockCard.simple("card.promissory.white:base/support-for-the-throne");
+  MockCard.simple("card.action:base/not-scorable");
+
+  const cards: Array<Card> = new UpdatorObjectives()._getRelevantCards();
+  const nsids: Array<string> = cards.map((card) => NSID.get(card));
+  expect(nsids).toEqual([
+    "card.objective.public-1:pok/amass-wealth",
+    "card.objective.public-2:pok/achieve-supremacy",
+    "card.objective.secret:base/adapt-new-strategies",
+    "card.relic:pok/the-obsidian",
+    "card.promissory.white:base/support-for-the-throne",
+  ]);
+});
+
+it("_getRelevantCards (ignore held)", () => {
+  MockCard.simple("card.objective.public-1:pok/amass-wealth");
+  MockCard.simple("card.objective.public-2:pok/achieve-supremacy", {
+    isHeld: true,
+  });
+  const cards: Array<Card> = new UpdatorObjectives()._getRelevantCards();
+  const nsids: Array<string> = cards.map((card) => NSID.get(card));
+  expect(nsids).toEqual(["card.objective.public-1:pok/amass-wealth"]);
+});
+
+it("_getRelevantCards (ignore snapped to certain points)", () => {
+  MockCard.simple("card.objective.public-1:pok/amass-wealth", {
+    snappedToPoint: new MockSnapPoint({ tags: ["card-agenda"] }),
+  });
+  MockCard.simple("card.objective.public-2:pok/achieve-supremacy", {
+    snappedToPoint: new MockSnapPoint({ tags: ["discard-agenda"] }),
+  });
+  MockCard.simple("card.objective.secret:base/adapt-new-strategies", {
+    snappedToPoint: new MockSnapPoint({ tags: ["active-agenda"] }),
+  });
+
+  const cards: Array<Card> = new UpdatorObjectives()._getRelevantCards();
+  const nsids: Array<string> = cards.map((card) => NSID.get(card));
+  expect(nsids).toEqual(["card.objective.public-1:pok/amass-wealth"]);
+});
+
+it("_fillObjectivesType", () => {
+  new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.objective.public-1:pok/amass-wealth",
+        name: "Amass Wealth",
+      }),
+    ],
+  });
+  new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.objective.public-2:pok/achieve-supremacy",
+        name: "Achieve Supremacy",
+      }),
+    ],
+  });
+  new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.objective.secret:base/adapt-new-strategies",
+        name: "Adapt New Strategies",
+      }),
+    ],
+  });
+  new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.relic:pok/the-obsidian",
+        name: "The Obsidian",
+      }),
+    ],
+  });
+  new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.promissory.white:base/support-for-the-throne",
+        name: "Support for the Throne",
+      }),
+    ],
+  });
+  new MockCard({
+    cardDetails: [
+      new MockCardDetails({
+        metadata: "card.action:base/not-scorable",
+        name: "Not Scorable",
+      }),
+    ],
+  });
+
+  const updatorObjectives = new UpdatorObjectives();
+  const cards: Array<Card> = updatorObjectives._getRelevantCards();
+  expect(cards.length).toBe(5);
+
+  const objectivesType: UpdatorObjectivesType =
+    updatorObjectives._fillObjectivesType(cards);
+  expect(objectivesType).toEqual({
+    Agenda: [],
+    Other: ["Support for the Throne"],
+    "Public Objectives I": ["Amass Wealth"],
+    "Public Objectives II": ["Achieve Supremacy"],
+    Relics: ["The Obsidian"],
+    "Secret Objectives": ["Adapt New Strategies"],
+  });
+});
--- src/lib/game-data-lib/updators/updator-objectives/updator-objectives.ts
+++ src/lib/game-data-lib/updators/updator-objectives/updator-objectives.ts
@@ -1,4 +1,11 @@
-import { Card, GameObject, world } from "@tabletop-playground/api";
+import {
+  Card,
+  CardDetails,
+  GameObject,
+  SnapPoint,
+  world,
+} from "@tabletop-playground/api";
+import { NSID } from "ttpg-darrell";
 import { GameData } from "../../game-data/game-data";
 import { IGameDataUpdator } from "../../i-game-data-updator/i-game-data-updator";
 import { UpdatorObjectivesType } from "./updator-objectives-type";
@@ -7,25 +14,55 @@ import { RightClickScorePublic } from "../../../../context-menu/right-click-scor
 
 export class UpdatorObjectives implements IGameDataUpdator {
   update(gameData: GameData): void {
-    const controlTokens: Array<GameObject> = [];
-    const objectiveCards: Array<Card> = [];
+    const controlTokens: Array<GameObject> = this._getControlTokens();
+    const objectiveCards: Array<Card> = this._getRelevantCards();
 
     // Root objectives.
+    gameData.objectives = this._fillObjectivesType(objectiveCards);
+
     // Per-player scored objectives.
 
     // If a secret objective is on an "extra" slot on the stage 1/2 mat count it as a public.
     // TODO XXX
   }
 
-  _getRelevanCards(): Array<Card> {
+  _getControlTokens(): Array<GameObject> {
+    const controlTokens: Array<GameObject> = [];
+    const skipContained: boolean = true;
+    for (const obj of world.getAllObjects(skipContained)) {
+      const nsid: string = NSID.get(obj);
+      if (nsid.startsWith("token.control:")) {
+        controlTokens.push(obj);
+      }
+    }
+    return controlTokens;
+  }
+
+  _getRelevantCards(): Array<Card> {
     const objectiveCards: Array<Card> = [];
 
     const skipContained: boolean = true;
     for (const obj of world.getAllObjects(skipContained)) {
       if (obj instanceof Card) {
+        if (obj.isHeld()) {
+          continue;
+        }
+        const snapPoint: SnapPoint | undefined = obj.getSnappedToPoint();
+        if (snapPoint) {
+          const tags: Array<string> = snapPoint.getTags();
+          if (
+            tags.includes("discard-agenda") ||
+            tags.includes("active-agenda")
+          ) {
+            continue;
+          }
+        }
+
+        const nsid: string = NSID.get(obj);
         if (
           RightClickScorePrivate.isScorablePrivate(obj) ||
-          RightClickScorePublic.isScorablePublic(obj)
+          RightClickScorePublic.isScorablePublic(obj) ||
+          nsid === "card.relic:pok/the-obsidian" // not scorable, but wanted for streamer display
         ) {
           objectiveCards.push(obj);
         }
@@ -34,15 +71,43 @@ export class UpdatorObjectives implements IGameDataUpdator {
     return objectiveCards;
   }
 
-  _getAvailableObjectives(): UpdatorObjectivesType {
-    const objectives: UpdatorObjectivesType = {
-      "Public Objectives I": [],
-      "Public Objectives II": [],
-      "Secret Objectives": [],
-      Agenda: [],
-      Other: [],
-      Relics: [],
+  _fillObjectivesType(objectiveCards: Array<Card>): UpdatorObjectivesType {
+    const public1s: Array<Card> = [];
+    const public2s: Array<Card> = [];
+    const secrets: Array<Card> = [];
+    const agendas: Array<Card> = [];
+    const relics: Array<Card> = [];
+    const other: Array<Card> = [];
+
+    for (const card of objectiveCards) {
+      const nsid: string = NSID.get(card);
+      if (nsid.startsWith("card.objective.public-1:")) {
+        public1s.push(card);
+      } else if (nsid.startsWith("card.objective.public-2:")) {
+        public2s.push(card);
+      } else if (nsid.startsWith("card.objective.secret:")) {
+        secrets.push(card);
+      } else if (nsid.startsWith("card.relic:")) {
+        relics.push(card);
+      } else {
+        other.push(card); // support for the throne
+      }
+    }
+
+    const getCardNames = (cards: Array<Card>): Array<string> => {
+      return cards.map((card: Card): string => {
+        const cardDetails: CardDetails = card.getCardDetails();
+        return cardDetails.name;
+      });
     };
-    return objectives;
+    const objectivesType: UpdatorObjectivesType = {
+      "Public Objectives I": getCardNames(public1s),
+      "Public Objectives II": getCardNames(public2s),
+      "Secret Objectives": getCardNames(secrets),
+      Agenda: getCardNames(agendas),
+      Other: getCardNames(other),
+      Relics: getCardNames(relics),
+    };
+    return objectivesType;
   }
 }
