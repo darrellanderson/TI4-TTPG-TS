7f6bd2d1252d56e4167c06472564a7cfe16ed9c9
darrell <darrell.anderson@gmail.com>
2025-06-01 23:25:44 -0700

    run inject script

---
+++ src/lib/homebrew-lib/run-inject-script/run-inject-script.ts
@@ -0,0 +1,43 @@
+import {
+  GameObject,
+  globalEvents,
+  ObjectType,
+  Package,
+  world,
+} from "@tabletop-playground/api";
+import { DeletedItemsContainer, IGlobal, Spawn } from "ttpg-darrell";
+
+export const RUN_SCRIPT_NSID: string = "unit:base/carrier";
+
+/**
+ * Homebrew modules can have inject scripts, run them when loaded.
+ */
+export class RunInjectScript implements IGlobal {
+  readonly _onPackageAdded = (pkg: Package): void => {
+    this._maybeRunInjectScript(pkg);
+  };
+
+  init(): void {
+    globalEvents.onPackageAdded.add(this._onPackageAdded);
+    for (const pkg of world.getAllowedPackages()) {
+      this._maybeRunInjectScript(pkg);
+    }
+  }
+
+  _maybeRunInjectScript(pkg: Package): void {
+    if (pkg.getScriptFiles().includes("inject.js")) {
+      // Create a new object, set the script to this, delete next frame.
+      const obj: GameObject | undefined = Spawn.spawn(
+        RUN_SCRIPT_NSID,
+        [0, 0, -100]
+      );
+      if (obj) {
+        obj.setObjectType(ObjectType.NonInteractive);
+        obj.setScript("inject.js", pkg.getUniqueId());
+        process.nextTick(() => {
+          DeletedItemsContainer.destroyWithoutCopying(obj);
+        });
+      }
+    }
+  }
+}
