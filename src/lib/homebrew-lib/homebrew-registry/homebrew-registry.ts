1b0b25ab055491df45a005b489caa5223a5279ec
darrell <darrell.anderson@gmail.com>
2025-08-26 21:53:08 -0700

    make the homebrew definition a zod schema for simpler validation

--- src/lib/homebrew-lib/homebrew-registry/homebrew-registry.ts
+++ src/lib/homebrew-lib/homebrew-registry/homebrew-registry.ts
@@ -1,34 +1,6 @@
 import { Spawn } from "ttpg-darrell";
-import { FactionSchemaType } from "../../faction-lib/schema/faction-schema";
-import { PlanetAttachmentSchemaType } from "../../system-lib/schema/planet-attachment-schema";
-import { SourceAndPackageIdSchemaType } from "../../system-lib/schema/basic-types-schema";
-import { SystemAttachmentSchemaType } from "../../system-lib/schema/system-attachment-schema";
-import { SystemSchemaType } from "../../system-lib/schema/system-schema";
-import { UnitAttrsSchemaType } from "../../unit-lib/schema/unit-attrs-schema";
-import { UnitModifierSchemaType } from "../../unit-lib/schema/unit-modifier-schema";
 import { SpawnMissingCards } from "../spawn-missing-cards/spawn-missing-cards";
-import { TechSchemaType } from "../../tech-lib/schema/tech-schema";
-
-export type HomebrewModuleType = {
-  // Each of source and package id uniquely identifies a module.
-  // Source is used in all relevant NSIDs.
-  sourceAndPackageId: SourceAndPackageIdSchemaType;
-
-  factions?: Array<FactionSchemaType>;
-  systems?: Array<SystemSchemaType>;
-
-  planetAttachments?: Array<PlanetAttachmentSchemaType>;
-  systemAttachments?: Array<SystemAttachmentSchemaType>;
-
-  unitAttrs?: Array<UnitAttrsSchemaType>;
-  unitModifiers?: Array<UnitModifierSchemaType>;
-
-  technologies?: Array<TechSchemaType>;
-
-  remove?: Array<string>; // NSIDs
-
-  nsidToTemplateId?: { [key: string]: string };
-};
+import { HomebrewModuleType } from "./homebrew-schema";
 
 /**
  * Homebrew modules register via this class.
---
+++ src/lib/homebrew-lib/homebrew-registry/homebrew-schema.ts
@@ -0,0 +1,32 @@
+import { z } from "zod";
+import { FactionSchema } from "../../faction-lib/schema/faction-schema";
+import { PlanetAttachmentSchema } from "../../system-lib/schema/planet-attachment-schema";
+import { SourceAndPackageIdSchema } from "../../system-lib/schema/basic-types-schema";
+import { SystemAttachmentSchema } from "../../system-lib/schema/system-attachment-schema";
+import { SystemSchema } from "../../system-lib/schema/system-schema";
+import { TechSchema } from "../../tech-lib/schema/tech-schema";
+import { UnitModifierSchema } from "../../unit-lib/schema/unit-modifier-schema";
+import { UnitAttrsSchema } from "../../unit-lib/schema/unit-attrs-schema";
+
+export const HomebrewModuleSchema = z.object({
+  // Each of source and package id uniquely identifies a module.
+  // Source is used in all relevant NSIDs.
+  sourceAndPackageId: SourceAndPackageIdSchema,
+
+  factions: z.array(FactionSchema).optional(),
+  systems: z.array(SystemSchema).optional(),
+
+  planetAttachments: z.array(PlanetAttachmentSchema).optional(),
+  systemAttachments: z.array(SystemAttachmentSchema).optional(),
+
+  unitAttrs: z.array(UnitAttrsSchema),
+  unitModifiers: z.array(UnitModifierSchema),
+
+  technologies: z.array(TechSchema),
+
+  remove: z.array(z.string()), // NSIDs
+
+  nsidToTemplateId: z.record(z.string(), z.string()),
+});
+
+export type HomebrewModuleType = z.infer<typeof HomebrewModuleSchema>;
