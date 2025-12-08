94da9d5913398665673ab63882e30903a6a97df8
darrell <darrell.anderson@gmail.com>
2025-12-08 13:49:37 -0800

    bag draft ui placeholder

---
+++ src/ui/draft/draft-bag-ui/draft-bag-ui.ts
@@ -0,0 +1,12 @@
+import { AbstractUI } from "../../abstract-ui";
+
+/**
+ * Configurable bag draft:
+ * - blues
+ * - reds
+ * - legendaries
+ * - wormholes
+ * - factions
+ * - use factions on table
+ */
+export class DraftBagUI extends AbstractUI {}
