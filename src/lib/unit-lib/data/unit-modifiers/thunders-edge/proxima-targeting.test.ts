1808f9d47ba4481cdd6bfcfcb08ef2d6e1928470
Darrell <darrell.anderson@gmail.com>
2025-12-13 13:46:55 -0800

    fix test

--- src/lib/unit-lib/data/unit-modifiers/thunders-edge/proxima-targeting.test.ts
+++ src/lib/unit-lib/data/unit-modifiers/thunders-edge/proxima-targeting.test.ts
@@ -1,7 +1,15 @@
 import { CombatRoll } from "../../../../combat-lib/combat-roll/combat-roll";
-import { OPPONENT, SELF } from "../abstract.test";
+import { OPPONENT, placeGameObjects, SELF } from "../abstract.test";
+
+it("registry", () => {
+  const nsid: string = "card.technology.red:thunders-edge/proxima-targeting-vi";
+  expect(TI4.unitModifierRegistry.getByNsid(nsid)).toBeDefined();
+});
 
 it("proxima targeting", () => {
+  placeGameObjects({
+    self: ["card.technology.red:thunders-edge/proxima-targeting-vi"],
+  });
   const combatRoll: CombatRoll = CombatRoll.createCooked({
     rollType: "proximaTargeting",
     hex: "<0,0,0>",
