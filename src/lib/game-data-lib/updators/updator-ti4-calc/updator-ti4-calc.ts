4f2e333293e0dee62a03dbeb3307ab6f75dddbe8
Darrell <darrell.anderson@gmail.com>
2025-12-23 17:11:03 -0800

    dele

--- src/lib/game-data-lib/updators/updator-ti4-calc/updator-ti4-calc.ts
+++
@@ -1,9 +0,0 @@
-import { GameData } from "../../game-data/game-data";
-import { IGameDataUpdator } from "../../i-game-data-updator/i-game-data-updator";
-
-export class UpdatorTI4Calc implements IGameDataUpdator {
-  // Cache old result in order to fill when gameData gets reset.
-  // (Replace wtih better data when we have it.)
-
-  update(gameData: GameData): void {}
-}
