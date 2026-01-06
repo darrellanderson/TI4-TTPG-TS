6fa3f22d4f572a3bb33d96061bd75f59cf7672aa
darrell <darrell.anderson@gmail.com>
2026-01-05 22:15:26 -0800

    saar docks roll for rift

--- src/context-menu/right-click-rift/right-click-rift.test.ts
+++ src/context-menu/right-click-rift/right-click-rift.test.ts
@@ -1,5 +1,5 @@
 import { GameObject, Player } from "@tabletop-playground/api";
-import { MockGameObject, MockPlayer } from "ttpg-mock";
+import { MockCardHolder, MockGameObject, MockPlayer } from "ttpg-mock";
 
 import { RIFT_ACTION_NAME, RightClickRift } from "./right-click-rift";
 
@@ -26,6 +26,25 @@ it("static getShipsInRift", () => {
     "unit:base/destroyer",
     { position: [100, 0, 0] }
   );
+  const ships: Array<GameObject> = RightClickRift.getShipsInRift(riftObj);
+  expect(ships).toEqual([ship]);
+});
+
+it("static getShipsInRift (saar dock)", () => {
+  new MockCardHolder({
+    templateMetadata: "card-holder:base/player-hand",
+    owningPlayerSlot: 2,
+  });
+  MockGameObject.simple("sheet.faction:base/saar", { owningPlayerSlot: 2 });
+
+  expect(TI4.factionRegistry.getByPlayerSlot(2)?.getNsid()).toBe(
+    "faction:base/saar"
+  );
+
+  const riftObj: GameObject = MockGameObject.simple("tile.system:base/41");
+  const ship: GameObject = MockGameObject.simple("unit:base/space-dock", {
+    owningPlayerSlot: 2,
+  });
   const ships: Array<GameObject> = RightClickRift.getShipsInRift(riftObj);
   expect(ships).toEqual([ship]);
 });
