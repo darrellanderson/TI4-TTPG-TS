ddeb90d3ebcc948135ebcd1d0e5181eeddbe12a4
Darrell <darrell.anderson@gmail.com>
2026-06-10 10:15:25 -0700

    index

--- src/locale/index.ts
+++ src/locale/index.ts
@@ -1,4 +1,5 @@
 // created from 'create-ts-index'
 
+export * from './extracted-descriptions';
 export * from './locale-context-menus';
 export * from './locale-descriptions';
--- src/ui/draft/draft-start-ui/index.ts
+++ src/ui/draft/draft-start-ui/index.ts
@@ -2,5 +2,6 @@
 
 export * from './draft-start-ui';
 export * from './draft-start-window';
+export * from './scpt-2026-semis-ui';
 export * from './scpt-draft-button-ui';
 export * from './scpt-drafts-ui';
