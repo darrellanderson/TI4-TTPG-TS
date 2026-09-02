d58c5cd31622de3befa247b2c41b2f7b52afc878
Darrell <darrell.anderson@gmail.com>
2026-09-02 09:57:08 -0700

    index

--- src/lib/twilights-fall-lib/data/index.ts
+++ src/lib/twilights-fall-lib/data/index.ts
@@ -1,6 +1,8 @@
 // created from 'create-ts-index'
 
 export * from './ability.data';
+export * from './echo.data';
+export * from './faction-tech.data';
 export * from './genome.data';
 export * from './paradigm.data';
 export * from './unit-upgrade.data';
