2dee4de43d7fee06e28daf41f1fd3e8a417d8a72
Rithvik Subramanya <rithviksub@gmail.com>
2026-09-20 17:14:19 -0400

    Added special case for Planesplitter and Neural Parasite so they don't count as face down when in obsidian side

--- src/lib/game-data-lib/updators/updator-player-tech/updator-player-tech.ts
+++ src/lib/game-data-lib/updators/updator-player-tech/updator-player-tech.ts
@@ -95,13 +95,23 @@ export class UpdatorPlayerTech implements IGameDataUpdator {
           .map((card: Card): string => {
             const nsid: string = NSID.get(card);
             const tech: Tech | undefined = TI4.techRegistry.getByNsid(nsid);
-            return tech ? tech.getName() : "";
+            if (!tech) return "";
+            let name = tech.getName();
+            if (!Facing.isFaceUp(card)) {
+              if (name === "Planesplitter") {
+                name = "Planesplitter-Obs.";
+              } else if (name === "Neural Parasite") {
+                name = "Neural Parasite-Obs.";
+              }
+            }
+            return name;
           })
           .filter((name: string): boolean => name.length > 0)
           .filter(
             (name: string, index: number, array: Array<string>): boolean =>
               array.indexOf(name) === index,
           ); // unique
+
         player.technologiesFaceDown = cards
           .filter((card: Card): boolean => {
             const isFaceUp: boolean = Facing.isFaceUp(card);
@@ -110,7 +120,12 @@ export class UpdatorPlayerTech implements IGameDataUpdator {
           .map((card: Card): string => {
             const nsid: string = NSID.get(card);
             const tech: Tech | undefined = TI4.techRegistry.getByNsid(nsid);
-            return tech ? tech.getAbbr() : "";
+            if (!tech) return "";
+            const name = tech.getName();
+            if (name === "Planesplitter" || name === "Neural Parasite") {
+              return "";
+            }
+            return tech.getAbbr();
           })
           .filter((name: string): boolean => name.length > 0)
           .filter(
